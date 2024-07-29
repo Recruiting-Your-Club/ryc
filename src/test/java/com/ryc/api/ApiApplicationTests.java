fd1e2fcf7136418bd7340e60f60af4e76fdca43a
josangjun <juncho12011201@gmail.com>
2024-07-29 22:39:55 +0900

    Create Project

---
+++ src/test/java/com/ryc/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryc.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
