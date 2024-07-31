9ad2d09f9efea5c4d6ee5a5a866fc00846494a8a
josangjun <juncho12011201@gmail.com>
2024-07-31 18:15:46 +0900

    chore: Swagger 설정 추가
    springdoc 라이브러리 추가 및 swagger 기본설정 SwaggerConfiguration 생성

---
+++ src/main/java/com/ryc/api/config/SwaggerConfiguration.java
@@ -0,0 +1,37 @@
+package com.ryc.api.config;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfiguration {
+    @Bean
+    public OpenAPI openAPI(){
+        String jwt = "JWT";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
+        Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
+                .name(jwt)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+        );
+
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo())
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("RYC API")
+                .description("Recruiting Your Club API 문서")
+                .version("1.0.0");
+    }
+}
