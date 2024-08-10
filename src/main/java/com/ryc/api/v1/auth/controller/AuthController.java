3ba1812c8e1efa80e9eefec584b66ba3aaf09b8a
josangjun <juncho12011201@gmail.com>
2024-08-10 20:03:23 +0900

    chore: swagger Login 엔드포인트 표시
    login 로직이 EmailPasswordAuthenticationFilter에서 수행되는 관계로 Swagger Docs에 엔드포인트가 표시 되지 않는 오류 Controller에 메소드 추가로 수정
    해당 메소드는 실제로 실행되지 않는다.

--- src/main/java/com/ryc/api/v1/auth/controller/AuthController.java
+++ src/main/java/com/ryc/api/v1/auth/controller/AuthController.java
@@ -1,8 +1,10 @@
 package com.ryc.api.v1.auth.controller;
 
+import com.ryc.api.v1.auth.dto.LoginRequest;
 import com.ryc.api.v1.auth.dto.RegisterRequest;
 import com.ryc.api.v1.auth.dto.RegisterResponse;
 import com.ryc.api.v1.auth.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -20,6 +22,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
+
+    @PostMapping("/login")
+    @Operation(summary = "Login", description = "사용자 로그인 인증 후, 인증 성공시 토큰 발행")
+    public void login(@RequestBody LoginRequest loginRequest) {
+        // 이 메서드는 실제로 작동하지 않습니다.
+        // UsernamePasswordAuthenticationFilter가 이 경로를 가로채어 인증을 처리합니다.
+        throw new IllegalStateException("This method should not be called. It's handled by Spring Security filter chain.");
+    }
 
     @PostMapping("/register")
     public ResponseEntity<?> register(@Valid @RequestBody RegisterRequest body) {
---
+++ src/main/java/com/ryc/api/v1/auth/dto/LoginRequest.java
@@ -0,0 +1,7 @@
+package com.ryc.api.v1.auth.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+
+public record LoginRequest(@NotEmpty(message = "email is empty") String email,
+                           @NotEmpty(message = "password is empty") String password) {
+}
