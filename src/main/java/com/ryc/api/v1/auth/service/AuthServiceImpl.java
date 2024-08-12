e5b572eecb3c0dcb5e2e7f2b98dd9c7dce0fb9b6
josangjun <juncho12011201@gmail.com>
2024-08-12 12:41:15 +0900

    refactor: UserRepository 네이밍 변경

--- src/main/java/com/ryc/api/v1/auth/service/AuthServiceImpl.java
+++ src/main/java/com/ryc/api/v1/auth/service/AuthServiceImpl.java
@@ -3,7 +3,7 @@ package com.ryc.api.v1.auth.service;
 import com.ryc.api.v1.auth.dto.RegisterRequest;
 import com.ryc.api.v1.auth.dto.RegisterResponse;
 import com.ryc.api.v1.user.domain.User;
-import com.ryc.api.v1.user.repository.userRepository;
+import com.ryc.api.v1.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
 
-    private final userRepository userRepository;
+    private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
