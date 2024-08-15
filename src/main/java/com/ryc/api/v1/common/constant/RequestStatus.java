3215b5a7fa19f847c54636ddb792ab54dc41a9d4
josangjun <juncho12011201@gmail.com>
2024-08-15 15:14:42 +0900

    fix: COMPLETED 타입 삭제
    APPROVED와 같은 기능을 하는 타입으로, 의미중복으로 삭제

--- src/main/java/com/ryc/api/v1/common/constant/RequestStatus.java
+++ src/main/java/com/ryc/api/v1/common/constant/RequestStatus.java
@@ -3,7 +3,6 @@ package com.ryc.api.v1.common.constant;
 public enum RequestStatus {
     PENDING,      // 요청이 제출되어 대기 중인 상태
     IN_PROGRESS,  // 요청이 처리 중인 상태
-    COMPLETED,    // 요청이 성공적으로 완료된 상태
     APPROVED,     // 요청이 승인된 상태
     REJECTED,     // 요청이 거부된 상태
     CANCELLED,    // 요청이 취소된 상태
@@ -11,3 +10,4 @@ public enum RequestStatus {
     ON_HOLD,       // 요청이 보류된 상태
     ALL
 }
+
