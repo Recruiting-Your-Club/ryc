3599503a591be6b5d0731e4d18bb912cee061ecd
josangjun <juncho12011201@gmail.com>
2024-09-13 16:14:09 +0900

    feat: 동아리내 권한(role) 리소스 관련 API 접근 전 권한검증 로직(AOP) 추가
    테스트 완료

--- src/main/java/com/ryc/api/v1/role/dto/request/UpdateStatusRequest.java
+++ src/main/java/com/ryc/api/v1/role/dto/request/UpdateStatusRequest.java
@@ -1,10 +1,12 @@
 package com.ryc.api.v1.role.dto.request;
 
 import com.ryc.api.v1.common.constant.RequestStatus;
+import com.ryc.api.v1.common.dto.ClubRoleSecuredDto;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 public record UpdateStatusRequest(
+        @NotNull(message = "clubRoleSecuredDto shouldn't be null") ClubRoleSecuredDto clubRoleSecuredDto,
         @NotEmpty(message = "clubRoleApplicationId shouldn't be empty") String clubRoleApplicationId,
         @NotNull(message = "requestStatus shouldn't be empty") RequestStatus requestStatus) {
 }
