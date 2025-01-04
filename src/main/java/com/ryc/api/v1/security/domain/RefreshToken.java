0efa5c88d37cb8bb09c1b02086f36fff99aaabb9
10Hyuna <kimha105@naver.com>
2025-01-05 01:06:08 +0900

    feat: refresh token entity

---
+++ src/main/java/com/ryc/api/v1/security/domain/RefreshToken.java
@@ -0,0 +1,38 @@
+package com.ryc.api.v1.security.domain;
+
+import com.ryc.api.v1.user.domain.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "refresh_token")
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @Column(nullable = false)
+    private LocalDateTime expiration;
+
+    public RefreshToken(User user, String token, LocalDateTime expiration) {
+        this.user = user;
+        this.token = token;
+        this.expiration = expiration;
+    }
+
+}
