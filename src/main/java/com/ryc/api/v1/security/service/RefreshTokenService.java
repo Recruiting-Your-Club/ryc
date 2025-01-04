6ca57bd1c65df8dffe06a90c95ec4d249c347d5e
10Hyuna <kimha105@naver.com>
2025-01-05 02:05:53 +0900

    feat: CRUD service for refresh token

--- src/main/java/com/ryc/api/v1/security/service/RefreshTokenService.java
+++ src/main/java/com/ryc/api/v1/security/service/RefreshTokenService.java
@@ -1,4 +1,31 @@
 package com.ryc.api.v1.security.service;
 
+import com.ryc.api.v1.security.dao.RefreshTokenRepository;
+import com.ryc.api.v1.security.domain.RefreshToken;
+import com.ryc.api.v1.user.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class RefreshTokenService {
+
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public RefreshToken createRefreshToken(User user, String token, long expirationMinutes) {
+        RefreshToken refreshToken = new RefreshToken(user, token, LocalDateTime.now().plusMinutes(expirationMinutes));
+        return refreshTokenRepository.save(refreshToken);
+    }
+
+    public Optional<RefreshToken> findByToken(String token) {
+        return refreshTokenRepository.findByToken(token);
+    }
+
+    public void deleteByToken(String token) {
+        refreshTokenRepository.deleteByToken(token);
+    }
+
 }
