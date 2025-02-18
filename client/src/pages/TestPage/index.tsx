b8f764202164956fb0f6779ae0d3616f294263f6
aiminghee3 <zzuni3423@naver.com>
2025-02-18 22:19:33 +0900

    fix: 컨테이너 안에 버튼 추가

--- client/src/pages/TestPage/index.tsx
+++ client/src/pages/TestPage/index.tsx
@@ -1,13 +1,36 @@
 import React from 'react';
-import { BaseInput } from '@components/Input';
-
+import { BaseInput, PasswordInput } from '@components/Input';
+import { Button } from '@components/Button';
 function TestPage() {
     return (
         <>
             <div>기본 input</div>
-            <BaseInput variant="primary" />
-            <div>밑에만 있는 input</div>
             <BaseInput variant="lined" />
+            <BaseInput variant="primary" helperText="tetaetae" />
+            <PasswordInput label="비밀번호" placeholder="******" />
+            <div css={{ width: '200px', marginBottom: '20px' }}>
+                <PasswordInput label="비밀번호 확인" placeholder="******" />
+            </div>
+            <BaseInput
+                variant="primary"
+                helperText="테스트요"
+                label="테스트제목"
+                endNode={
+                    <Button variant="primary" size="xs">
+                        테스트
+                    </Button>
+                }
+            />
+            <BaseInput
+                variant="lined"
+                helperText="테스트요"
+                label="테스트제목"
+                endNode={
+                    <Button variant="primary" size="xs">
+                        테스트
+                    </Button>
+                }
+            />
         </>
     );
 }
