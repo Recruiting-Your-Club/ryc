bd0afdc4472dcdb2cf06938630cf0d6f0a784ecf
aiminghee3 <zzuni3423@naver.com>
2025-02-20 14:55:12 +0900

    feat: 기본global css 제거

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Global } from '@emotion/react';
+import globalStyles from '@styles/global';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App />);
+root.render(
+    <React.StrictMode>
+        <Global styles={globalStyles} />
+        <App />
+    </React.StrictMode>,
+);
