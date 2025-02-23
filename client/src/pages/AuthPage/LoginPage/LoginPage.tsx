fb810dc02c59d7bb94de5daf1db861cea19c72b0
aiminghee3 <zzuni3423@naver.com>
2025-02-23 17:22:31 +0900

    fix: 로그인 페이지 회원가입 이동 버튼 호버 이벤트 추가

--- client/src/pages/AuthPage/LoginPage/LoginPage.tsx
+++ client/src/pages/AuthPage/LoginPage/LoginPage.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { LoginContainer, LoginBox, inputContainer, buttonContainer, titleContainer } from './LoginPage.style';
 import { Button, BaseInput, PasswordInput } from '@components';
 import { css } from '@emotion/react';
+import { colors } from '@styles/color';
 
 function LoginPage() {
     return (
@@ -18,7 +19,15 @@ function LoginPage() {
                     <Button variant="primary" size="full">
                         로그인
                     </Button>
-                    <Button variant="transparent" size="full">
+                    <Button
+                        variant="transparent"
+                        size="full"
+                        customCss={css`
+                            :hover {
+                                color: ${colors.defaultHover};
+                            }
+                        `}
+                    >
                         회원가입
                     </Button>
                 </div>
