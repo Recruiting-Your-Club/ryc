44522f90f33dca2c31c802bfee72c7c27bd7da6d
aiminghee3 <zzuni3423@naver.com>
2025-02-23 16:29:18 +0900

    chore: component경로추가

--- client/webpack.common.js
+++ client/webpack.common.js
@@ -22,6 +22,7 @@ module.exports = {
         extensions: ['.tsx', '.ts', '.js'],
         alias: {
             '@assets': path.resolve(__dirname, 'src/assets'),
+            '@components': path.resolve(__dirname, 'components/index'),
             '@components': path.resolve(__dirname, 'src/components'),
             '@constants': path.resolve(__dirname, 'src/constants'),
             '@layouts': path.resolve(__dirname, 'src/layouts'),
