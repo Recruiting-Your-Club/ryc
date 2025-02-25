88e6bb4687127d0ebef8e6d7e53e454d7734764c
aiminghee3 <zzuni3423@naver.com>
2025-02-25 20:59:20 +0900

    feat: Tag 컴포넌트 추가

---
+++ client/src/components/Tag/Tag.style.ts
@@ -0,0 +1,27 @@
+import { css } from '@emotion/react';
+import { colors } from '@styles/color';
+
+const baseTag = css`
+    display: inline-block;
+    padding: 0.4rem 0.8rem;
+    border-radius: 1rem;
+    font-size: 1rem;
+    font-weight: 700;
+`;
+export const tag = {
+    primary: css`
+        ${baseTag};
+        background-color: ${colors.inputBorder};
+        color: ${colors.textHelper};
+    `,
+    progress: css`
+        ${baseTag};
+        background-color: ${colors.default};
+        color: ${colors.defaultHover};
+    `,
+    end: css`
+        ${baseTag};
+        background-color: ${colors.red[200]};
+        color: ${colors.red[800]};
+    `,
+};
