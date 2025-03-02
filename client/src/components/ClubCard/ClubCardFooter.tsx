c2c45059f70646d68e24b6d7b60fc5efc45b9302
littleemm <lilysolar@naver.com>
2025-03-02 22:37:24 +0900

    feat: 동아리 태그를 보여주는 부분을 Footer로 분기하여 생성

---
+++ client/src/components/ClubCard/ClubCardFooter.tsx
@@ -0,0 +1,51 @@
+import { Tag } from '@components/Tag';
+import React, { useEffect, useRef, useState } from 'react';
+import { invisibleTag, line, perTag, tagDisplay } from './ClubCard.style';
+
+interface ClubCardFooterProps {
+    tag: string[];
+}
+
+function ClubCardFooter({ tag }: ClubCardFooterProps) {
+    const visibleRef = useRef<HTMLDivElement | null>(null);
+    const invisibleRef = useRef<HTMLDivElement | null>(null);
+    const [visibleTag, setVisibleTag] = useState<string[]>([]);
+
+    useEffect(() => {
+        if (!visibleRef.current || !invisibleRef.current) return;
+
+        const cardWidth = visibleRef.current.clientWidth;
+        let currentTagsWidth = 0;
+        const newVisibleTag: string[] = [];
+
+        const tagElements = Array.from(invisibleRef.current.children) as HTMLDivElement[];
+        tagElements.map((tagElement, index) => {
+            const tagWidth = tagElement.offsetWidth;
+            if (currentTagsWidth + tagWidth <= cardWidth) {
+                newVisibleTag.push(tag[index]);
+                currentTagsWidth += tagWidth;
+            }
+        });
+        setVisibleTag(newVisibleTag);
+    }, [tag]);
+
+    return (
+        <div>
+            <hr css={line} />
+            <div ref={visibleRef} css={tagDisplay}>
+                {visibleTag.map((tag, index) => (
+                    <span key={index} css={perTag}>
+                        <Tag text={tag} variant="primary" />
+                    </span>
+                ))}
+            </div>
+            <div ref={invisibleRef} css={invisibleTag} aria-hidden="true">
+                {tag.map((tag, index) => (
+                    <Tag key={index} text={tag} variant="primary" />
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export { ClubCardFooter };
