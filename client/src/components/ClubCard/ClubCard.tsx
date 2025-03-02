ac7ad25b8cddde76345c66e433c8528419ebc5ec
littleemm <lilysolar@naver.com>
2025-03-02 17:32:31 +0900

    feat: 추후 Link로 페이지 전환을 하기 위해 onClick() 관련 함수 삭제

--- client/src/components/ClubCard/ClubCard.tsx
+++ client/src/components/ClubCard/ClubCard.tsx
@@ -1,6 +1,7 @@
 import Calendar from '@assets/images/Calendar.svg';
 import type { tagVariant } from '@components/Tag';
 import { Tag } from '@components/Tag';
+import { getStatus } from '@utils/compareTime';
 import React from 'react';
 import {
     baseClubCard,
@@ -17,17 +18,14 @@ import {
     statusTagDisplay,
     tagDisplay,
 } from './ClubCard.style';
-export type recruitmentStatus = 'progress' | 'end';
+
 interface ClubCardProps {
     imageURL: string;
-    status: tagVariant;
     name: string;
     type: string;
     startDate: string;
     endDate: string;
     tag: string[];
-    onClick?: () => void;
-    onClickHandler?: () => void;
 }
 
 const RECRUITMENT_STATUS: Record<tagVariant, string> = {
@@ -36,9 +34,11 @@ const RECRUITMENT_STATUS: Record<tagVariant, string> = {
     end: '모집마감',
 };
 
-function ClubCard({ imageURL, status, name, type, startDate, endDate, tag, onClick, onClickHandler }: ClubCardProps) {
+function ClubCard({ imageURL, name, type, startDate, endDate, tag }: ClubCardProps) {
+    const status = getStatus({ startDate, endDate });
+
     return (
-        <div onClick={onClick} onKeyDown={onClickHandler} tabIndex={0} role="button" css={baseClubCard}>
+        <div css={baseClubCard}>
             <div css={baseDisplay}>
                 <img src={imageURL} alt="동아리 대표 이미지" css={clubImage} />
                 <div css={clubTitle}>
