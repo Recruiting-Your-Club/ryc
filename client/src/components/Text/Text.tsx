762afc478e5ae0d0910ddf30cee9fbb61638317a
aiminghee3 <zzuni3423@naver.com>
2025-03-03 17:40:04 +0900

    feat: Typography 설정

--- client/src/components/Text/Text.tsx
+++ client/src/components/Text/Text.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import type { TYPOGRAPHY } from '@styles/theme/typography';
-import type { colors } from '@styles/theme/colors';
-import type { CSSProperties, PropsWithChildren } from 'react';
+import type { CSSProperties, PropsWithChildren, ElementType } from 'react';
+import { textStyle } from './Text.style';
 
 export type TextType = keyof typeof TYPOGRAPHY;
-export type TextColor = keyof typeof colors;
+export type TextColor = 'black' | 'primary' | 'warning' | 'caption';
 
 interface TextProps extends PropsWithChildren {
     type: TextType;
     color?: TextColor;
+    textAlign?: CSSProperties['textAlign'];
+    noWrap?: boolean;
+    cropped?: boolean;
+    as?: ElementType;
 }
-function Text() {
-    return <div>Text</div>;
+function Text({
+    type = 'bodyRegular',
+    color = 'black',
+    textAlign = 'center',
+    noWrap = false,
+    cropped = false,
+    children,
+    as: Tag = 'p',
+}: TextProps) {
+    return <Tag css={textStyle({ type, color, textAlign, noWrap, cropped })}>{children}</Tag>;
 }
 export { Text };
