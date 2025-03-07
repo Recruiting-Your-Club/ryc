84abf8b96f576d6d7a9a900003dfc5253218218a
littleemm <lilysolar@naver.com>
2025-03-07 16:55:14 +0900

    feat: 호버 없는 경우 추가

--- client/src/components/Card/CardRoot.tsx
+++ client/src/components/Card/CardRoot.tsx
@@ -5,18 +5,27 @@ import { baseCard } from './Card.style';
 interface CardRootProps {
     width: string;
     radius: string;
+    hover: boolean;
     customCss?: SerializedStyles;
     children?: React.ReactNode;
     onClick?: () => void;
     onClickHandler?: () => void;
 }
 
-function CardRoot({ width, radius, children, customCss, onClick, onClickHandler }: CardRootProps) {
+function CardRoot({
+    width,
+    radius,
+    hover,
+    customCss,
+    children,
+    onClick,
+    onClickHandler,
+}: CardRootProps) {
     return (
         <div
             onClick={onClick}
             onKeyDown={onClickHandler}
-            css={[baseCard(width, radius), customCss]}
+            css={[baseCard(width, radius, hover), customCss]}
             tabIndex={0}
             role="button"
         >
