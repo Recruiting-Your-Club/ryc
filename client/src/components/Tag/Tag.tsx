213b9dc0e5ca40e90c07638c1f29218981053f60
aiminghee3 <zzuni3423@naver.com>
2025-03-07 14:48:38 +0900

    fix: 컴포넌트 css 커스텀 sx로 수정

--- client/src/components/Tag/Tag.tsx
+++ client/src/components/Tag/Tag.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { tag } from './Tag.style';
+import type { CSSObject } from '@emotion/react';
 
 export type tagVariant = 'primary' | 'progress' | 'end';
 
 export interface TagProps {
     text: string;
     variant: tagVariant;
+    sx?: CSSObject;
 }
-function Tag({ text = 'sample', variant = 'primary' }: TagProps) {
+function Tag({ text = 'sample', variant = 'primary', sx }: TagProps) {
     return (
         <div>
-            <span css={tag[variant]}>{text}</span>
+            <span css={[tag[variant], sx]}>{text}</span>
         </div>
     );
 }
