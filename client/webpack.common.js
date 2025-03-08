aad0253167d7988b9e3f750f30af4bc5936aa9f2
littleemm <lilysolar@naver.com>
2025-03-08 17:44:54 +0900

    feat: 사진 파일 처리 가능하도록 코드 추가

--- client/webpack.common.js
+++ client/webpack.common.js
@@ -17,6 +17,10 @@ module.exports = {
                 use: ['@svgr/webpack'],
             },
             {
+                test: /\.(png|jpe?g|gif)$/,
+                type: 'asset/resource',
+            },
+            {
                 test: /\.(woff|woff2|eot|ttf|otf)$/i,
                 type: 'asset/resource',
                 generator: {
