92a36fd285a3204b1cb1705bf72a4a4e3dd1f689
aiminghee3 <zzuni3423@naver.com>
2025-03-09 18:00:57 +0900

    feat: 스토리북 코드 작성

--- client/src/components/Dialog/BaseDialog/BaseDialog.stories.tsx
+++ client/src/components/Dialog/BaseDialog/BaseDialog.stories.tsx
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
-import type { Meta, StoryObj } from '@storybook/react';
 import { Button } from '@components/Button';
 import { Text } from '@components/Text';
 import XIcon from '@assets/images/xIcon.svg';
 import { Dialog } from '.';
+import type { Meta, StoryObj } from '@storybook/react';
+import theme from '@styles/theme';
+
+type Story = StoryObj<typeof Dialog>;
+type Header = StoryObj<typeof Dialog.Header>;
+type Content = StoryObj<typeof Dialog.Content>;
+type Action = StoryObj<typeof Dialog.Action>;
 
 const meta: Meta<typeof Dialog> = {
-    title: 'Components/Dialog',
+    title: 'Dialog',
     component: Dialog,
     parameters: {
         docs: {
@@ -17,8 +23,181 @@ const meta: Meta<typeof Dialog> = {
     },
 };
 
-export default meta;
-type Story = StoryObj<typeof Dialog>;
+export const EmptyDialog: Story = {
+    render: (args) => {
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        const [isOpen, setIsOpen] = useState(false);
+        const handleClose = () => {
+            setIsOpen(false);
+        };
+        return (
+            <>
+                <Button size="full" variant="primary" onClick={() => setIsOpen(true)}>
+                    아무것도 채우지 않은 Dialog입니다. (각 Dialog는 Docs에서 코드 참고부탁)
+                </Button>
+                <Dialog open={isOpen} handleClose={handleClose} size={args.size} sx={args.sx}>
+                    <Dialog.Header position="center">헤더</Dialog.Header>
+                    <Dialog.Content>컨텐츠</Dialog.Content>
+                    <Dialog.Action>액션</Dialog.Action>
+                </Dialog>
+            </>
+        );
+    },
+    args: {
+        children: 'ReactNode',
+        open: false,
+        size: 'md',
+        handleClose: () => {},
+        sx: {},
+    },
+};
+
+export const DialogHeader: Header = {
+    render: (args) => {
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        const [isOpen, setIsOpen] = useState(false);
+        const handleClose = () => {
+            setIsOpen(false);
+        };
+        return (
+            <>
+                <Button size="full" variant="primary" onClick={() => setIsOpen(true)}>
+                    헤더만 있는 Dialog입니다.
+                </Button>
+                <Dialog open={isOpen} handleClose={handleClose} size="md">
+                    <Dialog.Header
+                        position={args.position}
+                        sx={{ backgroundColor: theme.colors.blue[100], ...args.sx }}
+                        border={args.border}
+                    >
+                        {args.children}
+                    </Dialog.Header>
+                </Dialog>
+            </>
+        );
+    },
+    args: {
+        position: 'start',
+        border: false,
+        children: '헤더를 넣어주세용',
+        sx: {},
+    },
+    argTypes: {
+        position: {
+            options: ['start', 'center', 'end'],
+            control: { type: 'radio' },
+        },
+        border: {
+            options: [true, false],
+            control: { type: 'boolean' },
+        },
+        children: {
+            control: { type: 'text' },
+        },
+        sx: {
+            control: { type: 'object' },
+        },
+    },
+    parameters: {
+        controls: {
+            exclude: ['open', 'size', 'backdrop', 'handleClose'], // Dialog props 제외
+        },
+    },
+};
+
+export const DialogContent: Content = {
+    render: (args) => {
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        const [isOpen, setIsOpen] = useState(false);
+        const handleClose = () => {
+            setIsOpen(false);
+        };
+        return (
+            <>
+                <Button size="full" variant="primary" onClick={() => setIsOpen(true)}>
+                    컨텐츠만 있는 Dialog입니다.
+                </Button>
+                <Dialog open={isOpen} handleClose={handleClose} size="md">
+                    <div>임의값(헤더가 들어갈자리)</div>
+                    <Dialog.Content sx={args.sx}>{args.children}</Dialog.Content>
+                    <div>임의값(액션이 들어갈자리)</div>
+                </Dialog>
+            </>
+        );
+    },
+    args: {
+        children: '컨텐츠를 넣어주세용',
+        sx: {},
+    },
+    argTypes: {
+        children: {
+            control: { type: 'text' },
+        },
+        sx: {
+            control: { type: 'object' },
+        },
+    },
+    parameters: {
+        controls: {
+            exclude: ['open', 'size', 'backdrop', 'handleClose'], // Dialog props 제외
+        },
+    },
+};
+
+export const DialogAction: Action = {
+    render: (args) => {
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        const [isOpen, setIsOpen] = useState(false);
+        const handleClose = () => {
+            setIsOpen(false);
+        };
+        return (
+            <>
+                <Button size="full" variant="primary" onClick={() => setIsOpen(true)}>
+                    액션만 있는 Dialog입니다.
+                </Button>
+                <Dialog open={isOpen} handleClose={handleClose} size="md">
+                    <div>임의값(헤더가 들어갈자리)</div>
+                    <div>임의값(컨텐츠가 들어갈자리)</div>
+                    <Dialog.Action
+                        position={args.position}
+                        sx={{ backgroundColor: theme.colors.blue[100], ...args.sx }}
+                        border={args.border}
+                    >
+                        {args.children}
+                    </Dialog.Action>
+                </Dialog>
+            </>
+        );
+    },
+    args: {
+        position: 'start',
+        border: false,
+        children: '버튼 또는 액션을 만들 요소를 만들어주세용',
+        sx: {},
+    },
+    argTypes: {
+        position: {
+            options: ['start', 'center', 'end'],
+            control: { type: 'radio' },
+        },
+        border: {
+            options: [true, false],
+            control: { type: 'boolean' },
+        },
+        children: {
+            control: { type: 'text' },
+        },
+        sx: {
+            control: { type: 'object' },
+        },
+    },
+    parameters: {
+        controls: {
+            exclude: ['open', 'size', 'backdrop', 'handleClose'], // Dialog props 제외
+        },
+    },
+};
 
 export const BaseDialog: Story = {
     render: (args) => {
@@ -30,12 +209,18 @@ export const BaseDialog: Story = {
         return (
             <>
                 <Button size="full" variant="primary" onClick={() => setIsOpen(true)}>
-                    ㅋ눌러보세용ㅋ
+                    Empty Dialog에 뼈대를 넣은 Dialog입니다.
                 </Button>
-                <Dialog open={isOpen} handleClose={handleClose}>
-                    <Dialog.Header border>
+                <Dialog
+                    open={isOpen}
+                    handleClose={handleClose}
+                    size={args.size}
+                    sx={args.sx}
+                    backdrop={args.backdrop}
+                >
+                    <Dialog.Header sx={{ justifyContent: 'space-between' }}>
                         <Text as="span" type="h4Bold">
-                            전체 이메일 보내기
+                            타이틀
                         </Text>
                         <Button
                             variant="transparent"
@@ -47,8 +232,8 @@ export const BaseDialog: Story = {
                         </Button>
                     </Dialog.Header>
                     <Dialog.Content>
-                        <Text as="span" type="bodyRegular" textAlign="start">
-                            내용물을 채워보아요~~~~~~~~~ 이거 왜 스토리북 Ui 이상하냐고요
+                        <Text type="bodyRegular" textAlign="start">
+                            기본적인 뼈대를 만들어보아요
                         </Text>
                     </Dialog.Content>
                     <Dialog.Action>
@@ -64,7 +249,10 @@ export const BaseDialog: Story = {
         children: 'ReactNode',
         open: false,
         size: 'md',
+        backdrop: true,
         handleClose: () => {},
         sx: {},
     },
 };
+
+export default meta;
---
+++ client/src/components/Dialog/ConfirmDialog/ConfirmDialog.stories.tsx
@@ -0,0 +1,118 @@
+import React, { useState } from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+import { ConfirmDialog } from '.';
+import { Button } from '@components/Button';
+
+const meta: Meta<typeof ConfirmDialog> = {
+    title: 'ConfirmDialog',
+    component: ConfirmDialog,
+    parameters: {
+        docs: {
+            description: {
+                component: 'ConfirmDialog 컴포넌트입니다.',
+            },
+        },
+    },
+};
+
+type Story = StoryObj<typeof ConfirmDialog>;
+
+export const NotificationDialog: Story = {
+    render: (args) => {
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        const [open, setIsOpen] = useState(false);
+        const handleClose = () => {
+            setIsOpen(false);
+        };
+        return (
+            <>
+                <Button size="full" variant="primary" onClick={() => setIsOpen(true)}>
+                    BaseDialog를 기반으로 만든 ConfirmDialog 입니다.
+                </Button>
+                <ConfirmDialog
+                    title={args.title}
+                    dialogSize={args.dialogSize}
+                    content={args.content}
+                    type={args.type}
+                    open={open}
+                    handleClose={handleClose}
+                    titlePosition={args.titlePosition}
+                    contentPosition={args.contentPosition}
+                    actionPosition={args.actionPosition}
+                    buttonSize={args.buttonSize}
+                    closeIcon={args.closeIcon}
+                    cancleButton={args.cancleButton}
+                    actionHandler={args.actionHandler}
+                    backdrop={args.backdrop}
+                />
+            </>
+        );
+    },
+    args: {
+        type: 'text',
+        title: '알림사항',
+        content: '공고 게시 시 면접 날짜는 미정으로 등록됩니다. 면접 날짜를 수정하시겠습니까?',
+        dialogSize: 'sm',
+        open: false,
+        backdrop: false,
+        titlePosition: 'center',
+        contentPosition: 'center',
+        actionPosition: 'end',
+        buttonSize: 'xl',
+        closeIcon: false,
+        cancleButton: false,
+        handleClose: () => {},
+        actionHandler: () => {},
+    },
+};
+
+export const YesOrNoDialog: Story = {
+    render: (args) => {
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+        const [open, setIsOpen] = useState(false);
+        const handleClose = () => {
+            setIsOpen(false);
+        };
+        return (
+            <>
+                <Button size="full" variant="primary" onClick={() => setIsOpen(true)}>
+                    BaseDialog를 기반으로 만든 예 아니오 선택지가 있는 Dialog 입니다.
+                </Button>
+                <ConfirmDialog
+                    title={args.title}
+                    dialogSize={args.dialogSize}
+                    content={args.content}
+                    type={args.type}
+                    open={open}
+                    handleClose={handleClose}
+                    titlePosition={args.titlePosition}
+                    contentPosition={args.contentPosition}
+                    actionPosition={args.actionPosition}
+                    buttonSize={args.buttonSize}
+                    closeIcon={args.closeIcon}
+                    cancleButton={args.cancleButton}
+                    actionHandler={args.actionHandler}
+                    backdrop={args.backdrop}
+                />
+            </>
+        );
+    },
+    args: {
+        type: 'warning',
+        title: '진심으로 할거임?',
+        content: '공고 게시 시 면접 날짜는 미정으로 등록됩니다. 면접 날짜를 수정하시겠습니까?',
+        dialogSize: 'sm',
+        open: false,
+        backdrop: false,
+        titlePosition: 'center',
+        contentPosition: 'center',
+        actionPosition: 'end',
+        buttonSize: 'xl',
+        closeIcon: false,
+        cancleButton: true,
+        handleClose: () => {},
+        actionHandler: () => {},
+    },
+};
+
+export default meta;
