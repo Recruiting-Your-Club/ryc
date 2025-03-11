513e07e28369e18b09b0fa7411fe575791abee75
Dochanii <dmlwhd010@naver.com>
2025-03-11 17:36:19 +0900

    style: ScrollBar 스타일 수정

--- client/src/components/ScrollBar/ScrollBar.style.ts
+++ client/src/components/ScrollBar/ScrollBar.style.ts
@@ -4,28 +4,29 @@ import  theme  from '@styles/theme';
 export const scrollBarStyle = css`
   
   overflow-y : auto; 
+  
 
   // Chrome, edge, safari에서 scrollbar를 적용하려면 -webkit-scrollbar로 적용시켜야한다.
 
   // 전체 스크롤바
-  &::-webkit-scrollbar {
-  width: 6px; 
+ &::-webkit-scrollbar {
+  width: 8px;
 }
 
   // 스크롤바에서 움직이는 부분을 제외한 나머지 부분
-  &::-webkit-scrollbar-track {
+ &::-webkit-scrollbar-track {
   background: ${theme.colors.gray[200]};
-  border-radius: 10px; 
+  border-radius: 10px;
 }
 
   // 스크롤바에서 움직이는 부분
-  &::-webkit-scrollbar-thumb {
-  background:${theme.colors.default}; 
+ &::-webkit-scrollbar-thumb {
+  background:${theme.colors.default}; opacity : 0.2;
   border-radius: 10px;
-  min-height: 150px;
+  min-height: 100px;
+}
+ &::-webkit-scrollbar-thumb:hover{
+  background: ${theme.colors.defaultHover}; opacity : 0.8;
 }
 
-  scrollbar-width: thin; 
-  scrollbar-color: ${theme.colors.default} ${theme.colors.gray[200]};
-  
 `;
