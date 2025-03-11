8b60f5be617444b2d7de281e92a7656bd155963f
Dochanii <dmlwhd010@naver.com>
2025-03-11 17:35:08 +0900

    fix: ScrollBar 컴포넌트 부모 태그의 높이를 기반으로 변경 및 Props 변경

--- client/src/components/ScrollBar/ScrollBar.tsx
+++ client/src/components/ScrollBar/ScrollBar.tsx
@@ -1,21 +1,41 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import type { ReactNode } from 'react';
 import { css } from '@emotion/react';
 import { scrollBarStyle } from './ScrollBar.style';
 
 interface ScrollBarProps {
   children: ReactNode;
-  height?: string;
-  maxHeight?: string;
+  isMouseIn : boolean;
 }
 
-function ScrollBar({ children, height, maxHeight }: ScrollBarProps) {
+function ScrollBar({ children,isMouseIn }: ScrollBarProps) {
+  // 스크롤바에 부착된 부모 태그의 높이 초기설정
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [parentHeight, setParentHeight] = useState<string | undefined>();
+
+  useEffect(() => {
+    if (containerRef.current?.parentElement) {
+      const updateHeight = () => {
+        setParentHeight(`${containerRef.current!.parentElement!.clientHeight}px`); // 높이 계산해서 업데이트
+      };
+
+      updateHeight(); 
+
+      const observer = new ResizeObserver(updateHeight); // 크기가 변경되면 updateHeight 함수를 실행할수있게 observer 설정
+      observer.observe(containerRef.current.parentElement); // 부모 요소가 변경되면 실행할 수 있게
+
+      return () => {
+        observer.disconnect(); // 언 마운트시 observer 중단
+      };
+    }
+  }, []);
+
   return (
     <div
+      ref={containerRef}
       css={css`
         ${scrollBarStyle};
-        height: ${height};
-        max-height: ${maxHeight};
+        height: ${parentHeight ?? "100%"}; 
       `}
     >
       {children}
