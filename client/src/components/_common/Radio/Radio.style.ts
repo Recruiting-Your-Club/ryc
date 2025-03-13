682ce60018542e5e6577cde7232ef43ff0618e08
Dochanii <dmlwhd010@naver.com>
2025-03-13 14:35:05 +0900

    add: Radio 컴포넌트 1차 구현

--- client/src/components/_common/Radio/Radio.style.ts
+++ client/src/components/_common/Radio/Radio.style.ts
@@ -1,1 +1,45 @@
 import { css } from '@emotion/react';
+import theme from '@styles/theme';
+
+export const radioContainer = css`
+    display: flex;
+    justify-content: center;
+    gap: 0.5rem;
+`;
+
+export const s_input = css`
+    display: none;
+`;
+
+export const s_label = css`
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    gap: 0.5rem;
+    font-size: ${theme.typography.helperTextRegular};
+    &:hover {
+        & > span {
+            border: 0.1rem solid ${theme.colors.default};
+        }
+    }
+`;
+
+export const s_radio = (checked: boolean) => css`
+    width: 1.2rem;
+    height: 1.2rem;
+    border-radius: 50%;
+    border: 0.1rem solid ${theme.colors.black};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+
+    &::after {
+        content: '';
+        width: 0.8rem;
+        height: 0.8rem;
+        border-radius: 50%;
+        background-color: ${checked && theme.colors.default};
+        transition: background-color 0.1s ease-in;
+    }
+`;
--- client/src/components/_common/Radio/Radio.tsx
+++ client/src/components/_common/Radio/Radio.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import type { SerializedStyles } from '@emotion/react';
+import { s_radio, s_label, s_input } from './Radio.style';
 
-function Radio() {
-    return <></>;
+interface RadioProps {
+    label: string;
+    name: string;
+    checked: boolean;
+    onChange: () => void;
+    customCSS?: SerializedStyles;
+}
+
+function Radio({ label, name, checked, onChange, customCSS }: RadioProps) {
+    return (
+        <label css={s_label}>
+            <input type="radio" name={name} checked={checked} onChange={onChange} css={s_input} />
+            <span css={s_radio(checked)} />
+            {label}
+        </label>
+    );
 }
 
 export { Radio };
