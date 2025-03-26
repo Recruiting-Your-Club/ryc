f2c6e317bb7b9f51a4ca2bd8e381728b2a669de6
Dochanii <dmlwhd010@naver.com>
2025-03-26 21:12:59 +0900

    feat: FileUpLoader 컴포넌트 초기 설계

--- client/src/components/FileUpLoader/FileUpLoaderButton.tsx
+++ client/src/components/FileUpLoader/FileUpLoaderButton.tsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import { Button } from '@components/Button';
+import React, { useRef } from 'react';
+import { s_fileUpLoaderInput } from './FileUpLoader.style';
+import { useFileLoader } from './FileUpLoaderContext';
 
 function FileUpLoaderButton() {
-    return <></>;
+    const { files, setFiles } = useFileLoader();
+    const fileInputRef = useRef<HTMLInputElement>(null);
+    const handleClick = () => {
+        fileInputRef.current?.click();
+    };
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const selectedFiles = e.target.files;
+        if (!selectedFiles) return;
+
+        const selectedArray = Array.from(selectedFiles);
+        const copyFiles = files ? [...files, ...selectedArray] : selectedArray;
+
+        setFiles(copyFiles);
+    };
+
+    return (
+        <>
+            <input
+                ref={fileInputRef}
+                type="file"
+                multiple
+                onChange={handleChange}
+                css={s_fileUpLoaderInput}
+            />
+            <Button variant="outlined" size="s" onClick={handleClick}>
+                파일선택
+            </Button>
+        </>
+    );
 }
 
 export { FileUpLoaderButton };
