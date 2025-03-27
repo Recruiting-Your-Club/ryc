c3704a9625d61a53e925075d9dc1ab8049ab0f43
Dochanii <dmlwhd010@naver.com>
2025-03-27 18:18:57 +0900

    style: 파일이 있을 때와 없을 때의 디자인 초기 설계

--- client/src/components/FileUpLoader/FileUpLoader.style.ts
+++ client/src/components/FileUpLoader/FileUpLoader.style.ts
@@ -5,12 +5,29 @@ export const s_fileUpLoaderInput = css`
     display: none;
 `;
 
-export const s_fileUpLoaderBox = () => css`
+export const s_fileUpLoaderBox = (hasFile: boolean) => css`
+    display: flex;
+    align-items: center;
+    justify-content: center;
     width: 50rem;
     height: 10rem;
     border: 1px solid ${theme.colors.gray[300]};
     border-radius: 8px;
     margin-top: 0.5rem;
+    ${hasFile &&
+    css`
+        flex-direction: column;
+    `}
 
     overflow: auto;
+`;
+
+export const s_fileUpLoaderHelperText = css`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    ${theme.typography.helperTextRegular}
+`;
+export const s_fileImage = css`
+    margin-right: 0.5rem;
 `;
