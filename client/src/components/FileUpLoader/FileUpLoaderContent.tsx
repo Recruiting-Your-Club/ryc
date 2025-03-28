05b0a067fd42a646aecaac105bf8a9e3044b662a
Dochanii <dmlwhd010@naver.com>
2025-03-28 13:42:07 +0900

    chore: p -> span 태그로 변경

--- client/src/components/FileUpLoader/FileUpLoaderContent.tsx
+++ client/src/components/FileUpLoader/FileUpLoaderContent.tsx
@@ -4,9 +4,9 @@ import { s_fileImage, s_fileUpLoaderHelperText } from './FileUpLoader.style';
 
 function FileUpLoaderContent() {
     return (
-        <p css={s_fileUpLoaderHelperText}>
+        <span css={s_fileUpLoaderHelperText}>
             <File css={s_fileImage} /> 파일을 불러와주세요.
-        </p>
+        </span>
     );
 }
 
