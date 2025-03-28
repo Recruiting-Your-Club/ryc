28543ca97372054b2ceee75a4db58c5a3641fc99
Dochanii <dmlwhd010@naver.com>
2025-03-28 18:01:43 +0900

    add: 파일이 있을 때와 없을 때의 컴포넌트 스타일 추가

--- client/src/components/FileUpLoader/FileUpLoader.style.ts
+++ client/src/components/FileUpLoader/FileUpLoader.style.ts
@@ -6,22 +6,27 @@ export const s_fileUpLoaderInput = css`
 `;
 
 export const s_fileUpLoaderBox = (hasFile: boolean) => css`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 50rem;
-    height: 10rem;
+    width: 60rem;
     border: 1px solid ${theme.colors.gray[300]};
     border-radius: 8px;
     margin-top: 0.5rem;
-    ${hasFile &&
-    css`
-        flex-direction: column;
-    `}
-
+    padding: 0;
+    ${theme.typography.helperTextRegular};
     overflow: auto;
-`;
 
+    ${hasFile
+        ? css`
+              display: block;
+              max-height: 10rem;
+          `
+        : css`
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              text-align: center;
+              height: 5rem;
+          `}
+`;
 export const s_fileUpLoaderHelperText = css`
     display: flex;
     align-items: center;
@@ -30,4 +35,43 @@ export const s_fileUpLoaderHelperText = css`
 `;
 export const s_fileImage = css`
     margin-right: 0.5rem;
+`;
+
+export const s_fileTable = css`
+    width: 100%;
+    text-align: left;
+`;
+
+export const s_fileTableHead = css`
+    background-color: ${theme.colors.gray[100]};
+    color: ${theme.colors.gray[700]};
+    ${theme.typography.subCaptionRegular};
+    th {
+        padding: 0.5rem;
+    }
+`;
+
+export const s_fileTableBody = css`
+    td {
+        padding: 0.75rem;
+        border-top: 1px solid ${theme.colors.gray[200]};
+        word-break: break-all;
+    }
+`;
+
+export const s_fileNameWithIcon = css`
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+`;
+
+export const s_fileIcon = css`
+    width: 1.25rem;
+    height: 1.25rem;
+`;
+export const s_xIcon = css`
+    width: 1.25rem;
+    height: 1.25rem;
+
+    cursor: pointer;
 `;
