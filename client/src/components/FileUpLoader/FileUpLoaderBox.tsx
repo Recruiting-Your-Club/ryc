de428a0786c5f0b19c26e4648157321815e12501
Dochanii <dmlwhd010@naver.com>
2025-03-28 18:03:20 +0900

    refactor: FileUpLoaderBox 코드 구조 변경

--- client/src/components/FileUpLoader/FileUpLoaderBox.tsx
+++ client/src/components/FileUpLoader/FileUpLoaderBox.tsx
@@ -1,28 +1,14 @@
 import React from 'react';
 import { s_fileUpLoaderBox } from './FileUpLoader.style';
 import { useFileUpLoaderContext } from './FileUpLoaderContext';
-import { FileUpLoaderContent } from './FileUpLoaderContent';
+import { FileUpLoaderEmptyView } from './FileUpLoaderEmptyView';
+import { FileUpLoaderItemView } from './FileUpLoaderItemView';
 
 function FileUpLoaderBox() {
     const { files, hasFile } = useFileUpLoaderContext();
-
     return (
         <div css={s_fileUpLoaderBox(hasFile)}>
-            {files?.length === 0 ? (
-                <FileUpLoaderContent />
-            ) : (
-                <>
-                    {/* 파일이 있을 때 UI */}
-                    <div style={{ fontWeight: 'bold' }}>선택된 파일들:</div>
-                    <ul>
-                        {files?.map((file, idx) => (
-                            <li key={idx}>
-                                {file.name} ({file.size} bytes)
-                            </li>
-                        ))}
-                    </ul>
-                </>
-            )}
+            {files?.length === 0 ? <FileUpLoaderEmptyView /> : <FileUpLoaderItemView />}
         </div>
     );
 }
