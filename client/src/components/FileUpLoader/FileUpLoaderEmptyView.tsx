4c1cc1b6a913ea1e3ecab8b36dd6ce2d25fb6bab
Dochanii <dmlwhd010@naver.com>
2025-03-31 16:58:05 +0900

    add: EmptyView에서 onClick 로직 추가

--- client/src/components/FileUpLoader/FileUpLoaderEmptyView.tsx
+++ client/src/components/FileUpLoader/FileUpLoaderEmptyView.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import File from '@assets/images/File.svg';
-import { s_fileImage, s_fileUpLoaderHelperText } from './FileUpLoader.style';
+import { s_fileImage, s_fileUpLoaderEmptyView } from './FileUpLoader.style';
+import { useFileUpLoaderContext } from './FileUpLoaderContext';
+import type { KeyboardEvent } from 'react';
 
 function FileUpLoaderEmptyView() {
+    const { handleClickInput } = useFileUpLoaderContext();
+
+    const handleKeyDown = (event: KeyboardEvent<HTMLDivElement>) => {
+        if (event.key === 'Enter' || event.key === ' ') {
+            handleClickInput();
+        }
+    };
     return (
-        <span css={s_fileUpLoaderHelperText}>
+        <span
+            role="button"
+            css={s_fileUpLoaderEmptyView}
+            onClick={handleClickInput}
+            tabIndex={0}
+            onKeyDown={handleKeyDown}
+        >
             <File css={s_fileImage} />
             파일을 불러와주세요.
         </span>
