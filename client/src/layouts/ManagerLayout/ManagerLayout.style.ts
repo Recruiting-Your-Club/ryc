2843fe6ab1cc6d8df688f3380f8dda596f439e05
aiminghee3 <zzuni3423@naver.com>
2025-04-01 11:49:25 +0900

    fix: managerLayout 수정

--- client/src/layouts/ManagerLayout/ManagerLayout.style.ts
+++ client/src/layouts/ManagerLayout/ManagerLayout.style.ts
@@ -3,8 +3,7 @@ import { css } from '@emotion/react';
 export const ManagerLayoutCss = css`
     display: flex;
     width: 100vw;
-
+    height: 100%;
     min-width: 32rem;
     background-color: #fff;
-    padding-top: 6rem; /* 헤더 높이만큼 패딩 추가 */
 `;
