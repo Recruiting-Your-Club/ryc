423fdef17fc011d01674846ad78d0d605b24bfff
Dochanii <dmlwhd010@naver.com>
2025-04-02 14:45:47 +0900

    refactor: box선택 시 클릭 가능하도록 로직 이동

--- client/src/components/FileUpLoader/FileUpLoaderBox.tsx
+++ client/src/components/FileUpLoader/FileUpLoaderBox.tsx
@@ -3,22 +3,33 @@ import { s_fileUpLoaderBox } from './FileUpLoader.style';
 import { useFileUpLoaderContext } from './FileUpLoaderContext';
 import { FileUpLoaderEmptyView } from './FileUpLoaderEmptyView';
 import { FileUpLoaderItemView } from './FileUpLoaderItemView';
+import type { KeyboardEvent } from 'react';
 
 function FileUpLoaderBox() {
-    const { files, hasFile, isActive, handleDragStart, handleDragEnd, handleDrop } =
+    const { files, hasFile, handleDragStart, handleDragEnd, handleDrop, handleClickButton } =
         useFileUpLoaderContext();
 
     const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
         e.preventDefault();
     };
 
+    const handleKeyDown = (event: KeyboardEvent<HTMLDivElement>) => {
+        if (event.key === 'Enter' || event.key === ' ') {
+            handleClickButton();
+        }
+    };
+
     return (
         <div
-            css={s_fileUpLoaderBox(hasFile, isActive)}
+            role="button"
+            css={s_fileUpLoaderBox(hasFile)}
             onDragEnter={handleDragStart}
             onDragLeave={handleDragEnd}
             onDragOver={handleDragOver}
             onDrop={handleDrop}
+            onKeyDown={handleKeyDown}
+            onClick={handleClickButton}
+            tabIndex={0}
         >
             {files?.length === 0 ? <FileUpLoaderEmptyView /> : <FileUpLoaderItemView />}
         </div>
--- client/src/components/FileUpLoader/FileUpLoaderEmptyView.tsx
+++ client/src/components/FileUpLoader/FileUpLoaderEmptyView.tsx
@@ -1,25 +1,10 @@
 import React from 'react';
 import File from '@assets/images/File.svg';
 import { s_fileImage, s_fileUpLoaderEmptyView } from './FileUpLoader.style';
-import { useFileUpLoaderContext } from './FileUpLoaderContext';
-import type { KeyboardEvent } from 'react';
 
 function FileUpLoaderEmptyView() {
-    const { handleClickButton } = useFileUpLoaderContext();
-
-    const handleKeyDown = (event: KeyboardEvent<HTMLDivElement>) => {
-        if (event.key === 'Enter' || event.key === ' ') {
-            handleClickButton();
-        }
-    };
     return (
-        <span
-            role="button"
-            css={s_fileUpLoaderEmptyView}
-            onClick={handleClickButton}
-            tabIndex={0}
-            onKeyDown={handleKeyDown}
-        >
+        <span css={s_fileUpLoaderEmptyView}>
             <File css={s_fileImage} />
             파일을 불러와주세요.
         </span>
