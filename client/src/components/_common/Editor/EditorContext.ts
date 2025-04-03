9aa851df3558160e9df4556d63006d8ab3fba151
littleemm <lilysolar@naver.com>
2025-04-04 00:52:08 +0900

    feat: editor context 생성

---
+++ client/src/components/_common/Editor/EditorContext.ts
@@ -0,0 +1,20 @@
+import { createContext, useContext } from 'react';
+import type { Format } from './EditorToolbar';
+
+interface EditorContextType {
+    format?: Format;
+}
+
+export const EditorContext = createContext<EditorContextType | undefined>(undefined);
+
+export const useEditorContext = () => {
+    const context = useContext(EditorContext);
+
+    if (context === undefined) {
+        throw new Error(
+            'useEditorContext는 useEditorContext.Provider 내부에서만 사용할 수 있습니다.',
+        );
+    }
+
+    return context;
+};
