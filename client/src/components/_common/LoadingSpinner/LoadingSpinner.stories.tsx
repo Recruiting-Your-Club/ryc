63e8b482d7057e74bc05ac96d2ab1cc54d197636
aiminghee3 <zzuni3423@naver.com>
2025-04-04 16:43:23 +0900

    feat: 스토리북 작성

---
+++ client/src/components/_common/LoadingSpinner/LoadingSpinner.stories.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+import { SpinSpinner, PulseSpinner } from '.';
+
+const meta: Meta<typeof SpinSpinner> = {
+    title: '스피너',
+    component: SpinSpinner,
+    parameters: {
+        docs: {
+            description: {
+                component: '로딩스피너 컴포넌트입니다.',
+            },
+        },
+    },
+};
+
+export default meta;
+
+export const Spin: StoryObj<typeof SpinSpinner> = {
+    args: {},
+};
+
+export const Pulse: StoryObj<typeof PulseSpinner> = {
+    render: (args) => <PulseSpinner color="black" />,
+    args: {},
+};
