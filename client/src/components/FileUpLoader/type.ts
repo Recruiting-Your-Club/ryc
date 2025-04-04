81b6c93495d5ef0bb09b4849b39320684c922fae
Dochanii <dmlwhd010@naver.com>
2025-04-04 15:25:52 +0900

    refactor: headerItems -> type.ts로 이동

--- client/src/components/FileUpLoader/type.ts
+++ client/src/components/FileUpLoader/type.ts
@@ -66,3 +66,10 @@ export enum ImageExtension {
     GIF = 'gif',
     WEBP = 'webp',
 }
+
+export const headerItems = [
+    { label: '파일명', align: 'left' },
+    { label: '최종 수정 일시', align: 'center' },
+    { label: '크기', align: 'center' },
+    { label: '파일 유형', align: 'center' },
+] as const;
