f1144a32e3b108a0172fd9d9338c6d93f50057ab
Dochanii <dmlwhd010@naver.com>
2025-04-04 15:25:52 +0900

    refactor: headerItems -> type.ts로 이동

--- client/src/components/FileUpLoader/FileUpLoaderItemHeader.tsx
+++ client/src/components/FileUpLoader/FileUpLoaderItemHeader.tsx
@@ -3,13 +3,7 @@ import { useFileUpLoaderContext } from './FileUpLoaderContext';
 import XIcon from '@assets/images/gray_xicon.svg';
 import { s_fileHeader, s_fileRow, s_xIcon } from './FileUpLoader.style';
 import { FileUpLoaderItemCell } from './FileUpLoaderItemCell';
-
-const headerItems = [
-    { label: '파일명', align: 'left' },
-    { label: '최종 수정 일시', align: 'center' },
-    { label: '크기', align: 'center' },
-    { label: '파일 유형', align: 'center' },
-] as const;
+import { headerItems } from './type';
 
 function FileUpLoaderItemHeader() {
     const { handleDeleteEntire } = useFileUpLoaderContext();
