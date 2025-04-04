3bf9bef621c4777d5f10c1948e417537bd0d8752
aiminghee3 <zzuni3423@naver.com>
2025-04-04 16:16:00 +0900

    feat: 로딩스피너 구현

---
+++ client/src/components/_common/LoadingSpinner/LoadingSpinner.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import {
+    loadingSpinnerContainer,
+    dotContainer,
+    spinSpinnerContainer,
+} from './LoadingSpinner.style';
+import type { CSSObject } from '@emotion/react';
+import theme from '@styles/theme';
+
+interface LoadingProps {
+    size?: number;
+    color?: string;
+    sx?: CSSObject;
+}
+
+export interface SpinnerProps extends LoadingProps {
+    backgroundColor?: string;
+}
+
+function SpinSpinner(props: SpinnerProps) {
+    const defaultProps = {
+        size: 30,
+        backgroundColor: 'gray',
+        color: theme.colors.defaultHover,
+        sx: {},
+    };
+    const mergeProps = { ...defaultProps, ...props };
+    return (
+        <>
+            <div css={[spinSpinnerContainer(mergeProps), mergeProps.sx]} />
+        </>
+    );
+}
+
+function PulseSpinner(props: LoadingProps) {
+    const defaultProps = {
+        size: 8,
+        color: theme.colors.white,
+        sx: {},
+    };
+    const mergeProps = { ...defaultProps, ...props };
+    return (
+        <>
+            <div css={loadingSpinnerContainer}>
+                <div css={[dotContainer(0, mergeProps), mergeProps.sx]} />
+                <div css={[dotContainer(0.2, mergeProps), mergeProps.sx]} />
+                <div css={[dotContainer(0.4, mergeProps), mergeProps.sx]} />
+            </div>
+        </>
+    );
+}
+
+export { PulseSpinner, SpinSpinner };
