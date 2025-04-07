d415a0ca1cdb11e5233ca026512681fd6724efac
Dochanii <dmlwhd010@naver.com>
2025-04-07 15:17:20 +0900

    chore:  fileImage -> fileUpLoaderIcon으로 이름 변경 및 적용

--- client/src/components/FileUpLoader/FileUpLoaderEmptyView.tsx
+++ client/src/components/FileUpLoader/FileUpLoaderEmptyView.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import File from '@assets/images/File.svg';
-import { s_fileImage, s_fileUpLoaderEmptyView } from './FileUpLoader.style';
+import { s_fileUpLoaderIcon, s_fileUpLoaderEmptyView } from './FileUpLoader.style';
 
 function FileUpLoaderEmptyView() {
     return (
         <span css={s_fileUpLoaderEmptyView}>
-            <File css={s_fileImage} />
+            <File css={s_fileUpLoaderIcon} />
             파일을 불러와주세요.
         </span>
     );
