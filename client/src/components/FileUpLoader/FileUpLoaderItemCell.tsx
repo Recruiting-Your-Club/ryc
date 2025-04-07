89d26af049b0af4f5debb22000e6d4b9d49e0889
Dochanii <dmlwhd010@naver.com>
2025-04-07 16:39:41 +0900

    refactor: 조건에 따른 cell style 지정하는 로직 삼항연산자 -> if-else로 변경

--- client/src/components/FileUpLoader/FileUpLoaderItemCell.tsx
+++ client/src/components/FileUpLoader/FileUpLoaderItemCell.tsx
@@ -5,14 +5,21 @@ import type { FileUpLoaderItemCellProps } from './type';
 function FileUpLoaderItemCell({
     children,
     align = 'center',
-    isNameCell = false,
+    isFileNameCell = false,
     isHeader = false,
 }: FileUpLoaderItemCellProps) {
-    const baseStyle = isHeader
-        ? s_fileHeaderText(align)
-        : isNameCell
-          ? s_fileNameWithIcon
-          : s_fileMetaItem;
+    // header이면 s_fileHeaderText 적용,
+    // header가 아니면 file명이 있는 셀이 true -> file명과 아이콘을 같이 띄우는 스타일 적용
+    // 다 아니면 파일명을 제외한 파일 정보를 나타내는 metaItem 스타일 적용
+    let baseStyle;
+
+    if (isHeader) {
+        baseStyle = s_fileHeaderText(align);
+    } else if (isFileNameCell) {
+        baseStyle = s_fileNameWithIcon;
+    } else {
+        baseStyle = s_fileMetaItem;
+    }
 
     return <div css={baseStyle}>{children}</div>;
 }
