59e81dd5e1ad0390851dda498d1571d8099c68f2
Dochanii <dmlwhd010@naver.com>
2025-04-10 15:18:23 +0900

    fix: style에서 width 인자 위치 변경

--- client/src/components/_common/TextArea/TextArea.style.ts
+++ client/src/components/_common/TextArea/TextArea.style.ts
@@ -1,35 +1,39 @@
 import { css } from '@emotion/react';
 import theme from '@styles/theme';
+import type { TextAreaSize } from './type';
+import { textAreaSize } from './type';
 
-export const s_textAreaWrapper = css`
+export const s_textAreaSize = (size: TextAreaSize) => css`
+    height: ${textAreaSize[size].height};
+    padding: ${textAreaSize[size].padding};
+    ${textAreaSize[size].typography};
+`;
+
+export const s_textAreaWrapper = (width: string) => css`
+    width: ${width};
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
     margin: 1rem;
+    position: relative;
 `;
-export const s_textAreaLabelWrapper = (width: string) => css`
+export const s_textAreaLabelWrapper = css`
     display: flex;
     align-items: center;
     justify-content: space-between;
-    width: ${width};
+    width: 100%;
 `;
 
-export const s_textArea = (
-    height: number,
-    width: string,
-    error?: boolean,
-    disabled?: boolean,
-) => css`
+export const s_textArea = (size: TextAreaSize, error?: boolean, disabled?: boolean) => css`
     resize: none;
-    width: ${width};
-    height: ${height}rem;
     border-radius: 8px;
     border: 1px solid ${theme.colors.gray[500]};
-    padding: 1rem;
-    ${theme.typography.bodySemibold};
     transition: border-color 0.3s ease-in-out;
     outline: none;
+    width: 100%;
+
+    ${s_textAreaSize(size)}
 
     &:focus {
         border-color: ${error ? theme.colors.red[800] : theme.colors.default};
@@ -47,23 +51,25 @@ export const s_textArea = (
         background-color: ${theme.colors.gray[100]};
     `}
 `;
-
 export const s_label = css`
     margin-bottom: 0.5rem;
     ${theme.typography.h3Bold};
 `;
 
 export const s_errorText = css`
+    position: absolute;
+    right: 0;
+    top: 1.5rem;
     color: ${theme.colors.red[800]};
     margin-top: 0.25rem;
-    ${theme.typography.helperTextLight};
+    ${theme.typography.subCaptionLight};
 `;
 
-export const s_charCount = (width: string) => css`
-    width: ${width};
+export const s_charCount = css`
+    width: 100%;
     display: flex;
     justify-content: flex-end;
     margin-top: 0.25rem;
-    ${theme.typography.helperTextLight};
+    ${theme.typography.subCaptionLight};
     color: ${theme.colors.gray[600]};
 `;
