9ad5e5d1c05d58121144f0aedb04b445229505d6
Dochanii <dmlwhd010@naver.com>
2025-04-14 17:16:57 +0900

    fix: 코드 리뷰 기반 수정 let 사용 x

--- client/src/components/FileUpLoader/FileUpLoaderItemCell.tsx
+++ client/src/components/FileUpLoader/FileUpLoaderItemCell.tsx
@@ -11,15 +11,11 @@ function FileUpLoaderItemCell({
     // header이면 s_fileHeaderText 적용,
     // header가 아니면 file명이 있는 셀이 true -> file명과 아이콘을 같이 띄우는 스타일 적용
     // 다 아니면 파일명을 제외한 파일 정보를 나타내는 metaItem 스타일 적용
-    let baseStyle;
-
-    if (isHeader) {
-        baseStyle = s_fileHeaderText(align);
-    } else if (isFileNameCell) {
-        baseStyle = s_fileNameWithIcon;
-    } else {
-        baseStyle = s_fileMetaItem;
-    }
+    const baseStyle = isHeader
+        ? s_fileHeaderText(align)
+        : isFileNameCell
+          ? s_fileNameWithIcon
+          : s_fileMetaItem;
 
     return <div css={baseStyle}>{children}</div>;
 }
