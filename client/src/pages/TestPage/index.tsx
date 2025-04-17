162ed3bd1353deb9c4001ffffa1622273c3b9f17
Dochanii <dmlwhd010@naver.com>
2025-04-17 13:46:23 +0900

    fix: 코드 리뷰 기반 수정 TestPage

--- client/src/pages/TestPage/index.tsx
+++ client/src/pages/TestPage/index.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import type { ChangeEvent } from 'react';
+import React, { TextareaHTMLAttributes, useState } from 'react';
 import { Button } from '@components/_common/Button';
 import { Text } from '@components/_common/Text';
 import { ClubCard } from '@components';
@@ -6,6 +7,9 @@ import { Input } from '@components/_common/Input';
 import { TextArea } from '@components/_common/TextArea/TextArea';
 function TestPage() {
     const [text, setText] = useState('');
+    const onChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        setText(e.target.value);
+    };
     return (
         <>
             <div>
@@ -18,17 +22,19 @@ function TestPage() {
                     하이요
                 </Text>
                 <Input />
-                <TextArea size="sm" label="간단한 메모" />
-                <TextArea size="md" label="중간 크기" />
-                <TextArea size="lg" label="긴 내용 입력용" />
+                <TextArea size="xs" variant="flushed" value={text} onChange={onChange} />
+                <TextArea size="sm" variant="subtle" value={text} onChange={onChange} />
+                <TextArea size="md" variant="outline" value={text} onChange={onChange} />
+                <TextArea size="lg" maxLength={300} value={text} onChange={onChange} />
                 <TextArea
-                    size="md"
-                    label="제목"
-                    error={true}
-                    charCount
+                    size="xl"
+                    error={text.length < 10}
                     maxLength={300}
-                    errorText="gggg"
+                    errorText="10자 이상 입력해주세요."
+                    value={text}
+                    onChange={onChange}
                 />
+                <TextArea size="full" value={text} onChange={onChange} />
                 <div style={{ display: 'flex', gap: '1rem', marginTop: '10rem' }}>
                     <ClubCard
                         imageURL="https://avatars.githubusercontent.com/u/176916276?s=400&u=48a14c04c14ce04adfebdf9290c6e36492b0994d&v=4"
