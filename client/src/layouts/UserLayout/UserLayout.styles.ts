8f703b2f9582c9e5c9037cfab48d63d192a4fce8
aiminghee3 <zzuni3423@naver.com>
2025-04-23 02:25:54 +0900

    fix: width 기본 반응형 레이아웃에서 수정할 수 있도록 변경

--- client/src/layouts/UserLayout/UserLayout.styles.ts
+++ client/src/layouts/UserLayout/UserLayout.styles.ts
@@ -1,11 +1,30 @@
 import { css } from '@emotion/react';
+import theme from '@styles/theme';
 
 export const UserLayoutCss = css`
     display: flex;
     flex-direction: column;
-    width: 100%;
     height: 100%;
+    width: 100%;
     min-width: 32rem;
     background-color: #fff;
-    padding-top: 6rem; /* 헤더 높이만큼 패딩 추가 */
+    padding: 6rem 6rem 0rem 6rem;
+    // 840px ~ 1200px
+    @media (max-width: ${theme.breakpoint.desktop}) {
+        width: 90rem;
+        padding-left: 3rem;
+        padding-right: 3rem;
+    }
+    // 600px ~ 840px
+    @media (max-width: ${theme.breakpoint.tablet}) {
+        width: 70rem;
+        padding-left: 1rem;
+        padding-right: 1rem;
+    }
+    // 0px ~ 600px
+    @media (max-width: ${theme.breakpoint.mobile}) {
+        width: 35rem;
+        padding-left: 1rem;
+        padding-right: 1rem;
+    }
 `;
