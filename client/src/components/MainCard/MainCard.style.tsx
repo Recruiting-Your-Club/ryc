c2c9ef0b44ffdf94e5c2f297170e12376fae4403
aiminghee3 <zzuni3423@naver.com>
2025-04-23 04:07:02 +0900

    fix: Card 호버 css 추가

--- client/src/components/MainCard/MainCard.style.tsx
+++ client/src/components/MainCard/MainCard.style.tsx
@@ -5,11 +5,17 @@ export const cardContainer = css`
     display: flex;
     flex-direction: column;
     justify-content: space-between;
-    width: 35rem;
+    width: 33rem;
+    height: 16rem;
     padding: 1rem 2rem;
     gap: 1.5rem;
     border: 0.5px solid ${theme.colors.gray[300]};
+    //box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
     border-radius: 10px;
+    transition: background-color 0.1s ease;
+    :hover {
+        background-color: ${theme.colors.gray[100]};
+    }
 `;
 
 export const cardHeaderContainer = css`
