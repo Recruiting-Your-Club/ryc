5aad0003ae17e8fd87ebf245b30f5cd859b40050
aiminghee3 <zzuni3423@naver.com>
2025-04-22 23:20:27 +0900

    fix: Header, 레이아웃 반응형에 맞게 수정

--- client/src/layouts/UserLayout/UserLayout.tsx
+++ client/src/layouts/UserLayout/UserLayout.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { UserLayoutCss } from './UserLayout.styles';
 import { Header } from '@components/Header';
 import { Outlet } from 'react-router-dom';
-
+import { Divider } from '@components';
 function UserLayout() {
     return (
         <>
