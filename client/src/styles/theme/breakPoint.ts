e217213babf1abccc46c358e058f8b4c3a9e2b41
aiminghee3 <zzuni3423@naver.com>
2025-04-29 19:28:45 +0900

    fix: tabletMini, mobile 브레이크 포인트 픽셀 수정

--- client/src/styles/theme/breakPoint.ts
+++ client/src/styles/theme/breakPoint.ts
@@ -1,7 +1,7 @@
 export const BREAKPOINT = {
     compact: '0px',
-    mobile: '600px',
-    tabletMini: '840px',
+    mobile: '480px',
+    tabletMini: '768px',
     tablet: '1024px',
     desktop: '1200px',
 };
