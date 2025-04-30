c253bef1be3303c4c231c8f2fc3370e3df91db56
Dochanii <dmlwhd010@naver.com>
2025-04-30 18:58:30 +0900

    fix: 코드 리뷰 기반 수정 min-height: 100%추가

--- client/src/layouts/UserLayout/UserLayout.styles.ts
+++ client/src/layouts/UserLayout/UserLayout.styles.ts
@@ -4,6 +4,7 @@ export const UserLayoutCss = css`
     display: flex;
     flex-direction: column;
     height: 100%;
+    min-height: 100%;
     width: 115rem;
     min-width: 32rem;
     background-color: #fff;
