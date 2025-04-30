1c3cc472dfb0485a7698ccd6ed6ec012f71b2669
Dochanii <dmlwhd010@naver.com>
2025-04-30 18:58:00 +0900

    fix: 코드 리뷰 기반 수정 UserLayoutCss feature/issue-162와 동일시

--- client/src/layouts/UserLayout/UserLayout.styles.ts
+++ client/src/layouts/UserLayout/UserLayout.styles.ts
@@ -3,16 +3,9 @@ import { css } from '@emotion/react';
 export const UserLayoutCss = css`
     display: flex;
     flex-direction: column;
-    min-height: 100dvh;
-    width: 100%;
+    height: 100%;
+    width: 115rem;
     min-width: 32rem;
     background-color: #fff;
-
-    main {
-        flex: 1;
-        padding-top: 6rem; /* 헤더 높이만큼 패딩 추가 */
-        max-width: 75rem;
-        margin: 0 auto;
-        width: 100%;
-    }
+    padding: 6rem 6rem 0rem 6rem;
 `;
