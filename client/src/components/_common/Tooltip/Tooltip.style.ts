9fe80b8b2b53cbdd229f8a8a1d14dc2f186eb4b4
Dochanii <dmlwhd010@naver.com>
2025-05-01 18:28:30 +0900

    add: top, bottom, left, right 방향 추가

--- client/src/components/_common/Tooltip/Tooltip.style.ts
+++ client/src/components/_common/Tooltip/Tooltip.style.ts
@@ -1,19 +1,45 @@
 import { css } from '@emotion/react';
+import type { Direction } from './Tooltip';
+import theme from '@styles/theme';
 
 export const tooltipContainter = css`
     position: relative;
     display: inline-block;
 `;
 
-export const tooltipStyle = css`
-    position: absolute;
-    bottom: 100%;
-    left: 50%;
-    transform: translateX(-50%);
-    background-color: #333;
-    color: #fff;
-    padding: 6px 14px;
-    border-radius: 10px;
-    white-space: nowrap;
-    z-index: 10;
-`;
+export const tooltipStyle = (direction: Direction) => {
+    const positionStyles: Record<Direction, ReturnType<typeof css>> = {
+        top: css`
+            bottom: 100%;
+            left: 50%;
+            margin-bottom: 0.3rem;
+        `,
+        bottom: css`
+            top: 100%;
+            left: 50%;
+            margin-top: 0.3rem;
+        `,
+        left: css`
+            top: 50%;
+            right: 100%;
+            transform: translateY(-50%);
+            margin-right: 0.3rem;
+        `,
+        right: css`
+            top: 50%;
+            left: 100%;
+            transform: translateY(-50%);
+            margin-left: 0.3rem;
+        `,
+    };
+    return css`
+        position: absolute;
+        background-color: ${theme.colors.black};
+        color: ${theme.colors.white};
+        padding: 0.8rem 1rem;
+        border-radius: 5px;
+        white-space: nowrap;
+        ${theme.typography.subCaptionRegular};
+        ${positionStyles[direction]};
+    `;
+};
--- client/src/components/_common/Tooltip/Tooltip.tsx
+++ client/src/components/_common/Tooltip/Tooltip.tsx
@@ -1,13 +1,17 @@
 import type { ReactNode } from 'react';
 import React, { useState } from 'react';
 import { tooltipContainter, tooltipStyle } from './Tooltip.style';
+import type { CSSObject } from '@emotion/react';
+export type Direction = 'top' | 'bottom' | 'left' | 'right';
 
 export interface TooltipProps {
     content: string;
+    direction?: Direction;
     children: ReactNode;
+    sx?: CSSObject;
 }
 
-function Tooltip({ content, children }: TooltipProps) {
+function Tooltip({ content, direction = 'bottom', children, sx }: TooltipProps) {
     const [visible, setVisible] = useState<boolean>();
 
     const showTooltip = () => {
@@ -19,7 +23,7 @@ function Tooltip({ content, children }: TooltipProps) {
     return (
         <div css={tooltipContainter} onMouseEnter={showTooltip} onMouseLeave={hideTooltip}>
             {children}
-            {visible && <div css={tooltipStyle}>{content}</div>}
+            {visible && <div css={[tooltipStyle(direction), sx]}>{content}</div>}
         </div>
     );
 }
