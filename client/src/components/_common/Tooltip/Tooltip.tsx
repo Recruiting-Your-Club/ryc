a1ba8e490614ecc3f9ed135050b28af8d0e2034c
Dochanii <dmlwhd010@naver.com>
2025-05-01 18:47:53 +0900

    add: delay 추가

--- client/src/components/_common/Tooltip/Tooltip.tsx
+++ client/src/components/_common/Tooltip/Tooltip.tsx
@@ -1,5 +1,5 @@
 import type { ReactNode } from 'react';
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { tooltipContainter, tooltipStyle } from './Tooltip.style';
 import type { CSSObject } from '@emotion/react';
 export type Direction = 'top' | 'bottom' | 'left' | 'right';
@@ -8,16 +8,21 @@ export interface TooltipProps {
     content: string;
     direction?: Direction;
     children: ReactNode;
+    delay?: number;
     sx?: CSSObject;
 }
 
-function Tooltip({ content, direction = 'bottom', children, sx }: TooltipProps) {
+function Tooltip({ content, direction = 'bottom', children, delay = 300, sx }: TooltipProps) {
     const [visible, setVisible] = useState<boolean>();
+    const timeoutRef = useRef<NodeJS.Timeout>();
 
     const showTooltip = () => {
-        setVisible(true);
+        timeoutRef.current = setTimeout(() => {
+            setVisible(true);
+        }, delay);
     };
     const hideTooltip = () => {
+        if (timeoutRef.current) clearTimeout(timeoutRef.current);
         setVisible(false);
     };
     return (
