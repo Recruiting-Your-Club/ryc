2a221a0219f84e8e7f9cd362dfba1ab48ff3f048
Dochanii <dmlwhd010@naver.com>
2025-05-02 19:45:34 +0900

    fix: 코드 리뷰 기반 수정 스타일 구조, 리턴 타입 변경

--- client/src/components/_common/Tooltip/Tooltip.style.ts
+++ client/src/components/_common/Tooltip/Tooltip.style.ts
@@ -1,4 +1,5 @@
 import { css } from '@emotion/react';
+import type { CSSObject } from '@emotion/react';
 import type { Direction } from './types';
 import theme from '@styles/theme';
 
@@ -7,49 +8,48 @@ export const tooltipContainter = css`
     display: inline-block;
 `;
 
-export const tooltipStyle = (direction: Direction) => {
-    const positionStyles: Record<Direction, ReturnType<typeof css>> = {
-        topLeft: css`
-            bottom: 100%;
-            right: 50%;
-            margin-bottom: 0.3rem;
-        `,
-        topRight: css`
-            bottom: 100%;
-            left: 50%;
-            margin-bottom: 0.3rem;
-        `,
-        bottomLeft: css`
-            top: 100%;
-            right: 50%;
-            margin-top: 0.3rem;
-        `,
-        bottomRight: css`
-            top: 100%;
-            left: 50%;
-            margin-top: 0.3rem;
-        `,
-        left: css`
-            top: 50%;
-            right: 100%;
-            transform: translateY(-50%);
-            margin-right: 0.3rem;
-        `,
-        right: css`
-            top: 50%;
-            left: 100%;
-            transform: translateY(-50%);
-            margin-left: 0.3rem;
-        `,
-    };
-    return css`
-        position: absolute;
-        background-color: ${theme.colors.black};
-        color: ${theme.colors.white};
-        padding: 0.8rem 1rem;
-        border-radius: 5px;
-        white-space: nowrap;
-        ${theme.typography.subCaptionLight};
-        ${positionStyles[direction]};
-    `;
+export const positionStyles: Record<Direction, CSSObject> = {
+    topLeft: css`
+        bottom: 100%;
+        right: 50%;
+        margin-bottom: 0.3rem;
+    `,
+    topRight: css`
+        bottom: 100%;
+        left: 50%;
+        margin-bottom: 0.3rem;
+    `,
+    bottomLeft: css`
+        top: 100%;
+        right: 50%;
+        margin-top: 0.3rem;
+    `,
+    bottomRight: css`
+        top: 100%;
+        left: 50%;
+        margin-top: 0.3rem;
+    `,
+    left: css`
+        top: 50%;
+        right: 100%;
+        transform: translateY(-50%);
+        margin-right: 0.3rem;
+    `,
+    right: css`
+        top: 50%;
+        left: 100%;
+        transform: translateY(-50%);
+        margin-left: 0.3rem;
+    `,
 };
+
+export const tooltipStyle = (direction: Direction) => css`
+    position: absolute;
+    background-color: ${theme.colors.black};
+    color: ${theme.colors.white};
+    padding: 0.8rem 1rem;
+    border-radius: 5px;
+    white-space: nowrap;
+    ${theme.typography.subCaptionLight};
+    ${positionStyles[direction]};
+`;
