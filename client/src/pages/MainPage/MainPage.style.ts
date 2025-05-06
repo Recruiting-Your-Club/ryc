a3ac4051adc807448b1e7244cfeccc35aa1d9d49
aiminghee3 <zzuni3423@naver.com>
2025-05-07 02:42:49 +0900

    refactor: 메인페이지 모바일 퍼스트로 반응형 수정

--- client/src/pages/MainPage/MainPage.style.ts
+++ client/src/pages/MainPage/MainPage.style.ts
@@ -10,20 +10,24 @@ export const mainPageContainer = css`
 export const bannerContainer = css`
     display: flex;
     justify-content: center;
-    padding: 2rem 3rem;
-    height: 22rem;
-    width: 100%;
     margin-bottom: 1rem;
-    @media (max-width: ${theme.breakpoint.desktop}) {
+
+    @media (min-width: ${theme.breakpoint.mobileMini}) {
+        height: 10rem;
+        padding: 1rem;
+    }
+    @media (min-width: ${theme.breakpoint.mobile}) {
+        height: 12rem;
         padding: 2rem 6rem;
-        height: 20rem;
     }
-    @media (max-width: ${theme.breakpoint.tablet}) {
-        height: 20rem;
+    @media (min-width: ${theme.breakpoint.tabletMini}) {
+        height: 14rem;
     }
-    @media (max-width: ${theme.breakpoint.mobile}) {
-        height: 8rem;
-        padding: 0.5rem;
+    @media (min-width: ${theme.breakpoint.tabletMini}) {
+        height: 18rem;
+    }
+    @media (min-width: ${theme.breakpoint.desktop}) {
+        height: 20rem;
     }
 `;
 
@@ -33,18 +37,6 @@ export const totalClubContainer = css`
     align-items: start;
     padding: 0rem 0.5rem;
     gap: 0.5rem;
-    margin-bottom: 1rem;
-
-    @media (max-width: ${theme.breakpoint.tablet}) {
-        padding-left: 4rem;
-    }
-    @media (max-width: ${theme.breakpoint.tabletMini}) {
-        padding-left: 0.5rem;
-        margin: 0;
-    }
-    @media (max-width: ${theme.breakpoint.mobile}) {
-        padding-left: 1rem;
-    }
 `;
 
 export const clubCategoryContainer = css`
@@ -54,15 +46,6 @@ export const clubCategoryContainer = css`
     width: 100%;
     height: 3.5rem;
     border-bottom: 1px solid ${theme.colors.gray[200]};
-    padding-right: 0.5rem;
-    @media (max-width: ${theme.breakpoint.tablet}) {
-        margin-left: 4rem;
-        width: 90%;
-    }
-    @media (max-width: ${theme.breakpoint.tabletMini}) {
-        margin: 0;
-        width: 100%;
-    }
 `;
 
 export const emptyElement = css`
@@ -72,6 +55,7 @@ export const emptyElement = css`
         display: none;
     }
 `;
+
 export const progressContainer = (isActive: boolean) => css`
     display: flex;
     justify-content: end;
@@ -114,18 +98,22 @@ export const categoryButton = (active: boolean) => css`
 
 export const clubListContainer = css`
     display: grid;
-    grid-template-columns: repeat(3, 1fr); // 기본 3열
+    grid-template-columns: repeat(1, 1fr); // 기본 3열
     gap: 1rem;
     margin-top: 1rem;
     justify-items: center;
 
-    /* 1024px 이하일 때 2열 */
-    @media (max-width: ${theme.breakpoint.tablet}) {
+    @media (min-width: ${theme.breakpoint.tabletMini}) {
         grid-template-columns: repeat(2, 1fr);
     }
 
-    /* 640px인가 이하일 때 */
-    @media (max-width: ${theme.breakpoint.mobile}) {
-        grid-template-columns: 1fr;
+    @media (min-width: ${theme.breakpoint.tablet}) {
+        grid-template-columns: repeat(3, 1fr);
     }
 `;
+
+// mobileMini: '340px',
+// mobile: '480px',
+// tabletMini: '768px',
+// tablet: '1024px',
+// desktop: '1200px',
