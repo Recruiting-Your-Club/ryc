d578592adea7635a6d5636aef861b069e4c244b7
aiminghee3 <zzuni3423@naver.com>
2025-05-07 02:41:41 +0900

    refactor: 유저 레이아웃 모바일 퍼스트 반응형 픽셀로 수정

--- client/src/layouts/UserLayout/UserLayout.styles.ts
+++ client/src/layouts/UserLayout/UserLayout.styles.ts
@@ -5,32 +5,44 @@ export const UserLayoutCss = css`
     display: flex;
     flex-direction: column;
     height: 100%;
-    width: 115rem;
+    width: 100%;
+    max-width: 120rem;
     min-width: 32rem;
     background-color: #fff;
     padding: 6rem 6rem 0rem 6rem;
-    // 1200px 이하
-    @media (max-width: ${theme.breakpoint.desktop}) {
-        width: 110rem;
-        padding-left: 3rem;
-        padding-right: 3rem;
-    }
-    // 1024px 이하
-    @media (max-width: ${theme.breakpoint.tablet}) {
-        width: 90rem;
-        padding-left: 3rem;
-        padding-right: 3rem;
-    }
-    // 840px 이하
-    @media (max-width: ${theme.breakpoint.tabletMini}) {
-        width: 70rem;
+
+    /* 400px 이상 */
+    @media (min-width: ${theme.breakpoint.mobileMini}) {
         padding-left: 1rem;
         padding-right: 1rem;
     }
-    // 600px 이하
-    @media (max-width: ${theme.breakpoint.mobile}) {
-        width: 35rem;
+
+    /* 480px 이상 */
+    @media (min-width: ${theme.breakpoint.mobile}) {
         padding-left: 1rem;
         padding-right: 1rem;
+    }
+
+    /* 768px 이상 */
+    @media (min-width: ${theme.breakpoint.tabletMini}) {
+        padding-left: 3rem;
+        padding-right: 3rem;
+    }
+
+    /* 1024px 이상 */
+    @media (min-width: ${theme.breakpoint.tablet}) {
+        padding-left: 3rem;
+        padding-right: 3rem;
+    }
+
+    /* 1200px 이상 */
+    @media (min-width: ${theme.breakpoint.desktop}) {
+        padding-left: 3rem;
+        padding-right: 3rem;
     }
 `;
+// mobileMini: '400px',
+// mobile: '480px',
+// tabletMini: '768px',
+// tablet: '1024px',
+// desktop: '1200px',
