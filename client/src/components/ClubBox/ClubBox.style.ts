1035bac9a53e8bb010c044d5e480b29ce0e6acc5
aiminghee3 <zzuni3423@naver.com>
2025-05-10 16:45:36 +0900

    refactor: clubBox 데스크탑 퍼스트에서 모바일 퍼스트로 가고 싶어

--- client/src/components/ClubBox/ClubBox.style.ts
+++ client/src/components/ClubBox/ClubBox.style.ts
@@ -3,21 +3,20 @@ import theme from '@styles/theme';
 
 export const clubBoxContainer = css`
     display: grid;
-    grid-template-columns: repeat(2, 3fr);
+    width: 100%;
+    grid-template-columns: repeat(1, 1fr);
     column-gap: 10rem;
     row-gap: 2rem;
     background-color: ${theme.colors.gray[100]};
     padding: 2rem 3rem;
     border-radius: 15px;
-    margin-top: 10px;
 
-    @media (max-width: 480px) {
-        grid-template-columns: repeat(1, 2fr);
+    @media (min-width: ${theme.breakpoint.tabletMini}) {
+        grid-template-columns: repeat(2, 1fr);
     }
 `;
 
 export const clubBoxItem = css`
     display: flex;
-    align-items: center;
     gap: 3rem;
 `;
