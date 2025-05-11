bed9d8796ce092fb01fbe877e90ffbad0623993e
littleemm <lilysolar@naver.com>
2025-05-11 15:32:48 +0900

    feat: Score Tag 컴포넌트 구현

--- client/src/components/ScoreTag/ScoreTag.style.ts
+++ client/src/components/ScoreTag/ScoreTag.style.ts
@@ -1,1 +1,43 @@
 import { css } from '@emotion/react';
+import theme from '@styles/theme';
+import { hexToRgb } from '@utils/hexToRgb';
+
+const SCORE_COLOR_MAP = [
+    { min: 3.5, color: theme.colors.green[300] },
+    { min: 2.0, color: theme.colors.gray[800] },
+    { min: 0.0, color: theme.colors.brown[100] },
+    { min: -Infinity, color: theme.colors.red[1000] },
+];
+
+const baseScoreTag = (width: string) => css`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: ${width};
+    padding: 0.2rem 0.4rem;
+    border-radius: 4px;
+    gap: 0.2rem;
+`;
+
+const customPerTag = (color: string) => css`
+    color: ${color};
+    background-color: rgba(${hexToRgb(color)}, 0.1);
+    ${theme.typography.subCaptionRegular};
+`;
+
+const getScoreColor = (score: number): string => {
+    return SCORE_COLOR_MAP.find(({ min }) => score > min)!.color;
+};
+
+export const scoreTag = (width: string, score: string) => {
+    return css`
+        ${baseScoreTag(width)}
+        ${customPerTag(getScoreColor(Number(score)))}
+    `;
+};
+
+export const svgCss = css`
+    justify-content: center;
+    align-items: center;
+    height: 1.4rem;
+`;
--- client/src/components/ScoreTag/ScoreTag.tsx
+++ client/src/components/ScoreTag/ScoreTag.tsx
@@ -1,6 +1,22 @@
+import Star from '@assets/images/score-star.svg';
+import type { CSSObject } from '@emotion/react';
 import React from 'react';
+import { scoreTag, svgCss } from './ScoreTag.style';
 
-function ScoreTag() {
-    return <div></div>;
+export interface ScoreTagProps {
+    width?: string;
+    score: string;
+    sx?: CSSObject;
+}
+
+function ScoreTag({ width = '5rem', score, sx }: ScoreTagProps) {
+    return (
+        <div>
+            <span css={[scoreTag(width, score), sx]}>
+                <Star css={svgCss} />
+                {score}
+            </span>
+        </div>
+    );
 }
 export { ScoreTag };
