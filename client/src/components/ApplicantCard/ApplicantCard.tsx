1997e36fae44e250e8fbe92c56118593ed97aa5c
littleemm <lilysolar@naver.com>
2025-05-11 18:46:58 +0900

    feat: 지원자 카드 ui 구현 완료

---
+++ client/src/components/ApplicantCard/ApplicantCard.tsx
@@ -0,0 +1,68 @@
+import TimeCircle from '@assets/images/time-circle.svg';
+import { ScoreTag } from '@components/ScoreTag';
+import { Card, Checkbox, Text } from '@components/_common';
+import { Divider } from '@components/_common/Divider';
+import React from 'react';
+import {
+    bottomCss,
+    checkboxWrapper,
+    dateTextCss,
+    dateWrapper,
+    dividerCss,
+    emailTextCss,
+    FooterCss,
+    rootCss,
+    timeCircleSvgCss,
+} from './ApplicantCard.style';
+
+interface ApplicantCardProps {
+    name: string;
+    email: string;
+    date: string;
+    score: string;
+    status: string;
+}
+
+function ApplicantCard({ name, email, date, score, status }: ApplicantCardProps) {
+    // prop destruction
+    // lib hooks
+    // initial values
+    // state, ref, querystring hooks
+    // form hooks
+    // query hooks
+    // calculated values
+    // handlers
+    // effects
+
+    return (
+        <Card.Root width={'23rem'} radius={'5px'} sx={rootCss}>
+            <span css={checkboxWrapper}>
+                <Checkbox.Root size="xs">
+                    <Checkbox.HiddenInput />
+                    <Checkbox.Control />
+                </Checkbox.Root>
+            </span>
+            <Card.TopBody>
+                <Card.TitleContainer title={name} subTitle={email} subTitleSx={emailTextCss} />
+            </Card.TopBody>
+            <Card.BottomBody sx={bottomCss}>
+                <span css={dateWrapper}>
+                    <TimeCircle css={timeCircleSvgCss} />
+                    <Card.DescriptionText description={date} sx={dateTextCss} />
+                </span>
+            </Card.BottomBody>
+            <Divider sx={dividerCss} />
+            <Card.Footer sx={FooterCss}>
+                <Text
+                    as="span"
+                    type="helperTextBold"
+                    color={status.startsWith('평가 완료') ? 'primary' : 'black'}
+                >
+                    {status}
+                </Text>
+                <ScoreTag score={score} />
+            </Card.Footer>
+        </Card.Root>
+    );
+}
+export { ApplicantCard };
