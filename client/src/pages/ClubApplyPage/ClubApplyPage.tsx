c3b73acc94dbf312c3f10e45af09452d904c0e0d
Dochanii <dmlwhd010@naver.com>
2025-05-12 22:52:38 +0900

    feat: 질문 작성시  완료된 질문 개수 증가 로직 추가

--- client/src/pages/ClubApplyPage/ClubApplyPage.tsx
+++ client/src/pages/ClubApplyPage/ClubApplyPage.tsx
@@ -18,22 +18,43 @@ import { BREAKPOINT } from '@styles/theme/breakPoint';
 import { ClubApplyPersonalInfoPage } from './PersonalInfoPage';
 import { ClubApplyDetailQuestionPage } from './DetailQuestionPage';
 
-//질문 배열로 받아서 form map으로 뿌리기
-
 export const clubData = {
     clubName: '엔샵 (Enjoy C#)',
     tag: '학술동아리',
-    name: '이름',
-    birth: '생년월일',
-    phoneNumber: '전화번호',
-    gender: '성별',
 };
+
+export const clubPersonalQuestions = [
+    {
+        id: 1,
+        question: '이름',
+        type: 'string',
+    },
+    {
+        id: 2,
+        question: '생년월일',
+        type: 'string',
+    },
+    {
+        id: 3,
+        question: '전화번호',
+        type: 'string',
+    },
+    {
+        id: 4,
+        question: '성별',
+        type: 'boolean',
+    },
+];
+
 const applyData = [
     {
         question: '사전질문',
         index: 0,
     },
-    { question: '자기소개서', index: 1 },
+    {
+        question: '자기소개서',
+        index: 1,
+    },
 ];
 
 function ClubApplyPage() {
@@ -42,7 +63,7 @@ function ClubApplyPage() {
         window.innerWidth > parseInt(BREAKPOINT.tablet),
     );
     const [completedQuestions, setCompletedQuestions] = useState<number>(0);
-    const totalQuestions = 5; // 전체 질문 개수는 예시로 5개로 설정했습니다.
+    const totalQuestions = clubPersonalQuestions.length;
 
     useEffect(() => {
         const handleResize = () => {
@@ -52,6 +73,10 @@ function ClubApplyPage() {
         window.addEventListener('resize', handleResize);
         return () => window.removeEventListener('resize', handleResize);
     }, []);
+
+    const handleQuestionComplete = (completed: number) => {
+        setCompletedQuestions(completed);
+    };
 
     return (
         <div css={clubApplyPageContainer}>
@@ -79,7 +104,10 @@ function ClubApplyPage() {
                     </div>
                     {/* 페이지 */}
                     {idx === 0 ? (
-                        <ClubApplyPersonalInfoPage idx={idx} />
+                        <ClubApplyPersonalInfoPage
+                            idx={idx}
+                            onQuestionComplete={handleQuestionComplete}
+                        />
                     ) : (
                         <ClubApplyDetailQuestionPage idx={idx} />
                     )}
