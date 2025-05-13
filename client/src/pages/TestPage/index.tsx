c13fbd1f12c48c97a60dc4956c87d78e235c8c0b
aiminghee3 <zzuni3423@naver.com>
2025-05-13 11:12:01 +0900

    fix: 테스트페이지 import오류 수정:

--- client/src/pages/TestPage/index.tsx
+++ client/src/pages/TestPage/index.tsx
@@ -1,14 +1,7 @@
-import type { ChangeEvent } from 'react';
 import React, { TextareaHTMLAttributes, useState } from 'react';
-import { Button } from '@components/_common/Button';
-import { Text } from '@components/_common/Text';
-import { Input } from '@components/_common/Input';
-import { TextArea } from '@components/_common/TextArea/TextArea';
+import { MainCard } from '@components';
+
 function TestPage() {
-    const [text, setText] = useState('');
-    const onChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-        setText(e.target.value);
-    };
     return (
         <>
             <MainCard />
