61a3e543fb34c16f771b792002a5919456d472a5
littleemm <lilysolar@naver.com>
2025-05-14 16:05:15 +0900

    feat: 평가 카드 컴포넌트 구현 완료

---
+++ client/src/components/PersonalScoreCard/PersonalScoreCard.style.ts
@@ -0,0 +1,27 @@
+import { css } from '@emotion/react';
+export const cardContainer = css`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 0.8rem;
+    padding: 0.5rem 1rem;
+`;
+
+export const headerContainer = css`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+export const contentContainer = css`
+    width: 100%;
+    word-break: keep-all;
+    overflow-wrap: break-word;
+`;
+
+export const raterWrapper = css`
+    display: flex;
+    align-items: center;
+    gap: 0.7rem;
+`;
---
+++ client/src/components/PersonalScoreCard/PersonalScoreCard.tsx
@@ -0,0 +1,38 @@
+import { Avatar, Rating, Text } from '@components/_common';
+import React from 'react';
+import {
+    cardContainer,
+    contentContainer,
+    headerContainer,
+    raterWrapper,
+} from './PersonalScoreCard.style';
+
+interface PersonalScoreCardProps {
+    image?: string;
+    name: string;
+    score: number;
+    comment: string;
+}
+
+function PersonalScoreCard({ image, name, score, comment }: PersonalScoreCardProps) {
+    return (
+        <div css={cardContainer}>
+            <div css={headerContainer}>
+                <div css={raterWrapper}>
+                    <Avatar shape="round" size="xs" imageURL={image} />
+                    <Text as="span" type="bodySemibold" sx={{ paddingTop: '0.2rem' }}>
+                        {name}
+                    </Text>
+                </div>
+                <Rating value={score} size="lg" />
+            </div>
+            <div css={contentContainer}>
+                <Text as="span" textAlign="start">
+                    {comment}
+                </Text>
+            </div>
+        </div>
+    );
+}
+
+export { PersonalScoreCard };
