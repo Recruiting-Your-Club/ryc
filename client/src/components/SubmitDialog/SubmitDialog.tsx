77ca8e7605c7a0404f9781d497640139063f1b4b
Dochanii <dmlwhd010@naver.com>
2025-05-15 17:39:04 +0900

    refactor: 제출 모달 구조 변경

--- client/src/components/SubmitDialog/SubmitDialog.tsx
+++ client/src/components/SubmitDialog/SubmitDialog.tsx
@@ -10,35 +10,37 @@ import {
 } from './SubmiDialog.style';
 import type { SubmitDialogProps } from './types';
 
-const SubmitDialog = ({ open, onConfirm, onClose }: SubmitDialogProps) => (
-    <Dialog open={open} handleClose={onClose} size="sm">
-        <Dialog.Content>
-            <div css={submitDialogContentContainer}>
-                <Check />
-                <div css={submitDialogtTextContainer}>
-                    <Text type="h4Bold" color="black" textAlign="center">
-                        제출하시겠습니까?
-                    </Text>
-                    <Text type="bodyRegular" color="caption" textAlign="center">
-                        제출 후에는 수정할 수 없습니다
-                    </Text>
-                    <Text type="bodyRegular" color="subCaption" textAlign="center">
-                        제출 후 결과가 메일로 발송됩니다
-                    </Text>
+function SubmitDialog({ open, onConfirm, onClose }: SubmitDialogProps) {
+    return (
+        <Dialog open={open} handleClose={onClose} size="sm">
+            <Dialog.Content>
+                <div css={submitDialogContentContainer}>
+                    <Check />
+                    <div css={submitDialogtTextContainer}>
+                        <Text type="h4Bold" color="black" textAlign="center">
+                            제출하시겠습니까?
+                        </Text>
+                        <Text type="bodyRegular" color="caption" textAlign="center">
+                            제출 후에는 수정할 수 없습니다
+                        </Text>
+                        <Text type="bodyRegular" color="subCaption" textAlign="center">
+                            제출 후 결과가 메일로 발송됩니다
+                        </Text>
+                    </div>
                 </div>
-            </div>
-        </Dialog.Content>
-        <Dialog.Action>
-            <div css={submitDialogButtonContainer}>
-                <Button variant="primary" size="full" onClick={onConfirm}>
-                    예
-                </Button>
-                <Button variant="outlined" size="full" onClick={onClose}>
-                    아니오
-                </Button>
-            </div>
-        </Dialog.Action>
-    </Dialog>
-);
+            </Dialog.Content>
+            <Dialog.Action>
+                <div css={submitDialogButtonContainer}>
+                    <Button variant="primary" size="full" onClick={onConfirm}>
+                        예
+                    </Button>
+                    <Button variant="outlined" size="full" onClick={onClose}>
+                        아니오
+                    </Button>
+                </div>
+            </Dialog.Action>
+        </Dialog>
+    );
+}
 
 export { SubmitDialog };
