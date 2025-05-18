2ca789d5e31f5e23917188c07c01e46830e93ba1
Dochanii <dmlwhd010@naver.com>
2025-05-18 17:26:44 +0900

    fix: TestPage FileUpLoader로 적용

--- client/src/pages/TestPage/index.tsx
+++ client/src/pages/TestPage/index.tsx
@@ -1,19 +1,20 @@
-import type { ChangeEvent } from 'react';
 import React, { useState } from 'react';
 import { Button } from '@components/_common/Button';
 import { Tooltip } from '@components/_common/Tooltip';
-import React, { TextareaHTMLAttributes, useState } from 'react';
-import { MainCard } from '@components';
+import { FileUpLoader } from '@components/FileUpLoader';
 
 function TestPage() {
     return (
         <>
             <div>
                 <Tooltip content="Tooltip 예시입니다.">
-                    <Button variant="outlined">hd</Button>
+                    <FileUpLoader>
+                        <FileUpLoader.HelperText>하이</FileUpLoader.HelperText>
+                        <FileUpLoader.Button />
+                        <FileUpLoader.Box />
+                    </FileUpLoader>
                 </Tooltip>
             </div>
-
         </>
     );
 }
