589b10512ac33239f42aa45c8198f8a4cc625562
Dochanii <dmlwhd010@naver.com>
2025-05-20 17:42:06 +0900

    fix: 코드 리뷰 기반 수정 및 추가 css 리팩토링

--- client/src/components/Footer/Footer.style.ts
+++ client/src/components/Footer/Footer.style.ts
@@ -2,11 +2,12 @@ import { css } from '@emotion/react';
 import theme from '@styles/theme';
 
 export const footerContainer = (isManagerRoute: boolean) => css`
-    position: relative;
     width: 100dvw;
-    left: 50%;
-    margin-left: -50dvw;
+    height: 14rem;
     background-color: ${theme.colors.gray[100]};
+    display: flex;
+    align-items: center;
+
     ${isManagerRoute &&
     css`
         border-top: 1px solid ${theme.colors.gray[300]};
@@ -17,11 +18,10 @@ export const footerStyle = css`
     display: flex;
     flex-direction: column;
     align-items: flex-start;
+    justify-content: center;
     gap: 1rem;
-    width: 100%;
-    max-width: 75rem;
-    height: 14rem;
-    padding: 2.5rem 4rem;
+    padding: 2.5rem 0;
+    padding-left: 20rem;
 
     @media (max-width: ${theme.breakpoint.tablet}) {
         padding: 2rem 4rem;
@@ -31,13 +31,11 @@ export const footerStyle = css`
 
     @media (max-width: ${theme.breakpoint.mobile}) {
         padding: 1.5rem 3rem;
-        gap: 0.75rem;
         min-height: 10rem;
     }
 
     @media (max-width: ${theme.breakpoint.mobileMini}) {
         padding: 1rem 2rem;
-        gap: 0.5rem;
         min-height: 8rem;
     }
 `;
