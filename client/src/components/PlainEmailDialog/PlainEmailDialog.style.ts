d820d50ba40bbc355628ed79ad4dfdfd16bdd30e
littleemm <lilysolar@naver.com>
2025-05-20 23:26:57 +0900

    feat: 전체 이메일 보내기 (단순) dialog 구현 완료

---
+++ client/src/components/PlainEmailDialog/PlainEmailDialog.style.ts
@@ -0,0 +1,67 @@
+import { css } from '@emotion/react';
+import theme from '@styles/theme';
+export const dialogCss = css`
+    max-width: 64dvw;
+    max-height: 90dvh;
+    min-width: 115rem;
+`;
+
+export const headerCss = css`
+    justify-content: space-between;
+    padding: 1.4rem 3rem;
+`;
+
+export const contentCss = css`
+    display: flex;
+    flex-direction: column;
+    padding: 2rem 4rem;
+    gap: 2rem;
+    overflow: hidden;
+`;
+
+export const titleInputCss = css`
+    border: 1px solid ${theme.colors.gray[500]};
+    padding-left: 1rem;
+    ${theme.typography.captionRegular}
+`;
+
+export const inputCss = css`
+    ::placeholder {
+        ${theme.typography.captionRegular}
+    }
+`;
+
+const dialogContentWrapper = css`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+`;
+
+export const titleWrapper = css`
+    ${dialogContentWrapper}
+    flex: 1;
+`;
+
+export const contentWrapper = css`
+    ${dialogContentWrapper}
+    flex: 9;
+    height: 100%;
+`;
+
+export const textareaCss = css`
+    ${theme.typography.captionRegular}
+    margin: -2.5rem 0;
+    padding: 1rem 1.5rem;
+    min-height: 35rem;
+    height: 100%;
+
+    ::placeholder {
+        ${theme.typography.captionRegular}
+    }
+`;
+
+export const actionCss = css`
+    justify-content: flex-end;
+    padding: 1rem 4rem 3rem 4rem;
+`;
