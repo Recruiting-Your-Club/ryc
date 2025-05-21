1fc39cc807e9783ffc9363fe2fbca579a66a9ac5
littleemm <lilysolar@naver.com>
2025-05-21 22:39:15 +0900

    feat: 단계별 지원자 관리 화면에서 dialog 테스트할 수 있도록 state 및 버튼 추가

--- client/src/pages/StepManagementPage/StepManagementPage.tsx
+++ client/src/pages/StepManagementPage/StepManagementPage.tsx
@@ -1,5 +1,11 @@
 import Search from '@assets/images/search.svg';
-import { ApplicantDialog, Button, Input } from '@components';
+import {
+    ApplicantDialog,
+    Button,
+    Input,
+    InterviewSettingDialog,
+    PlainEmailDialog,
+} from '@components';
 import { ApplicantCard } from '@components/ApplicantCard';
 import { CardBox } from '@components/CardBox';
 import React, { useState } from 'react';
@@ -13,9 +19,138 @@ import {
     topContainer,
 } from './StepManagement.style';
 import type { Applicant, ClubNotice } from './types';
+const applicantList = [
+    {
+        name: '박민지',
+        email: 'test123@naver.com',
+        date: '2025. 02. 04',
+        score: '4.0',
+        status: '평가 중 (2/6)',
+    },
+    {
+        name: '김영림',
+        email: 'test1234@naver.com',
+        date: '2025. 02. 04',
+        score: '3.5',
+        status: '평가 완료 (6/6)',
+    },
+    {
+        name: '김일림',
+        email: 'test1235@naver.com',
+        date: '2025. 05. 20',
+        score: '0',
+        status: '평가 중 (0/6)',
+    },
+    {
+        name: '김이림',
+        email: 'test1236@naver.com',
+        date: '2025. 05. 20',
+        score: '1.5',
+        status: '평가 중 (3/6)',
+    },
+    {
+        name: '김세림',
+        email: 'test1237@naver.com',
+        date: '2025. 05. 20',
+        score: '2.4',
+        status: '평가 중 (2/6)',
+    },
+    {
+        name: '김네림',
+        email: 'test1238@naver.com',
+        date: '2025. 05. 20',
+        score: '5',
+        status: '평가 완료 (6/6)',
+    },
+];
+
+const applicantList2 = [
+    {
+        name: '박민지',
+        email: 'tes123@naver.com',
+        date: '2025. 02. 04',
+        score: '4.0',
+        status: '평가 중 (2/6)',
+    },
+    {
+        name: '김영림',
+        email: 'tes1234@naver.com',
+        date: '2025. 02. 04',
+        score: '3.5',
+        status: '평가 완료 (6/6)',
+    },
+    {
+        name: '김일림',
+        email: 'tes1235@naver.com',
+        date: '2025. 05. 20',
+        score: '0',
+        status: '평가 중 (0/6)',
+    },
+    {
+        name: '김이림',
+        email: 'tes1236@naver.com',
+        date: '2025. 05. 20',
+        score: '1.5',
+        status: '평가 중 (3/6)',
+    },
+    {
+        name: '김세림',
+        email: 'tes1237@naver.com',
+        date: '2025. 05. 20',
+        score: '2.4',
+        status: '평가 중 (2/6)',
+    },
+];
+
+const finalApplicantList = [
+    {
+        name: '박민지',
+        email: 't123@naver.com',
+        date: '2025. 02. 04',
+        score: '4.0',
+        status: '평가 완료 (6/6)',
+    },
+    {
+        name: '김영림',
+        email: 't1234@naver.com',
+        date: '2025. 02. 04',
+        score: '3.5',
+        status: '평가 완료 (6/6)',
+    },
+    {
+        name: '김일림',
+        email: 't1235@naver.com',
+        date: '2025. 05. 20',
+        score: '0',
+        status: '평가 완료 (6/6)',
+    },
+    {
+        name: '김이림',
+        email: 't1236@naver.com',
+        date: '2025. 05. 20',
+        score: '1.5',
+        status: '평가 완료 (6/6)',
+    },
+    {
+        name: '김세림',
+        email: 't1237@naver.com',
+        date: '2025. 05. 20',
+        score: '2.4',
+        status: '평가 완료 (6/6)',
+    },
+];
+
+const data: ClubNotice = {
+    document: true,
+    interview: true,
+};
 
 function StepManagementPage() {
     const [selectedEmails, setSelectedEmails] = useState<string[]>([]);
+    const [selectedApplicant, setSelectedApplicant] = useState<Applicant | null>(null);
+    const [isOpen, setIsOpen] = useState(false);
+    const [isEmailOpen, setIsEmailOpen] = useState(false);
+    const [isInterviewOpen, setIsInterviewOpen] = useState(false);
 
     const handleToggle = (email: string, checked: boolean) => {
         setSelectedEmails((prev) => (checked ? [...prev, email] : prev.filter((e) => e !== email)));
@@ -29,140 +164,22 @@ function StepManagementPage() {
         }
     };
 
-    const data: ClubNotice = {
-        document: true,
-        interview: true,
-    };
-
-    const applicantList = [
-        {
-            name: '박민지',
-            email: 'test123@naver.com',
-            date: '2025. 02. 04',
-            score: '4.0',
-            status: '평가 중 (2/6)',
-        },
-        {
-            name: '김영림',
-            email: 'test1234@naver.com',
-            date: '2025. 02. 04',
-            score: '3.5',
-            status: '평가 완료 (6/6)',
-        },
-        {
-            name: '김일림',
-            email: 'test1235@naver.com',
-            date: '2025. 05. 20',
-            score: '0',
-            status: '평가 중 (0/6)',
-        },
-        {
-            name: '김이림',
-            email: 'test1236@naver.com',
-            date: '2025. 05. 20',
-            score: '1.5',
-            status: '평가 중 (3/6)',
-        },
-        {
-            name: '김세림',
-            email: 'test1237@naver.com',
-            date: '2025. 05. 20',
-            score: '2.4',
-            status: '평가 중 (2/6)',
-        },
-        {
-            name: '김네림',
-            email: 'test1238@naver.com',
-            date: '2025. 05. 20',
-            score: '5',
-            status: '평가 완료 (6/6)',
-        },
-    ];
-
-    const applicantList2 = [
-        {
-            name: '박민지',
-            email: 'tes123@naver.com',
-            date: '2025. 02. 04',
-            score: '4.0',
-            status: '평가 중 (2/6)',
-        },
-        {
-            name: '김영림',
-            email: 'tes1234@naver.com',
-            date: '2025. 02. 04',
-            score: '3.5',
-            status: '평가 완료 (6/6)',
-        },
-        {
-            name: '김일림',
-            email: 'tes1235@naver.com',
-            date: '2025. 05. 20',
-            score: '0',
-            status: '평가 중 (0/6)',
-        },
-        {
-            name: '김이림',
-            email: 'tes1236@naver.com',
-            date: '2025. 05. 20',
-            score: '1.5',
-            status: '평가 중 (3/6)',
-        },
-        {
-            name: '김세림',
-            email: 'tes1237@naver.com',
-            date: '2025. 05. 20',
-            score: '2.4',
-            status: '평가 중 (2/6)',
-        },
-    ];
-
-    const finalApplicantList = [
-        {
-            name: '박민지',
-            email: 't123@naver.com',
-            date: '2025. 02. 04',
-            score: '4.0',
-            status: '평가 완료 (6/6)',
-        },
-        {
-            name: '김영림',
-            email: 't1234@naver.com',
-            date: '2025. 02. 04',
-            score: '3.5',
-            status: '평가 완료 (6/6)',
-        },
-        {
-            name: '김일림',
-            email: 't1235@naver.com',
-            date: '2025. 05. 20',
-            score: '0',
-            status: '평가 완료 (6/6)',
-        },
-        {
-            name: '김이림',
-            email: 't1236@naver.com',
-            date: '2025. 05. 20',
-            score: '1.5',
-            status: '평가 완료 (6/6)',
-        },
-        {
-            name: '김세림',
-            email: 't1237@naver.com',
-            date: '2025. 05. 20',
-            score: '2.4',
-            status: '평가 완료 (6/6)',
-        },
-    ];
-    const [selectedApplicant, setSelectedApplicant] = useState<Applicant | null>(null);
-    const [isOpen, setIsOpen] = useState(false);
     const handleOpen = (applicant: Applicant) => {
         setSelectedApplicant(applicant);
         setIsOpen(true);
     };
+
     const handleClose = () => {
         setIsOpen(false);
         setSelectedApplicant(null);
+    };
+
+    const handleEmailClose = () => {
+        setIsEmailOpen(false);
+    };
+
+    const handleInterviewSettingClose = () => {
+        setIsInterviewOpen(false);
     };
 
     return (
@@ -183,7 +200,6 @@ function StepManagementPage() {
             </div>
             <div css={stepBoxContainer}>
                 <CardBox stepTitle={data.document ? '서류 평가' : '지원서 접수'} step="normal">
-                    {/* <Button onClick={handleSelectAll}>전체선택</Button> */}
                     <div css={cardGroup}>
                         {applicantList.map((applicant) => (
                             <ApplicantCard
@@ -202,7 +218,6 @@ function StepManagementPage() {
                 </CardBox>
                 {data.interview && (
                     <CardBox stepTitle="면접" step="normal">
-                        {/* <Button onClick={handleSelectAll}>전체선택</Button> */}
                         <div css={cardGroup}>
                             {applicantList2.map((applicant) => (
                                 <ApplicantCard
@@ -221,7 +236,6 @@ function StepManagementPage() {
                     </CardBox>
                 )}
                 <CardBox stepTitle="최종 합격" step="final">
-                    {/* <Button onClick={handleSelectAll}>전체선택</Button> */}
                     <div css={cardGroup}>
                         {finalApplicantList.map((applicant) => (
                             <ApplicantCard
@@ -238,7 +252,6 @@ function StepManagementPage() {
                         ))}
                     </div>
                 </CardBox>
-                <div css={bottomContainer} />
                 {selectedApplicant && (
                     <ApplicantDialog
                         name={selectedApplicant.name}
@@ -247,7 +260,16 @@ function StepManagementPage() {
                         handleClose={handleClose}
                     />
                 )}
+                <Button onClick={() => setIsInterviewOpen(true)}>면접일정설정</Button>
+                <InterviewSettingDialog
+                    open={isInterviewOpen}
+                    handleClose={handleInterviewSettingClose}
+                />
+
+                <Button onClick={() => setIsEmailOpen(true)}>이메일</Button>
+                <PlainEmailDialog open={isEmailOpen} handleClose={handleEmailClose} />
             </div>
+            <div css={bottomContainer} />
         </div>
     );
 }
