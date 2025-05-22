8f64e33dc5009699961cf402b1a2045fa024383d
aiminghee3 <zzuni3423@naver.com>
2025-05-22 19:16:35 +0900

    style: clubNavigation에서 슬라이더 추가

--- client/src/components/ClubNavigation/types.ts
+++ client/src/components/ClubNavigation/types.ts
@@ -2,10 +2,10 @@ import type { ReactNode } from 'react';
 
 export interface NavigationItem {
     title: string;
-    page: ReactNode; // React 컴포넌트나 JSX 엘리먼트를 포함할 수 있음을 나타냅니다.
+    page: ReactNode;
+    width: string;
 }
 
 export interface ClubNavigationProps {
     navigationItem: NavigationItem[];
-    navItem?: ReactNode; // 선택적 prop으로, React 컴포넌트나 JSX 엘리먼트를 포함할 수 있습니다.
 }
