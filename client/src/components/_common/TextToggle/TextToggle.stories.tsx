3a7cc1fa321a4e675b4d5d70798d148a7aeab31e
aiminghee3 <zzuni3423@naver.com>
2025-05-23 15:35:12 +0900

    refactor: TextToggle 스토리북 네이밍 수정

--- client/src/components/_common/TextToggle/TextToggle.stories.tsx
+++ client/src/components/_common/TextToggle/TextToggle.stories.tsx
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { TextToggle } from './TextToggle';
 
 const meta: Meta<typeof TextToggle> = {
-    title: 'Toggle',
+    title: 'TextToggle',
     component: TextToggle,
     parameters: {
         docs: {
