295b3080cdcc5a183783458ebd64de7fc9661c80
aiminghee3 <zzuni3423@naver.com>
2025-05-26 18:01:27 +0900

    refactor: 전역으로 queryClient 설정

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,17 +5,21 @@ import { Global, ThemeProvider } from '@emotion/react';
 import globalStyles from '@styles/global';
 import theme from '@styles/theme';
 import { ToastProvider } from '@components/Toast/ToastProvider';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import dayjs from 'dayjs';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const queryClient = new QueryClient();
 dayjs.locale('ko'); // dayjs를 한국기준으로 설정
 root.render(
     <React.StrictMode>
         <Global styles={globalStyles} />
         <ThemeProvider theme={theme}>
-            <ToastProvider limit={5}>
-                <App />
-            </ToastProvider>
+            <QueryClientProvider client={queryClient}>
+                <ToastProvider limit={5}>
+                    <App />
+                </ToastProvider>
+            </QueryClientProvider>
         </ThemeProvider>
     </React.StrictMode>,
 );
