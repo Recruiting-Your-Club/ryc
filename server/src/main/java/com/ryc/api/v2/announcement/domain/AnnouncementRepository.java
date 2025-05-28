e2664e936259e76636d3f83c41c293f522a18448
huro0906 <huro0906@gmail.com>
2025-05-28 11:31:26 +0900

    Feat: finAllByClubId 추가
    해당 club의 Announcement List 불러오는 메소드

--- server/src/main/java/com/ryc/api/v2/announcement/domain/AnnouncementRepository.java
+++ server/src/main/java/com/ryc/api/v2/announcement/domain/AnnouncementRepository.java
@@ -1,5 +1,14 @@
 package com.ryc.api.v2.announcement.domain;
 
+import java.util.List;
+
 public interface AnnouncementRepository {
     public Announcement save(Announcement announcement);
+
+    /**
+     *
+     * @param clubId
+     * @return 해당 Club의 Announcement List
+     */
+    public List<Announcement> findAllByClubId(String clubId);
 }
--- server/src/main/java/com/ryc/api/v2/announcement/infra/jpa/AnnouncementJpaRepository.java
+++ server/src/main/java/com/ryc/api/v2/announcement/infra/jpa/AnnouncementJpaRepository.java
@@ -2,6 +2,12 @@ package com.ryc.api.v2.announcement.infra.jpa;
 
 import com.ryc.api.v2.announcement.infra.entity.AnnouncementEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface AnnouncementJpaRepository extends JpaRepository<AnnouncementEntity, String> {
+    @Query("select a from AnnouncementEntity a where a.clubEntity.id = :clubId")
+    List<AnnouncementEntity> findAllByClubId(@Param("clubId") String clubId);
 }
