c3870fce3f63217f55f584895df7d703c57aabf6
aiminghee3 <zzuni3423@naver.com>
2025-06-02 02:22:35 +0900

    add: 예시 코드 testPage에 추가

--- client/src/pages/TestPage/index.tsx
+++ client/src/pages/TestPage/index.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import { Tooltip } from '@components/_common/Tooltip';
 import { FileUpLoader } from '@components/FileUpLoader';
-
+import { Calendar } from '@components';
+import dayjs from 'dayjs';
 
 function TestPage() {
+    const [select, setSelect] = React.useState<string[]>([]);
+    const [clicked, setClicked] = React.useState<string>('');
+    const handleCalendar = () => {
+        setSelect([...select, clicked]);
+    };
     return (
         <>
             <div>
-                <Tooltip content="Tooltip 예시입니다.">
-                    <FileUpLoader>
-                        <FileUpLoader.HelperText>하이</FileUpLoader.HelperText>
-                        <FileUpLoader.Button />
-                        <FileUpLoader.Box />
-                    </FileUpLoader>
-                </Tooltip>
+                <Calendar selectedDate={[clicked, ...select]} setClickedDate={setClicked} />
+                <button onClick={handleCalendar}>날짜 적용</button>
+                <br />
+                <button onClick={() => setSelect([])}>초기화</button>
             </div>
         </>
     );
