a41dc3d8d104753335c086266b14ea6624001e51
aiminghee3 <zzuni3423@naver.com>
2025-06-03 18:54:33 +0900

    refactor: <div></div> 형태 <div/> 로 수정

--- client/src/pages/LoadingPage/DetailLoadingPage/DetailLoadingPage.tsx
+++ client/src/pages/LoadingPage/DetailLoadingPage/DetailLoadingPage.tsx
@@ -23,52 +23,52 @@ function DetailLoadingPage() {
             {/* 동아리 헤더 섹션 */}
             <div css={skeletonContentContainer}>
                 <div css={skeletonHeaderSection}>
-                    <div css={skeletonLogo}></div>
+                    <div css={skeletonLogo} />
                     <div>
-                        <div css={skeletonTitle}></div>
-                        <div css={skeletonCategory}></div>
+                        <div css={skeletonTitle} />
+                        <div css={skeletonCategory} />
                     </div>
                 </div>
 
                 {/* 탭 내비게이션 섹션 */}
                 <div css={skeletonTabsSection}>
-                    <div css={skeletonTab}></div>
+                    <div css={skeletonTab} />
                 </div>
 
                 {/* 정보 그리드 스켈레톤 (동아리 소개 탭의 콘텐츠) */}
                 <div css={skeletonInfoGrid}>
                     <div css={skeletonInfoItem}>
-                        <div css={skeletonInfoLabel}></div>
-                        <div css={skeletonInfoValue}></div>
+                        <div css={skeletonInfoLabel} />
+                        <div css={skeletonInfoValue} />
                     </div>
                     <div css={skeletonInfoItem}>
-                        <div css={skeletonInfoLabel}></div>
-                        <div css={skeletonInfoValue}></div>
+                        <div css={skeletonInfoLabel} />
+                        <div css={skeletonInfoValue} />
                     </div>
                     <div css={skeletonInfoItem}>
-                        <div css={skeletonInfoLabel}></div>
-                        <div css={skeletonInfoValue}></div>
+                        <div css={skeletonInfoLabel} />
+                        <div css={skeletonInfoValue} />
                     </div>
                     <div css={skeletonInfoItem}>
-                        <div css={skeletonInfoLabel}></div>
-                        <div css={skeletonInfoValue}></div>
+                        <div css={skeletonInfoLabel} />
+                        <div css={skeletonInfoValue} />
                     </div>
                     <div css={skeletonInfoItem}>
-                        <div css={skeletonInfoLabel}></div>
-                        <div css={skeletonInfoValue}></div>
+                        <div css={skeletonInfoLabel} />
+                        <div css={skeletonInfoValue} />
                     </div>
                     <div css={skeletonInfoItem}>
-                        <div css={skeletonInfoLabel}></div>
-                        <div css={skeletonInfoValue}></div>
+                        <div css={skeletonInfoLabel} />
+                        <div css={skeletonInfoValue} />
                     </div>
                 </div>
 
                 {/* 줄글 설명 스켈레톤 */}
                 <div css={skeletonContentSection}>
-                    <div css={skeletonContentLine}></div>
-                    <div css={skeletonContentLine}></div>
-                    <div css={skeletonContentLine}></div>
-                    <div css={skeletonContentShortLine}></div>
+                    <div css={skeletonContentLine} />
+                    <div css={skeletonContentLine} />
+                    <div css={skeletonContentLine} />
+                    <div css={skeletonContentShortLine} />
                 </div>
             </div>
         </div>
