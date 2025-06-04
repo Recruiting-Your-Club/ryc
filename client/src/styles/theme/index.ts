c11a718aec1340c507b63e0ce03cf4cabec7d571
Dochanii <dmlwhd010@naver.com>
2025-06-04 21:20:51 +0900

    chore: 중복된 import 제거

--- client/src/styles/theme/index.ts
+++ client/src/styles/theme/index.ts
@@ -2,7 +2,6 @@ import { TYPOGRAPHY } from './typography';
 import { COLORS } from './colors';
 import { BREAKPOINT } from './breakPoint';
 import type { Theme } from '@emotion/react';
-import { BREAKPOINT } from './breakPoint';
 
 const theme: Theme = {
     colors: COLORS,
