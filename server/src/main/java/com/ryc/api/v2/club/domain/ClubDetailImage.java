90ecff9314007cc62948494a0b6512a2be72f521
SangHyeok0908 <tkdgur9799@gmail.com>
2025-06-05 23:43:02 +0900

    Fix: imageUrl varchar(255) -> imageUrl varchar(500)
    길이가 255를 넘는 더미 값이 존재하여 크기를 수정하였습니다.

--- server/src/main/java/com/ryc/api/v2/club/domain/ClubDetailImage.java
+++ server/src/main/java/com/ryc/api/v2/club/domain/ClubDetailImage.java
@@ -1,19 +1,23 @@
 package com.ryc.api.v2.club.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
-
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @Embeddable
-@Getter
-@Builder
-@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
-@NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
-public class ClubDetailImage {
+ @Getter
+ @Builder
+ @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+ @NoArgsConstructor(access = AccessLevel.PROTECTED, force = true)
+ public class ClubDetailImage {
+
+  @Column(columnDefinition = "varchar(500")
   private final String imageUrl;
+
+  @Column(columnDefinition = "varchar(500")
   private final String thumbnailUrl;
-}
+ }
