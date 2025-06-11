373ecb61de6f333d2a247c8617d72f13cc441275
littleemm <lilysolar@naver.com>
2025-06-11 23:34:46 +0900

    refactor: InterviewTimeBox에 들어가는 시간 버튼 리스트 함수를 context를 이용하여 전달

--- client/src/components/InterviewSettingDialog/InterviewSettingDialog.tsx
+++ client/src/components/InterviewSettingDialog/InterviewSettingDialog.tsx
@@ -10,31 +10,36 @@ import {
     contentCss,
     contentWrapper,
     dialogCss,
-    emailWrapper,
     headerCss,
     informationContainer,
-    informationInput,
-    informationInputCss,
     inputCss,
-    inputFormSection,
-    perInformationInput,
     submitButtonWrapper,
+    s_emailContainer,
+    s_emptyPlace,
+    s_perInformationContainer,
+    s_resetButton,
+    s_select,
+    s_selectContainer,
+    s_selectTrigger,
     textareaCss,
-    timeSelectCss,
     titleInputCss,
     titleWrapper,
     verticalDivider,
 } from './InterviewSettingDialog.style';
+import { InterviewSettingDialogContext } from './interviewSettingDialogContext';
 import type { InterviewSettingDialogProps } from './types';
+
+const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+const times = ['15분', '30분', '1시간'];
 
 function InterviewSettingDialog({ open, handleClose }: InterviewSettingDialogProps) {
     // prop destruction
     // lib hooks
     // initial values
     // state, ref, querystring hooks
-    const [inputValue, setInputValue] = useState<string>('');
-    const [perNumber, setPerNumber] = useState<string>('');
-    const [unit, setUnit] = useState<string>('hour');
+    const [timeValue, setTimeValue] = useState<string>('15분');
+    const [number, setNumber] = useState<string>('1');
+    const [timeButtonList, setTimeButtonList] = useState<string[]>([]);
     const [selectedDates, setSelectedDates] = useState<string[]>([]);
     const [currentDate, setCurrentDate] = useState<string>('');
     const [selectedTimesByDate, setSelectedTimesByDate] = useState<Record<string, string[]>>({});
@@ -42,11 +47,13 @@ function InterviewSettingDialog({ open, handleClose }: InterviewSettingDialogPro
     // form hooks
     // query hooks
     // calculated values
+    const contextValue = useMemo(() => ({ timeButtonList, setTimeButtonList }), [timeButtonList]);
+
     const interval = useMemo((): number => {
-        const value = Number(inputValue);
-        if (isNaN(value) || value <= 0) return 0;
-        return unit === 'hour' ? value * 60 : value;
-    }, [inputValue, unit]);
+        const value = Number(timeValue.replace(/분|시간/g, ''));
+        if (value === 1) return 60;
+        return value;
+    }, [timeValue]);
 
     // handler
     const handleTimeClick = (time: string) => {
@@ -67,128 +74,155 @@ function InterviewSettingDialog({ open, handleClose }: InterviewSettingDialogPro
     };
 
     const handleSelectReset = () => {
-        handleClose();
-        setSelectedTimesByDate({});
-        setSelectedDates([]);
-        setUnit('시간');
-        setInputValue('');
+        setTimeValue('15분');
+        setNumber('1');
     };
 
     // effects
     useEffect(() => {
         setCurrentDate(selectedDates.at(-1)!);
+        setTimeButtonList([]);
     }, [selectedDates]);
 
     return (
-        <Dialog open={open} handleClose={handleSelectReset} size="full" sx={dialogCss}>
-            <Dialog.Header position="start" sx={headerCss}>
-                <Text as="span" type="bodyBold" sx={{ paddingTop: '0.3rem' }}>
-                    면접 일정 전송
-                </Text>
-                <Button
-                    variant="transparent"
-                    size="xs"
-                    aria-label="close"
-                    onClick={handleSelectReset}
-                >
-                    <XIcon alt="close" />
-                </Button>
-            </Dialog.Header>
-            <Divider color="black" sx={{ borderTop: '1px solid' }} />
-            <Dialog.Content sx={contentCss}>
-                <div css={informationContainer}>
-                    <Text as="span" type="h4Bold" textAlign="start">
-                        상세 면접 정보
+        <InterviewSettingDialogContext.Provider value={contextValue}>
+            <Dialog open={open} handleClose={handleSelectReset} size="full" sx={dialogCss}>
+                <Dialog.Header position="start" sx={headerCss}>
+                    <Text as="span" type="bodyBold" sx={{ paddingTop: '0.3rem' }}>
+                        면접 일정 보내기
                     </Text>
-                    <div css={informationInput}>
-                        <div css={perInformationInput}>
-                            <Text as="span" type="bodySemibold" textAlign="start">
-                                면접 당 최대 정원 수
+                    <Button
+                        variant="transparent"
+                        size="xs"
+                        aria-label="close"
+                        onClick={handleClose}
+                    >
+                        <XIcon alt="close" />
+                    </Button>
+                </Dialog.Header>
+                <Divider color="black" sx={{ borderTop: '1px solid' }} />
+                <Dialog.Content sx={contentCss}>
+                    <div css={s_selectContainer}>
+                        <Text as="span" type="h4Bold" textAlign="start">
+                            상세 면접 정보
+                        </Text>
+                        <div css={s_perInformationContainer}>
+                            <Text as="span" type="bodyBold" textAlign="start">
+                                면접 최대 인원 수
                             </Text>
-                            <div css={inputFormSection}>
-                                <Input
-                                    value={perNumber}
-                                    onChange={(e) => setPerNumber(e.target.value)}
-                                    inputSx={informationInputCss}
-                                />
-                                <Text
-                                    as="span"
-                                    type="captionSemibold"
-                                    sx={{ marginLeft: '-7.5rem' }}
-                                >
-                                    명
-                                </Text>
-                            </div>
+                            <Text as="span" type="captionRegular" textAlign="start">
+                                한 면접 당 최대 인원 수를 정해요.
+                            </Text>
+                            <Select
+                                value={number}
+                                onValueChange={setNumber}
+                                size="xs"
+                                sx={s_select}
+                            >
+                                <Select.Trigger sx={s_selectTrigger}>
+                                    <Select.Value />
+                                </Select.Trigger>
+                                <Select.Content>
+                                    {numbers.map((number) => (
+                                        <Select.Item key={number} value={number}>
+                                            {number}
+                                        </Select.Item>
+                                    ))}
+                                </Select.Content>
+                            </Select>
                         </div>
-                        <div css={perInformationInput}>
-                            <Text as="span" type="bodySemibold" textAlign="start">
+                        <div css={s_perInformationContainer}>
+                            <Text as="span" type="bodyBold" textAlign="start">
                                 면접 당 진행 시간
                             </Text>
-                            <div css={inputFormSection}>
-                                <Input
-                                    value={inputValue}
-                                    onChange={(e) => setInputValue(e.target.value)}
-                                    inputSx={informationInputCss}
-                                />
-                                <Select value={unit} onValueChange={setUnit} size="xs">
-                                    <Select.Trigger sx={timeSelectCss}>
-                                        <Select.Value placeholder="시간" />
-                                    </Select.Trigger>
-                                    <Select.Content>
-                                        <Select.Item value="hour">시간</Select.Item>
-                                        <Select.Item value="min">분</Select.Item>
-                                    </Select.Content>
-                                </Select>
-                            </div>
+                            <Text as="span" type="captionRegular" textAlign="start">
+                                한 면접 당 걸리는 시간을 정해요.
+                            </Text>
+                            <Select
+                                value={timeValue}
+                                onValueChange={setTimeValue}
+                                size="xs"
+                                sx={s_select}
+                            >
+                                <Select.Trigger sx={s_selectTrigger}>
+                                    <Select.Value />
+                                </Select.Trigger>
+                                <Select.Content>
+                                    {times.map((time) => (
+                                        <Select.Item key={time} value={time}>
+                                            {time}
+                                        </Select.Item>
+                                    ))}
+                                </Select.Content>
+                            </Select>
+                        </div>
+                        <div css={s_emptyPlace} />
+                        <div css={s_perInformationContainer}>
+                            <Text as="span" type="bodyBold" textAlign="start">
+                                초기화
+                            </Text>
+                            <Text as="span" type="captionRegular" textAlign="start">
+                                지금까지 정한 정보를 초기화해요.
+                            </Text>
+                            <Button
+                                size="md"
+                                variant="transparent"
+                                onClick={handleSelectReset}
+                                sx={s_resetButton}
+                            >
+                                초기화
+                            </Button>
                         </div>
                     </div>
-                    <Calendar
-                        // isMultiple
-                        size="sm"
-                        selectedDate={selectedDates}
-                        onSelect={setSelectedDates}
-                        sx={calendarCss}
-                    />
-                    <InterviewTimeBox
-                        interval={interval}
-                        selectedDate={currentDate}
-                        selectedTimes={selectedTimesByDate[currentDate] || []}
-                        handleClick={handleTimeClick}
-                        handleReset={handleTimeReset}
-                    />
-                </div>
-                <div css={verticalDivider}></div>
-                <div css={emailWrapper}>
-                    <div css={titleWrapper}>
-                        <Text as="span" type="h4Semibold" textAlign="start">
-                            제목
-                        </Text>
-                        <Input
-                            height="4rem"
-                            placeholder="이메일 제목을 입력해주세요."
-                            inputSx={titleInputCss}
-                            sx={inputCss}
-                        />
-                    </div>
-                    <div css={contentWrapper}>
-                        <Text as="span" type="h4Semibold" textAlign="start">
-                            내용
-                        </Text>
-                        <TextArea
-                            defaultValue={INITIAL_FORM}
+                    <div css={informationContainer}>
+                        <Calendar
+                            // isMultiple
                             size="md"
-                            placeholder="이메일 내용을 입력해주세요."
-                            sx={textareaCss}
-                            textAreaSx={{ height: '100%', margin: '0' }}
+                            selectedDate={selectedDates}
+                            onSelect={setSelectedDates}
+                            sx={calendarCss}
+                        />
+                        <InterviewTimeBox
+                            interval={interval}
+                            selectedDate={currentDate}
+                            selectedTimes={selectedTimesByDate[currentDate] || []}
+                            isSelected={Boolean(currentDate)}
+                            handleClick={handleTimeClick}
                         />
                     </div>
-                    <div css={submitButtonWrapper}>
-                        <Button>이메일 보내기</Button>
+                    <div css={verticalDivider} />
+                    <div css={s_emailContainer}>
+                        <div css={titleWrapper}>
+                            <Text as="span" type="h4Semibold" textAlign="start">
+                                제목
+                            </Text>
+                            <Input
+                                height="4rem"
+                                placeholder="이메일 제목을 입력해주세요."
+                                inputSx={titleInputCss}
+                                sx={inputCss}
+                            />
+                        </div>
+                        <div css={contentWrapper}>
+                            <Text as="span" type="h4Semibold" textAlign="start">
+                                내용
+                            </Text>
+                            <TextArea
+                                defaultValue={INITIAL_FORM}
+                                size="md"
+                                placeholder="이메일 내용을 입력해주세요."
+                                sx={textareaCss}
+                                textAreaSx={{ height: '100%', margin: '0' }}
+                            />
+                        </div>
+                        <div css={submitButtonWrapper}>
+                            <Button>이메일 보내기</Button>
+                        </div>
                     </div>
-                </div>
-            </Dialog.Content>
-            <Dialog.Action>{''}</Dialog.Action>
-        </Dialog>
+                </Dialog.Content>
+                <Dialog.Action>{''}</Dialog.Action>
+            </Dialog>
+        </InterviewSettingDialogContext.Provider>
     );
 }
 export { InterviewSettingDialog };
--- client/src/components/InterviewTimeBox/InterviewTimeBox.tsx
+++ client/src/components/InterviewTimeBox/InterviewTimeBox.tsx
@@ -1,12 +1,18 @@
+import { useInterviewSettingDialogContext } from '@components/InterviewSettingDialog/interviewSettingDialogContext';
+import { Select } from '@components/Select';
 import { Button, Divider, Text } from '@components/_common';
+import { useToast } from '@hooks/useToast';
 import { generateTimeRange } from '@utils/InterviewTimeBox/generateTime';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import {
     baseBox,
-    buttonCss,
     dividerCss,
-    resetButtonWrapper,
     selectedTimeSection,
+    s_applyButton,
+    s_select,
+    s_selectContent,
+    s_selectTrigger,
+    s_timeRangeSettingContainer,
     timeButtonCss,
     timeSelectSection,
 } from './InterviewTimeBox.style';
@@ -16,31 +22,45 @@ function InterviewTimeBox({
     selectedDate,
     selectedTimes,
     interval,
+    isSelected,
     handleClick,
-    handleReset,
 }: InterviewTimeBoxProps) {
     // prop destruction
     // lib hooks
     // initial values
+    const { toast } = useToast();
+
     // state, ref, querystring hooks
-    const [timeButtonList, setTimeButtonList] = useState<string[]>([]);
+    const { timeButtonList, setTimeButtonList } = useInterviewSettingDialogContext();
+    const [startTime, setStartTime] = useState<string>('08:00');
+    const [endTime, setEndTime] = useState<string>('22:00');
 
     // form hooks
     // query hooks
     // calculated values
+    const timeItems = generateTimeRange('08:00', '22:00', interval);
     // handler
+    // console.log(isSelected);
     const handleApply = () => {
         if (!interval) return;
-        const result = generateTimeRange('00:00', '24:00', interval);
+        if (!isSelected) {
+            toast.info('캘린더에서 날짜를 선택해주세요!', {
+                toastTheme: 'white',
+                position: 'topCenter',
+            });
+            alert('캘린더에서 날짜를 선택해주세요!');
+            return;
+        }
+        const result = generateTimeRange(startTime, endTime, interval);
         setTimeButtonList(result);
     };
 
     // effects
-    useEffect(() => {
-        if (selectedDate) {
-            handleApply();
-        }
-    }, [selectedDate]);
+    // useEffect(() => {
+    //     if (selectedDate) {
+    //         handleApply();
+    //     }
+    // }, [selectedDate]);
 
     return (
         <div css={baseBox}>
@@ -48,9 +68,34 @@ function InterviewTimeBox({
                 <Text as="span" type="captionSemibold">
                     면접 진행 시간
                 </Text>
-                <div css={resetButtonWrapper}>
-                    <Button onClick={handleReset} variant="transparent" size="xl" sx={buttonCss}>
-                        초기화
+                <div css={s_timeRangeSettingContainer}>
+                    <Select value={startTime} onValueChange={setStartTime} size="xs" sx={s_select}>
+                        <Select.Trigger sx={s_selectTrigger}>
+                            <Select.Value />
+                        </Select.Trigger>
+                        <Select.Content>
+                            {timeItems.map((item) => (
+                                <Select.Item key={item} value={item}>
+                                    {item}
+                                </Select.Item>
+                            ))}
+                        </Select.Content>
+                    </Select>
+                    -
+                    <Select value={endTime} onValueChange={setEndTime} size="xs" sx={s_select}>
+                        <Select.Trigger sx={s_selectTrigger}>
+                            <Select.Value />
+                        </Select.Trigger>
+                        <Select.Content sx={s_selectContent}>
+                            {timeItems.map((item) => (
+                                <Select.Item key={item} value={item}>
+                                    {item}
+                                </Select.Item>
+                            ))}
+                        </Select.Content>
+                    </Select>
+                    <Button onClick={handleApply} size="xl" sx={s_applyButton}>
+                        적용
                     </Button>
                 </div>
             </div>
