6a216da8940a3aaad59879196802ee7b45e795e0
littleemm <lilysolar@naver.com>
2025-06-15 16:25:39 +0900

    fix: 각 시각 button이 2줄이 되면 height 간격이 두 배가 되는 현상 제거

--- client/src/components/InterviewTimeBox/InterviewTimeBox.style.ts
+++ client/src/components/InterviewTimeBox/InterviewTimeBox.style.ts
@@ -8,7 +8,6 @@ export const baseBox = css`
     min-height: 10rem;
     border: 1px solid ${theme.colors.gray[400]};
     border-radius: 8px;
-    z-index: 999;
     overflow: hidden;
 `;
 
@@ -17,36 +16,47 @@ export const timeSelectSection = css`
     align-items: center;
     padding: 1.5rem;
     justify-content: space-between;
-    min-height: 5rem;
+    height: 6rem;
 `;
 
-export const resetButtonWrapper = css`
+export const s_timeRangeSettingContainer = css`
     display: flex;
     align-items: center;
+    gap: 0.5rem;
+`;
+
+export const s_select = css`
+    width: 7rem;
+    margin: 0.7rem 0;
+`;
+
+export const s_selectTrigger = css`
+    border: 1px solid ${theme.colors.gray[400]};
+    border-radius: 6px;
+    width: 7rem;
+    height: 2.5rem;
+`;
+
+export const s_selectContent = css`
+    background-color: ${theme.colors.white};
 `;
 
 export const dividerCss = css`
     border-top: 1px solid ${theme.colors.gray[400]};
-    z-index: 999;
 `;
 
 export const selectedTimeSection = css`
     display: flex;
-    min-height: 5rem;
     flex-wrap: wrap;
     gap: 0.5rem;
     padding: 1rem;
     overflow-y: auto;
 `;
 
-export const buttonCss = css`
+export const s_applyButton = css`
     ${theme.typography.subCaptionSemibold}
-    height: 3rem;
-    padding: 0 1.5rem;
-    background-color: ${theme.colors.gray[100]};
-    &:hover {
-        background-color: ${theme.colors.gray[200]};
-    }
+    height: 2.5rem;
+    padding: 0 1.7rem;
 `;
 
 export const timeButtonCss = (active: boolean) => css`
