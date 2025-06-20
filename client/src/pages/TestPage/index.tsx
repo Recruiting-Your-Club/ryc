9cec5b0c994633dac7d1cb8df1e46873c5eaf565
aiminghee3 <zzuni3423@naver.com>
2025-06-20 15:42:48 +0900

    test: Calendar 예시 코드 작성

--- client/src/pages/TestPage/index.tsx
+++ client/src/pages/TestPage/index.tsx
@@ -5,20 +5,51 @@ import { Calendar } from '@components';
 import dayjs from 'dayjs';
 
 function TestPage() {
-    const [select, setSelect] = React.useState<string[]>([]);
-    const [clicked, setClicked] = React.useState<string>('');
-    const handleCalendar = () => {
-        setSelect([...select, clicked]);
+    const [selectedDate2, setSelectedDate2] = React.useState<string[]>([
+        '2025-06-01',
+        '2025-06-07',
+    ]);
+
+    const [selectedDate3, setSelectedDate3] = React.useState<string[]>([
+        '2025-06-04',
+        '2025-06-05',
+        '2025-06-06',
+    ]);
+
+    const [clicked, setClicked] = React.useState<string[]>([]);
+
+    const [selectedDate, setSelectedDate] = React.useState<string[]>(['2025-06-01', '2025-06-09']);
+    const handleCalendar = (newSelected: string[]) => {
+        // newSelected는 달력에서 클릭한 날짜를 의미합니다.
     };
+
     return (
         <>
-            <div>
-                <Calendar selectedDate={[clicked, ...select]} setClickedDate={setClicked} />
-                <button onClick={handleCalendar}>날짜 적용</button>
-                <br />
-                <button onClick={() => setSelect([])}>초기화</button>
+            <div style={{ display: 'flex' }}>
+                <Calendar
+                    mode="custom"
+                    selectedDate={selectedDate}
+                    onSelect={handleCalendar}
+                    size="md"
+                />
+                <Calendar
+                    mode="single"
+                    selectedDate={selectedDate}
+                    onSelect={setSelectedDate}
+                    size="md"
+                />
+                <Calendar
+                    mode="custom"
+                    selectedDate={selectedDate3}
+                    onSelect={handleCalendar}
+                    size="md"
+                    onlySelected={true}
+                    highlightedDate={clicked}
+                />
             </div>
         </>
     );
 }
 export { TestPage };
+
+//<Calendar selectedDate={[clicked, ...select]} setClickedDate={setClicked} />
