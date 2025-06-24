8e03575e32eb730d295f8bc74538c95592e03e4d
Dochanii <dmlwhd010@naver.com>
2025-06-24 17:50:46 +0900

    fix: Radio props 사용하기 쉽게 options를 제외한 나머지 props optional로 변경 및 방향, disabled 초기값 설정

--- client/src/components/_common/Radio/Radio.tsx
+++ client/src/components/_common/Radio/Radio.tsx
@@ -1,5 +1,5 @@
 import type { CSSObject } from '@emotion/react';
-import React, { useState } from 'react';
+import React from 'react';
 import { radioContainer } from './Radio.style';
 import { RadioItem } from './RadioItem';
 
@@ -7,31 +7,39 @@ export type RadioOrientation = 'horizontal' | 'vertical';
 
 interface RadioOption {
     label?: string; // 실제로 보여질 옵션 값
-    value: string; // 서버에 넘겨질 옵션 값(직접 설정 가능)
+    value?: string; // 서버에 넘겨질 옵션 값(직접 설정 가능)
 }
 
 interface RadioProps {
     options: RadioOption[];
-    name: string;
+    name?: string;
     value?: string;
     disabled?: boolean;
-    orientation: RadioOrientation;
-    onChange: (value: string) => void;
+    orientation?: RadioOrientation;
+    onChange?: (value: string) => void;
     sx?: CSSObject;
 }
 
-function Radio({ options, name, value, disabled, orientation, onChange, sx }: RadioProps) {
+function Radio({
+    options,
+    name,
+    value,
+    disabled = false,
+    orientation = 'vertical',
+    onChange,
+    sx,
+}: RadioProps) {
     return (
         <div css={[radioContainer(orientation), sx]}>
             {options.map(({ label, value: itemValue }, index) => (
                 <RadioItem
                     key={index}
                     option={label}
-                    value={itemValue}
-                    name={name}
+                    value={itemValue ?? ''}
+                    name={name ?? ''}
                     checked={value === itemValue}
                     disabled={disabled}
-                    onChange={() => onChange?.(itemValue)}
+                    onChange={() => onChange?.(itemValue ?? '')}
                 />
             ))}
         </div>
