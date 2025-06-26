00bc17b303d02e2adea9053d993b2ffbb799085b
aiminghee3 <zzuni3423@naver.com>
2025-06-26 20:35:28 +0900

    chore: prettier, eslint 설정

---
+++ monorepo/.prettierrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    printWidth: 100,
+    singleQuote: true,
+    semi: true,
+    tabWidth: 4,
+    useTabs: false,
+    bracketSpacing: true,
+    arrowParens: 'always',
+    endOfLine: 'auto',
+    plugins: ['@trivago/prettier-plugin-sort-imports'],
+    importOrder: [
+        '^@ssoc/(.*)$', // 내부 패키지들
+        '^@/(.*)$', // 절대 경로
+        '^[./]', // 상대 경로
+    ],
+    importOrderSortSpecifiers: true,
+    importOrderSeparation: true,
+    importOrderCaseInsensitive: true,
+    //plugins: ['@trivago/prettier-plugin-sort-imports']
+};
