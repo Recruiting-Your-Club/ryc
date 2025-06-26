a81d0b43774bc36cb38811118f1560c1915939bd
littleemm <lilysolar@naver.com>
2025-06-27 02:38:28 +0900

    refactor: 하드코딩 자기소개서 내용을 props로 받도록 수정

--- client/src/components/InformationBox/types.ts
+++ client/src/components/InformationBox/types.ts
@@ -7,5 +7,11 @@ export interface ApplicantDetail {
 }
 export interface InformationBoxProps {
     applicant: ApplicantDetail | null;
+    documentList: Document | null;
     height?: string;
+}
+
+export interface Document {
+    id: number;
+    detail: { question: string; answer: string }[];
 }
