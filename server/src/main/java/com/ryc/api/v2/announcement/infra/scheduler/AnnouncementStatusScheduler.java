e1984b51b30475c8e3db62116e13f0f8b6a605e2
huro0906 <huro0906@gmail.com>
2025-06-27 13:16:35 +0900

    Fix: Scheduler 적용되지 않던 현상 수정
    @EnableScheduling 어노테이션 명시

--- server/src/main/java/com/ryc/api/v2/announcement/infra/scheduler/AnnouncementStatusScheduler.java
+++ server/src/main/java/com/ryc/api/v2/announcement/infra/scheduler/AnnouncementStatusScheduler.java
@@ -1,5 +1,6 @@
 package com.ryc.api.v2.announcement.infra.scheduler;
 
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -7,6 +8,7 @@ import com.ryc.api.v2.announcement.service.AnnouncementService;
 
 import lombok.RequiredArgsConstructor;
 
+@EnableScheduling
 @Component
 @RequiredArgsConstructor
 public class AnnouncementStatusScheduler {
