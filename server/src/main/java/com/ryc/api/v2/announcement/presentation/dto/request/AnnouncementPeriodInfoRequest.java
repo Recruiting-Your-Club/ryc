772561530c35cf8ac2a654b036635b4f62ded8ce
huro0906 <huro0906@gmail.com>
2025-06-27 10:22:13 +0900

    Feat: @Schema 누락된것 추가

--- server/src/main/java/com/ryc/api/v2/announcement/presentation/dto/request/AnnouncementPeriodInfoRequest.java
+++ server/src/main/java/com/ryc/api/v2/announcement/presentation/dto/request/AnnouncementPeriodInfoRequest.java
@@ -26,7 +26,9 @@ import lombok.Builder;
         필요 없는 값은 공백 또는 null 명시
         """)
 public record AnnouncementPeriodInfoRequest(
-    @NotNull(message = "applicationPeriod shouldn't be null") PeriodRequest applicationPeriod,
+    @NotNull(message = "applicationPeriod shouldn't be null") @Schema(description = "지원 기간")
+        PeriodRequest applicationPeriod,
     @Schema(description = "면접 기간") PeriodRequest interviewPeriod,
     @Schema(description = "서류 결과 발표 기간") PeriodRequest documentResultPeriod,
-    @NotNull(message = "finalResultPeriod shouldn't be null") PeriodRequest finalResultPeriod) {}
+    @NotNull(message = "finalResultPeriod shouldn't be null") @Schema(description = "최종 발표 기간")
+        PeriodRequest finalResultPeriod) {}
