a9010fde040875a7571c7ef78e0fdab999559df0
aiminghee3 <zzuni3423@naver.com>
2025-06-28 19:20:40 +0900

    refactor: 스토리북 globalstyle import 에러 수정

--- monorepo/packages/ui/.storybook/preview.tsx
+++ monorepo/packages/ui/.storybook/preview.tsx
@@ -2,14 +2,13 @@ import { Global, ThemeProvider } from '@emotion/react';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
-import theme from '@ssoc/styles';
-import { global } from '@ssoc/styles';
+import theme, { globalStyles } from '@ssoc/styles';
 
 export const decorators = [
     (Story) => (
         <BrowserRouter>
             <ThemeProvider theme={theme}>
-                <Global styles={global} />
+                <Global styles={globalStyles} />
                 <Story />
             </ThemeProvider>
         </BrowserRouter>
