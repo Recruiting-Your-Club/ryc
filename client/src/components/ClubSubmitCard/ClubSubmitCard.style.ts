8c278adc51c69b90cab9638d01a8f311311f3cda
Dochanii <dmlwhd010@naver.com>
2025-06-30 21:00:26 +0900

    remove: clubSumitCard에서 사용하던 질문 상태 텍스트 스타일 제거

--- client/src/components/ClubSubmitCard/ClubSubmitCard.style.ts
+++ client/src/components/ClubSubmitCard/ClubSubmitCard.style.ts
@@ -60,27 +60,3 @@ export const deadlineText = (diffDay: number) => css`
         color: ${theme.colors.red[800]};
     `}
 `;
-
-export const questionStatusContainer = css`
-    display: flex;
-    align-items: center;
-    gap: 0.5rem;
-`;
-export const questionStatusTextSx = (isAllQuestionCompleted: boolean) => css`
-    display: flex;
-    gap: 0.5rem;
-    align-items: center;
-    ${isAllQuestionCompleted &&
-    css`
-        color: ${theme.colors.default};
-    `};
-    ${!isAllQuestionCompleted &&
-    css`
-        color: ${theme.colors.red[800]};
-    `};
-`;
-
-export const arrowIcon = css`
-    width: 1rem;
-    height: 1rem;
-`;
