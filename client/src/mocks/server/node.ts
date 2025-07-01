d1434d4e8e3d263bb5294665e0b7a6143127c099
aiminghee3 <zzuni3423@naver.com>
2025-07-01 17:21:10 +0900

    refactor: nodeServer에서 테스트 환경 초기화 하는 코드 -> root에 있는 setupTest.js로 이동

--- client/src/mocks/server/node.ts
+++ client/src/mocks/server/node.ts
@@ -3,16 +3,4 @@ import { handlers } from '../handlers';
 
 const nodeServer = setupServer(...handlers);
 
-beforeAll(() => {
-    nodeServer.listen({ onUnhandledRequest: 'error' });
-});
-
-afterEach(() => {
-    nodeServer.resetHandlers();
-});
-
-afterAll(() => {
-    nodeServer.close();
-});
-
 export { nodeServer };
