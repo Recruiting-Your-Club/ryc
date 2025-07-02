62462b0a40aef70f2e6a1de99dcd4a362f62bc97
ohsung0722 <99doldol@naver.com>
2025-07-03 01:20:30 +0900

    refactor: 기존 StarSVG -> 피그마 StarSVG로 변경

--- client/src/components/_common/Rating/Star.tsx
+++ client/src/components/_common/Rating/Star.tsx
@@ -2,18 +2,7 @@ import React from 'react';
 import type { SerializedStyles } from '@emotion/react';
 import { s_halfStar, s_size, s_star } from './Rating.style';
 import type { StarProps } from './types';
-
-const STAR_PATH =
-    'M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z';
-const STAR_VIEWBOX = '0 0 24 24';
-
-function StarSVG() {
-    return (
-        <svg viewBox={STAR_VIEWBOX}>
-            <path d={STAR_PATH} />
-        </svg>
-    );
-}
+import StarSVG from '@assets/images/star.svg';
 
 export function Star({
     filled,
