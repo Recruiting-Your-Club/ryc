d0b92138ea5ac22092beb015181313316accfe8a
Dochanii <dmlwhd010@naver.com>
2025-07-03 18:27:31 +0900

    fix: 모바일에서 글자가 넘쳐서 글자 크기 줄임

--- client/src/pages/ClubApplyPage/DetailQuestionPage/ClubApplyDetailQuestionPage.style.ts
+++ client/src/pages/ClubApplyPage/DetailQuestionPage/ClubApplyDetailQuestionPage.style.ts
@@ -15,6 +15,9 @@ export const textAreaSx = css`
     width: 100%;
     height: 30rem;
     ${theme.typography.captionRegular};
+    @media (max-width: ${theme.breakpoint.mobile}) {
+        ${theme.typography.subCaptionRegular};
+    }
 `;
 
 export const s_labelContainer = css`
@@ -30,10 +33,30 @@ export const s_questionTitleContainer = css`
     display: flex;
     align-items: center;
     gap: 0.5rem;
+    margin-left: 0.5rem;
 `;
 
 export const s_infoIcon = css`
     width: 2rem;
     height: 2rem;
     color: ${theme.colors.gray[400]};
+`;
+
+export const s_tooltipSx = css`
+    @media (max-width: ${theme.breakpoint.mobile}) {
+        direction: 'bottomLeft';
+    }
+`;
+
+export const s_questionTitleSx = css`
+    @media (max-width: ${theme.breakpoint.mobile}) {
+        ${theme.typography.captionRegular};
+    }
+`;
+
+export const s_questionStarSx = css`
+    margin-top: 0.5rem;
+    @media (max-width: ${theme.breakpoint.mobile}) {
+        margin-top: 0.25rem;
+    }
 `;
