a0068246186ba82715858c63e3b340ba0de4f030
SangHyeok0908 <tkdgur9799@gmail.com>
2025-07-06 17:42:36 +0900

    Refactor: JPQL 최적화

--- server/src/main/java/com/ryc/api/v2/role/infra/jpa/ClubRoleJpaRepository.java
+++ server/src/main/java/com/ryc/api/v2/role/infra/jpa/ClubRoleJpaRepository.java
@@ -10,22 +10,25 @@ import com.ryc.api.v2.role.infra.entity.ClubRoleEntity;
 
 public interface ClubRoleJpaRepository extends JpaRepository<ClubRoleEntity, String> {
 
-  @Query("SELECT r FROM ClubRoleEntity r WHERE r.club.id = :clubId")
-  List<ClubRoleEntity> findByClubId(@Param("clubId") String clubId);
+  List<ClubRoleEntity> findByClubId(String clubId);
 
   @Query(
       """
-  SELECT CASE WHEN EXISTS (
-    SELECT 1 FROM ClubRoleEntity r
-    WHERE r.admin.id = :adminId AND r.club.id = :clubId
-  ) THEN true ELSE false END
-""")
+            SELECT CASE WHEN EXISTS (
+              SELECT 1 FROM ClubRoleEntity r
+              WHERE r.admin.id = :adminId AND r.club.id = :clubId
+            ) THEN true ELSE false END
+          """)
   boolean existsByAdminIdAndClubId(
       @Param("adminId") String adminId, @Param("clubId") String clubId);
 
   @Query(
-      "SELECT CASE WHEN COUNT(r) > 0 THEN true ELSE false END "
-          + "FROM ClubRoleEntity r WHERE r.admin.id = :adminId AND r.club.id = :clubId AND r.role = 'OWNER'")
+      """
+        SELECT CASE WHEN EXISTS (
+          SELECT 1 FROM ClubRoleEntity r
+          WHERE r.admin.id = :adminId AND r.club.id = :clubId AND r.role = 'OWNER'
+        ) THEN true ELSE false END
+      """)
   boolean existsOwnerRoleByAdminIdAndClubId(
       @Param("adminId") String adminId, @Param("clubId") String clubId);
 
