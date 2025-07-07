0b41066b43acfd8ec86c910e1f2f35362aa3dfe8
huro0906 <huro0906@gmail.com>
2025-07-07 13:54:55 +0900

    Docs: swagger error Response 문서화
    - AnnouncementBadRequest로 Announcement Api의 에러코드 그룹화

--- server/src/main/java/com/ryc/api/config/SwaggerConfiguration.java
+++ server/src/main/java/com/ryc/api/config/SwaggerConfiguration.java
@@ -1,20 +1,48 @@
 package com.ryc.api.config;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.ryc.api.v2.announcement.common.exception.code.AnnouncementErrorCode;
+import com.ryc.api.v2.common.exception.code.ErrorCode;
+import com.ryc.api.v2.common.exception.response.ErrorResponse;
+
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.examples.Example;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.media.MediaType;
+import io.swagger.v3.oas.models.responses.ApiResponse;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SwaggerConfiguration {
+
   @Bean
   public OpenAPI openAPI() {
     String jwt = "JWT";
     SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
+
+    // 에러 코드 예시 생성
+    Map<String, Example> announcementErrorExamples =
+        Arrays.stream(AnnouncementErrorCode.values())
+            .collect(Collectors.toMap(AnnouncementErrorCode::name, this::createErrorExample));
+
+    // AnnouncementBadRequest(400) 응답 정의
+    ApiResponse announcementBadRequestResponse =
+        new ApiResponse()
+            .description("Bad Request")
+            .content(
+                new Content()
+                    .addMediaType(
+                        "application/json", new MediaType().examples(announcementErrorExamples)));
+
     Components components =
         new Components()
             .addSecuritySchemes(
@@ -23,16 +51,24 @@ public class SwaggerConfiguration {
                     .name(jwt)
                     .type(SecurityScheme.Type.HTTP)
                     .scheme("bearer")
-                    .bearerFormat("JWT"));
+                    .bearerFormat("JWT"))
+            .addResponses(
+                "AnnouncementBadRequest",
+                announcementBadRequestResponse); // AnnouncementBadRequest 응답 추가
 
     return new OpenAPI()
-        .components(new Components())
+        .components(components)
         .info(apiInfo())
-        .addSecurityItem(securityRequirement)
-        .components(components);
+        .addSecurityItem(securityRequirement);
   }
 
   private Info apiInfo() {
     return new Info().title("RYC API").description("Recruiting Your Club API 문서").version("1.0.0");
+  }
+  //ErrorCode 예시문 작성 코드
+  private Example createErrorExample(ErrorCode errorCode) {
+    ErrorResponse errorResponse =
+        ErrorResponse.builder().code(errorCode.name()).message(errorCode.getMessage()).build();
+    return new Example().value(errorResponse);
   }
 }
--- server/src/main/java/com/ryc/api/v2/announcement/presentation/AnnouncementHttpApi.java
+++ server/src/main/java/com/ryc/api/v2/announcement/presentation/AnnouncementHttpApi.java
@@ -5,11 +5,13 @@ import java.util.List;
 import jakarta.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import com.ryc.api.v2.announcement.presentation.dto.request.AnnouncementCreateRequest;
 import com.ryc.api.v2.announcement.presentation.dto.request.AnnouncementUpdateRequest;
 import com.ryc.api.v2.announcement.presentation.dto.response.*;
+import com.ryc.api.v2.security.dto.CustomUserDetail;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.headers.Header;
@@ -17,13 +19,10 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RequestMapping("/api/v2")
-@Tag(name = "공고")
 public interface AnnouncementHttpApi {
 
-  /** todo @HasAnyRoleScured */
   @PostMapping("/clubs/{club-id}/announcements")
   @Operation(summary = "클럽 공고 생성")
   @ApiResponses(
@@ -35,30 +34,58 @@ public interface AnnouncementHttpApi {
                 @Content(
                     mediaType = "application/json",
                     schema = @Schema(implementation = AnnouncementCreateResponse.class)),
-            headers = {@Header(name = "Location", description = "생성된 리소스의 상세정보 조회 URI")})
+            headers = {@Header(name = "Location", description = "생성된 리소스의 상세정보 조회 URI")}),
+        @ApiResponse(responseCode = "400", ref = "#/components/responses/AnnouncementBadRequest"),
+        @ApiResponse(
+            responseCode = "404",
+            description = "존재하지 않는 클럽입니다.",
+            content = @Content(schema = @Schema(hidden = true)))
       })
-  public ResponseEntity<AnnouncementCreateResponse> create(
-      @PathVariable("club-id") String clubId, @Valid @RequestBody AnnouncementCreateRequest body);
+  ResponseEntity<AnnouncementCreateResponse> create(
+      @AuthenticationPrincipal CustomUserDetail userDetail,
+      @PathVariable("club-id") String clubId,
+      @Valid @RequestBody AnnouncementCreateRequest body);
 
   @GetMapping("/clubs/{club-id}/announcements")
   @Operation(summary = "클럽 공고 목록 조회")
-  public ResponseEntity<List<AnnouncementGetAllResponse>> getAnnouncementsByClubId(
+  @ApiResponses(
+      value = {
+        @ApiResponse(responseCode = "200", description = "OK"),
+        @ApiResponse(
+            responseCode = "404",
+            description = "존재하지 않는 클럽입니다.",
+            content = @Content(schema = @Schema(hidden = true)))
+      })
+  ResponseEntity<List<AnnouncementGetAllResponse>> getAnnouncementsByClubId(
       @PathVariable("club-id") String clubId);
 
   @GetMapping("/announcements/{announcement-id}")
   @Operation(summary = "공고 상세 조회")
-  public ResponseEntity<AnnouncementGetDetailResponse> getAnnouncementDetail(
+  @ApiResponses(
+      value = {
+        @ApiResponse(responseCode = "200", description = "OK"),
+        @ApiResponse(
+            responseCode = "404",
+            description = "존재하지 않는 공고입니다.",
+            content = @Content(schema = @Schema(hidden = true)))
+      })
+  ResponseEntity<AnnouncementGetDetailResponse> getAnnouncementDetail(
       @PathVariable("announcement-id") String announcementId);
 
-  /** 공고 수정 todo hasAnyRole 어노테이션 구현 후 추가 */
-  @PutMapping("/announcements/{announcement-id}")
+  @PutMapping("/clubs/{club-id}/announcements/{announcement-id}")
   @Operation(summary = "공고 수정")
-  public ResponseEntity<AnnouncementUpdateResponse> updateAnnouncementDetail(
+  @ApiResponses(
+      value = {
+        @ApiResponse(responseCode = "200", description = "OK"),
+        @ApiResponse(responseCode = "400", ref = "#/components/responses/AnnouncementBadRequest"),
+        @ApiResponse(
+            responseCode = "404",
+            description = "존재하지 않는 클럽 또는 공고입니다.",
+            content = @Content(schema = @Schema(hidden = true)))
+      })
+  ResponseEntity<AnnouncementUpdateResponse> updateAnnouncementDetail(
+      @AuthenticationPrincipal CustomUserDetail userDetail,
+      @PathVariable("club-id") String clubId,
       @PathVariable("announcement-id") String announcementId,
       @Valid @RequestBody AnnouncementUpdateRequest body);
-
-  @GetMapping("/announcements/{announcement-id}/application-form")
-  @Operation(summary = "공고 지원서 조회")
-  public ResponseEntity<ApplicationFormResponse> getApplicationForm(
-      @PathVariable("announcement-id") String announcementId);
 }
