cb1abb25e7702c1d6083646638274abcd0416ff8
SangHyeok0908 <tkdgur9799@gmail.com>
2025-07-10 17:32:10 +0900

    Docs: Swagger 구체화

--- server/src/main/java/com/ryc/api/v2/club/presentation/ClubHttpApi.java
+++ server/src/main/java/com/ryc/api/v2/club/presentation/ClubHttpApi.java
@@ -1,5 +1,6 @@
 package com.ryc.api.v2.club.presentation;
 
+import java.net.URI;
 import java.util.List;
 
 import jakarta.validation.Valid;
@@ -26,10 +27,13 @@ import com.ryc.api.v2.role.service.ClubRoleService;
 import com.ryc.api.v2.security.dto.CustomUserDetail;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
-/** 클라이언트나 외부 서버 어플리케이션에서 Http 기반으로 접근하는 API */
 @RestController
 @RequestMapping("api/v2/clubs")
 @RequiredArgsConstructor
@@ -43,15 +47,34 @@ public class ClubHttpApi {
 
   @PostMapping
   @Operation(summary = "동아리 생성 API")
+  @ApiResponses({
+    @ApiResponse(responseCode = "201", description = "동아리 생성 성공"),
+    @ApiResponse(
+        responseCode = "400",
+        description = "이미 존재하는 동아리 이름",
+        content = @Content(schema = @Schema(hidden = true)))
+  })
   public ResponseEntity<ClubCreateResponse> createClub(
       @AuthenticationPrincipal CustomUserDetail userDetail,
       @Valid @RequestBody ClubCreateRequest body) {
     ClubCreateResponse response = clubFacade.createClub(userDetail.getId(), body);
-    return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    URI location = URI.create(String.format("api/v2/clubs/%s", response.clubId()));
+    return ResponseEntity.created(location).body(response);
   }
 
   @PatchMapping("/{id}")
   @Operation(summary = "동아리 수정 API", description = "ID에 해당하는 동아리를 수정합니다. 수정하고싶은 필드만 포함시켜주세요.")
+  @ApiResponses({
+    @ApiResponse(responseCode = "200", description = "동아리 수정 성공"),
+    @ApiResponse(
+        responseCode = "404",
+        description = "동아리 ID가 존재하지 않음",
+        content = @Content(schema = @Schema(hidden = true))),
+    @ApiResponse(
+        responseCode = "400",
+        description = "이미 존재하는 동아리 이름으로 수정하려고 함",
+        content = @Content(schema = @Schema(hidden = true)))
+  })
   public ResponseEntity<ClubUpdateResponse> updateClub(
       @AuthenticationPrincipal CustomUserDetail userDetail,
       @PathVariable String id,
@@ -64,6 +87,7 @@ public class ClubHttpApi {
 
   @GetMapping
   @Operation(summary = "모든 동아리 조회 API")
+  @ApiResponse(responseCode = "200", description = "모든 동아리 조회 성공")
   public ResponseEntity<List<AllClubGetResponse>> getAllClub() {
     List<AllClubGetResponse> responses = clubAnnouncementFacade.getAllClubWithAnnouncementStatus();
     return ResponseEntity.status(HttpStatus.OK).body(responses);
@@ -71,6 +95,13 @@ public class ClubHttpApi {
 
   @GetMapping("/{id}")
   @Operation(summary = "동아리 상세 조회 API", description = "동아리 ID로 하나의 동아리를 조회합니다.")
+  @ApiResponses({
+    @ApiResponse(responseCode = "200", description = "동아리 상세 조회 성공"),
+    @ApiResponse(
+        responseCode = "404",
+        description = "동아리 ID가 존재하지 않음",
+        content = @Content(schema = @Schema(hidden = true)))
+  })
   public ResponseEntity<ClubGetResponse> getClub(@PathVariable String id) {
     ClubGetResponse response = clubService.getClub(id);
     return ResponseEntity.status(HttpStatus.OK).body(response);
@@ -78,6 +109,7 @@ public class ClubHttpApi {
 
   @GetMapping("/my")
   @Operation(summary = "사용자가 속한 동아리 조회 API", description = "사용자가 속한 동아리들을 조회합니다.")
+  @ApiResponse(responseCode = "200", description = "사용자가 속한 동아리 조회 성공")
   public ResponseEntity<List<ClubGetByAdminIdResponse>> getClubByAdminId(
       @AuthenticationPrincipal CustomUserDetail userDetail) {
     List<ClubGetByAdminIdResponse> responses = clubRoleService.getClubByAdminId(userDetail.getId());
