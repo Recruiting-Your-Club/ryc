4cd779b5a96907da42efb30ce844036266cc9ea2
ohsung0722 <99doldol@naver.com>
2025-07-14 01:35:48 +0900

    feat: httpClient Headers 객체 도입 및 httpRequest에 silent refresh 로직 구현

--- client/src/stores/authStore.ts
+++ client/src/stores/authStore.ts
@@ -1,10 +1,11 @@
+import { BASE_URL } from "@constants/api";
 import { create } from "zustand";
 
 interface AuthState {
     accessToken: string | null;
-    setAccessToken: (t: string | null) => void;
+    setAccessToken: (token: string | null) => void;
     clear: () => void;
-    bootstrap: () => Promise<void>;
+    bootstrap: () => Promise<string | null>;
 }
 
 export const useAuthStore = create<AuthState>((set) => ({
@@ -13,15 +14,18 @@ export const useAuthStore = create<AuthState>((set) => ({
     clear: () => set({accessToken: null}),
     bootstrap: async() => {
         try{
-            const res = await fetch('/auth/refreshToken', {
+            const refreshURL = new URL('/auth/refreshToken', BASE_URL).toString();
+            const res = await fetch(refreshURL, {
                 method: 'GET',
                 credentials: 'include'
             });
-            if(!res.ok) throw new Error();
+            if(!res.ok) throw new Error('refresh 실패');
             const {accessToken} = (await res.json()) as {accessToken: string}
             set({accessToken})
+            return accessToken;
         } catch {
             set({accessToken: null})
+            return null;
         }
     }
 }))
