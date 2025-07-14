54189d819ecbaf42d5f9001a87a52ac66dd201f4
ohsung0722 <99doldol@naver.com>
2025-07-14 15:19:59 +0900

    feat: 회원가입 api 연결

--- client/src/pages/AuthPage/RegisterPage/RegisterPage.tsx
+++ client/src/pages/AuthPage/RegisterPage/RegisterPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 import {
     RegisterContainer,
     RegisterBox,
@@ -11,18 +11,38 @@ import { Input, Button } from '@components/_common';
 import { PasswordInput } from '@components/PasswordInput';
 import { css } from '@emotion/react';
 import { useRouter } from '@hooks/useRouter';
+import { useRegister } from '@hooks/useRegister';
+import { useToast } from '@hooks/useToast';
 
 function RegisterPage() {
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordConfirm, setPasswordConfirm] = useState('');
+    const {mutate: register, isPending, error} = useRegister();
+    const {toast} = useToast();
     const { removeHistoryAndGo } = useRouter();
 
+    const handleSubmit = (event: FormEvent) => {
+        event.preventDefault();
+        if(password !== passwordConfirm){
+            toast.error('비밀번호가 일치하지 않습니다.', {
+                toastTheme: 'black',
+                position: 'topCenter',
+            });
+            return;
+        }
+        register({email, name, password});
+    }
+
     return (
-        <div css={RegisterContainer}>
+        <form css={RegisterContainer} onSubmit={handleSubmit}>
             <div css={RegisterBox}>
                 <h1 css={titleContainer}>회원가입</h1>
 
                 <div css={inputContainer}>
                     <div css={emailContainer}>
-                        <Input placeholder="test@naver.com" height={'4.5rem'} label={'이메일'} />
+                        <Input value={email} onChange={(e) => setEmail(e.target.value)} placeholder="test@naver.com" height={'4.5rem'} label={'이메일'} />
                         <Button
                             variant="primary"
                             size="full"
@@ -35,14 +55,18 @@ function RegisterPage() {
                             중복확인
                         </Button>
                     </div>
-                    <Input placeholder="홍길동" height={'4.5rem'} label={'이름'} />
-                    <PasswordInput placeholder="••••••" height={'4.5rem'} label={'비밀번호'} />
-                    <PasswordInput placeholder="••••••" height={'4.5rem'} label={'비밀번호확인'} />
+                    <Input value={name} onChange={(e) => setName(e.target.value)} placeholder="홍길동" height={'4.5rem'} label={'이름'} />
+                    <PasswordInput value={password} onChange={(e) => setPassword(e.target.value)} placeholder="••••••" height={'4.5rem'} label={'비밀번호'} />
+                    <PasswordInput value={passwordConfirm} onChange={(e) => setPasswordConfirm(e.target.value)} placeholder="••••••" height={'4.5rem'} label={'비밀번호확인'} />
                 </div>
 
+                {error && (
+                    <div>회원가입에 실패했습니다.</div>
+                )}
+
                 <div css={buttonContainer}>
-                    <Button variant="primary" size="full">
-                        회원가입
+                    <Button type='submit' variant="primary" size="full">
+                        {isPending ? '로딩 중...' : '회원가입'}
                     </Button>
                     <Button
                         onClick={() => removeHistoryAndGo('/login')}
@@ -53,7 +77,7 @@ function RegisterPage() {
                     </Button>
                 </div>
             </div>
-        </div>
+        </form>
     );
 }
 export { RegisterPage };
