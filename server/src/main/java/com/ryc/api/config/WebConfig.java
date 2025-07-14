a9d37dc2e075f251aaac8f1a437330affa8d9e82
SangHyeok0908 <tkdgur9799@gmail.com>
2025-07-14 13:52:48 +0900

    Fix: CORS 설정

--- server/src/main/java/com/ryc/api/config/WebConfig.java
+++ server/src/main/java/com/ryc/api/config/WebConfig.java
@@ -10,8 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry) {
     registry
         .addMapping("/**")
-        .allowedOrigins("http://localhost:3000") // 허용할 Origin
-        .allowedOrigins("https://d24yror9k3fwgn.cloudfront.net/") // 허용할 Origin
+        .allowedOrigins(
+            "http://localhost:3000", "https://d24yror9k3fwgn.cloudfront.net") // 허용할 Origin
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
         .allowCredentials(true) // 쿠키 인증 요청 허용
         .allowedHeaders("*")
