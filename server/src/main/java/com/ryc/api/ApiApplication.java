5402ceb370fa16898bca29b984ae891e70439c3e
SangHyeok0908 <tkdgur9799@gmail.com>
2025-07-14 14:52:22 +0900

    Refactor: BaseEntity

--- server/src/main/java/com/ryc/api/ApiApplication.java
+++ server/src/main/java/com/ryc/api/ApiApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableAspectJAutoProxy
 @EnableScheduling
 @SpringBootApplication
+@EnableJpaAuditing
 @EntityScan(basePackages = {"com.ryc.api.v2"})
 public class ApiApplication {
 
--- server/src/main/java/com/ryc/api/v2/common/entity/BaseEntity.java
+++ server/src/main/java/com/ryc/api/v2/common/entity/BaseEntity.java
@@ -17,7 +17,7 @@ import lombok.Getter;
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
   @CreatedDate
-  @Column(name = "created_at", updatable = false)
+  @Column(name = "created_at", nullable = false, updatable = false)
   private LocalDateTime createdAt;
 
   @LastModifiedDate
