7bafef2d4cf495764f8044d855ddeccb271adfe1
littleemm <lilysolar@naver.com>
2025-07-15 14:53:00 +0900

    fix: 평가 작성/삭제/수정 시 폼 상태 각각 관리하는 방식으로 개선

--- client/src/components/EvaluationBox/EvaluationBox.tsx
+++ client/src/components/EvaluationBox/EvaluationBox.tsx
@@ -1,17 +1,19 @@
 import { Button, Divider, Rating, Text } from '@components';
 import { PersonalScoreCard } from '@components/PersonalScoreCard';
 import { TextArea } from '@components/_common/TextArea';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
     perStarScoreGroup,
     s_averageNumber,
     s_averageText,
     s_boxContainer,
+    s_buttonContainerForEdit,
+    s_cancelButton,
     s_evaluationTitleContainer,
     s_savedEvaluationContainer,
     s_starScoreContainer,
-    textareaCss,
-    userEvaluation,
+    s_textarea,
+    s_userEvaluation,
 } from './EvaluationBox.style';
 import type { EvaluationBoxProps } from './types';
 
@@ -25,7 +27,21 @@ function EvaluationBox({
     // prop destruction
     // lib hooks
     // initial values
+    const [formStateMap, setFormStateMap] = useState<
+        Record<
+            number,
+            { score: number; comment: string; isOpenForm: boolean; commentIdForEdit: number | null }
+        >
+    >({});
+    const formState = formStateMap[evaluation.applicantId] ?? {
+        score: 0,
+        comment: '',
+        isOpenForm: false,
+        commentIdForEdit: null,
+    };
+
     // state, ref, querystring hooks
+    const [commentIdForEdit, setCommentIdForEdit] = useState<number | null>(null);
     const [score, setScore] = useState<number>(0);
     const [comment, setComment] = useState<string>('');
     const [isOpenForm, setIsOpenForm] = useState<boolean>(false);
@@ -39,7 +55,7 @@ function EvaluationBox({
     const handlePost = () => {
         onPostComment({
             applicantId: evaluation.applicantId,
-            body: { score: score, comment: comment },
+            body: { score: formState.score, comment: formState.comment },
         });
         setScore(0);
         setComment('');
@@ -56,16 +72,43 @@ function EvaluationBox({
             applicantId: evaluation.applicantId,
             commentId: myComment.id,
             body: {
-                score: score,
-                comment: comment,
+                score: formState.score,
+                comment: formState.comment,
             },
         });
         setIsOpenForm(false);
         setScore(0);
         setComment('');
+        setCommentIdForEdit(null);
+        handleFormState({ isOpenForm: false });
+    };
+
+    const handleFormState = (partial: Partial<typeof formState>) => {
+        setFormStateMap((prev) => ({
+            ...prev,
+            [evaluation.applicantId]: {
+                ...formState,
+                ...partial,
+            },
+        }));
+    };
+
+    const handleOpenFormState = (partial: Partial<typeof formState>) => {
+        setFormStateMap((prev) => ({
+            ...prev,
+            [evaluation.applicantId]: {
+                ...formState,
+                ...partial,
+            },
+        }));
+        setIsOpenForm(false);
     };
 
     // effects
+    useEffect(() => {
+        if (isOpenForm) handleFormState({ score, comment, isOpenForm, commentIdForEdit });
+    }, [isOpenForm]);
+
     return (
         <div css={s_boxContainer(height)}>
             <div css={s_savedEvaluationContainer(Boolean(myComment))}>
@@ -94,11 +137,13 @@ function EvaluationBox({
                                 score={evaluator.score}
                                 name={evaluator.name}
                                 comment={evaluator.comment}
+                                commentId={evaluator.id}
                                 isMine={evaluator.isMine}
                                 handleDelete={handleDelete}
                                 onComment={setComment}
                                 onScore={setScore}
                                 onOpenForm={setIsOpenForm}
+                                onId={setCommentIdForEdit}
                                 isEditable
                             />
                         ))
@@ -109,19 +154,39 @@ function EvaluationBox({
                     )}
                 </div>
             </div>
-            {(!Boolean(myComment) || isOpenForm) && (
-                <div css={userEvaluation}>
-                    <Rating size="lg" value={score} onChange={(score) => setScore(score)} />
+            {/* 등록된 평가가 없거나, 수정 버튼을 누르면 아래 Form이 열립니다. */}
+            {(!Boolean(myComment) || formState.isOpenForm) && (
+                <div css={s_userEvaluation}>
+                    <Rating
+                        size="lg"
+                        value={formState.score}
+                        onChange={(score) => handleFormState({ score })}
+                    />
                     <TextArea
                         size="xs"
                         placeholder="코멘트를 작성해주세요."
-                        textAreaSx={textareaCss}
-                        value={comment}
-                        onChange={(e) => setComment(e.target.value)}
+                        textAreaSx={s_textarea}
+                        value={formState.comment}
+                        onChange={(e) => handleFormState({ comment: e.target.value })}
                     />
-                    <Button size="full" onClick={isOpenForm ? handleUpdate : handlePost}>
-                        {isOpenForm ? '수정하기' : '저장하기'}
-                    </Button>
+                    {!formState.isOpenForm ? (
+                        <Button size="full" onClick={handlePost}>
+                            저장하기
+                        </Button>
+                    ) : (
+                        <div css={s_buttonContainerForEdit}>
+                            <Button
+                                size="full"
+                                onClick={() => handleOpenFormState({ isOpenForm: false })}
+                                sx={s_cancelButton}
+                            >
+                                취소하기
+                            </Button>
+                            <Button size="full" onClick={handleUpdate}>
+                                수정하기
+                            </Button>
+                        </div>
+                    )}
                 </div>
             )}
         </div>
