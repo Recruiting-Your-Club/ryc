5d1da0d97a044f0325ecd86f23fc45dcd99a221d
SangHyeok0908 <tkdgur9799@gmail.com>
2025-07-16 17:12:37 +0900

    Fix: allowOrigins

--- server/src/main/java/com/ryc/api/config/WebConfig.java
+++ server/src/main/java/com/ryc/api/config/WebConfig.java
@@ -10,10 +10,11 @@ public class WebConfig implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry) {
     registry
         .addMapping("/**")
-        .allowedOrigins("http://localhost:3000") // 허용할 Origin
-        .allowedOrigins("http://localhost:3000/")
-        .allowedOrigins("https://d24yror9k3fwgn.cloudfront.net")
-        .allowedOrigins("https://d24yror9k3fwgn.cloudfront.net/") // 허용할 Origin
+        .allowedOrigins(
+            "http://localhost:3000",
+            "http://localhost:3000/",
+            "https://d24yror9k3fwgn.cloudfront.net",
+            "https://d24yror9k3fwgn.cloudfront.net/") // 허용할 Origin
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
         .allowCredentials(true) // 쿠키 인증 요청 허용
         .allowedHeaders("*")
