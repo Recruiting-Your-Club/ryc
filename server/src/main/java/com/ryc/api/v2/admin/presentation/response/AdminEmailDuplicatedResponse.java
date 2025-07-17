8084ca5a7ba550033b095d0c8a5da027366ed7e9
SangHyeok0908 <tkdgur9799@gmail.com>
2025-07-17 18:56:49 +0900

    Docs: Response 명세 구체화

--- server/src/main/java/com/ryc/api/v2/admin/presentation/response/AdminEmailDuplicatedResponse.java
+++ server/src/main/java/com/ryc/api/v2/admin/presentation/response/AdminEmailDuplicatedResponse.java
@@ -1,3 +1,8 @@
 package com.ryc.api.v2.admin.presentation.response;
 
-public record AdminEmailDuplicatedResponse(boolean duplicated) {}
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record AdminEmailDuplicatedResponse(
+    @Schema(
+        description = "이메일이 중복되었는지 여부")
+    boolean duplicated) {}
