a6bf5f453a2bd643bd867543e5b6ba48e0cdf3f5
aiminghee3 <zzuni3423@naver.com>
2025-07-17 16:47:05 +0900

    test: 깃허브 액션 환경변수 테스트

--- monorepo/apps/user/webpack.common.js
+++ monorepo/apps/user/webpack.common.js
@@ -62,9 +62,18 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: 'public/index.html',
         }),
-        new Dotenv({
-            path: path.resolve(__dirname, './', dotenvFilename),
-            systemvars: true,
-        }),
+        ...(process.env.NODE_ENV === 'production'
+            ? [
+                  new webpack.DefinePlugin({
+                      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+                      'process.env.BASE_URL': JSON.stringify(process.env.BASE_URL),
+                      'process.env.API_MOKING': JSON.stringify(process.env.API_MOKING),
+                  }),
+              ]
+            : [
+                  new Dotenv({
+                      path: path.resolve(__dirname, './', dotenvFilename),
+                  }),
+              ]),
     ],
 };
