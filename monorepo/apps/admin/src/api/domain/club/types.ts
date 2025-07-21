d57e88b127aacc9beff846f2d0ff4d145429f1fc
ohsung0722 <99doldol@naver.com>
2025-07-21 18:48:44 +0900

    refactor: admin/club 도메인 최신화

--- monorepo/apps/admin/src/api/domain/club/types.ts
+++ monorepo/apps/admin/src/api/domain/club/types.ts
@@ -1,33 +1,7 @@
-type summaries = {
-    title: string;
-    value: string;
-};
-type images = {
-    imageUrl: string;
-    thumbnailUrl: string;
-};
-type announcementStatus = 'RECRUITING' | 'UPCOMING' | 'CLOSED' | 'EMPTY';
-
-interface ClubTag {
-    name: string;
-}
-export interface AllClub {
+export interface MyClubResponse {
     id: string;
     name: string;
     shortDescription: string;
     imageUrl: string;
     thumbnailUrl: string;
-    category: string;
-    announcementStatus?: announcementStatus;
-    clubTags: ClubTag[];
-}
-export interface Club {
-    name: string;
-    detailDescription: string;
-    imageUrl: string;
-    thumbnailUrl: string;
-    category: string;
-    clubTags: ClubTag[];
-    clubSummaries: summaries[];
-    clubDetailImages: images[];
 }
