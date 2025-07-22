3b20f3f62b4ae17fa977bcc7480c14dcc2a7b306
Dochanii <dmlwhd010@naver.com>
2025-07-22 19:01:20 +0900

    add: (mono) Dropdown 모노레포 공통 ui 컴포넌트에 추가

---
+++ monorepo/packages/ui/src/components/Dropdown/Dropdown.tsx
@@ -0,0 +1,76 @@
+import type { CSSObject } from '@emotion/react';
+import type { ReactNode, SetStateAction } from 'react';
+import React, { useCallback, useMemo, useRef, useState } from 'react';
+
+import { useClickOutside } from '@ssoc/hooks';
+
+import { s_dropdown } from './Dropdown.styles';
+import { DropdownContent } from './DropdownContent';
+import { DropdownContext } from './DropdownContext';
+import { DropdownGroup } from './DropdownGroup';
+import { DropdownItem } from './DropdownItem';
+import { DropdownLabel } from './DropdownLabel';
+import { DropdownSeperator } from './DropdownSeperator';
+import { DropdownSub } from './DropdownSub';
+import { DropdownSubContent } from './DropdownSubContent';
+import { DropdownSubTrigger } from './DropdownSubTrigger';
+import { DropdownTrigger } from './DropdownTrigger';
+import type { DropdownProps } from './types';
+
+function DropdownRoot({
+    children,
+    onOpenChange,
+    defaultOpen,
+    open: controlledOpen,
+    sx,
+}: DropdownProps) {
+    //prop destruction
+    //lib hooks
+    //state, ref, querystring hooks
+    const [uncontrolledOpen, setUncontrolledOpen] = useState(defaultOpen || false);
+    const triggerRef = useRef<HTMLButtonElement>(null);
+    const contentRef = useRef<HTMLDivElement>(null);
+
+    //form hooks
+    //query hooks
+    //calculated values
+    const open = controlledOpen ?? uncontrolledOpen;
+
+    //handlers
+    const setOpen = useCallback(
+        (value: SetStateAction<boolean>) => {
+            const newValue = typeof value === 'function' ? value(open) : value; //상태관리에 함수가 들어올 경우 생각
+            if (controlledOpen === undefined) {
+                setUncontrolledOpen(newValue);
+            }
+            //일단 상태 변경 값은 무조건 적용해야 할 것 같아서 if-else문 밖에 적용
+            onOpenChange?.(newValue);
+        },
+        [controlledOpen, onOpenChange, open],
+    );
+
+    const contextValue = useMemo(() => ({ open, setOpen, triggerRef, contentRef }), [open]);
+
+    //effects
+    useClickOutside([triggerRef, contentRef], () => setOpen(false));
+
+    return (
+        <DropdownContext.Provider value={contextValue}>
+            <div css={[s_dropdown, sx]}>{children}</div>
+        </DropdownContext.Provider>
+    );
+}
+
+const Dropdown = Object.assign(DropdownRoot, {
+    Trigger: DropdownTrigger,
+    Content: DropdownContent,
+    Item: DropdownItem,
+    Label: DropdownLabel,
+    Group: DropdownGroup,
+    Seperator: DropdownSeperator,
+    Sub: DropdownSub,
+    SubTrigger: DropdownSubTrigger,
+    SubContent: DropdownSubContent,
+});
+
+export { Dropdown };
---
+++ monorepo/packages/ui/src/components/Dropdown/DropdownGroup.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { s_dropdownGroup } from './Dropdown.styles';
+import type { DropdownGruopProps } from './types';
+
+function DropdownGroup({ children, sx, ...props }: DropdownGruopProps) {
+    return (
+        <div css={[s_dropdownGroup, sx]} {...props}>
+            {children}
+        </div>
+    );
+}
+
+export { DropdownGroup };
