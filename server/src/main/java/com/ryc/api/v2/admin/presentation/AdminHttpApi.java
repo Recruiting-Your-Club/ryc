1891ab4c397c95743c31277e0ffddb3e9bcfd80b
SangHyeok0908 <tkdgur9799@gmail.com>
2025-07-22 13:34:11 +0900

    Refactor: email 요청 값 validate 수행

--- server/src/main/java/com/ryc/api/v2/admin/presentation/AdminHttpApi.java
+++ server/src/main/java/com/ryc/api/v2/admin/presentation/AdminHttpApi.java
@@ -1,6 +1,8 @@
 package com.ryc.api.v2.admin.presentation;
 
+import jakarta.validation.constraints.Email;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import com.ryc.api.v2.admin.presentation.response.AdminEmailDuplicatedResponse;
@@ -13,6 +15,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/v2/admin")
+@Validated
 @RequiredArgsConstructor
 @Tag(name = "사용자")
 public class AdminHttpApi {
@@ -25,7 +28,7 @@ public class AdminHttpApi {
       description = "주어진 이메일이 이미 사용 중인지 확인합니다. 사용 중이면 true, 아니면 false를 반환합니다.")
   @ApiResponse(responseCode = "200", description = "이메일 중복 확인 성공")
   public ResponseEntity<AdminEmailDuplicatedResponse> checkEmailDuplicate(
-      @RequestParam String email) {
+      @RequestParam @Email String email) {
     AdminEmailDuplicatedResponse response = adminService.checkEmailDuplicate(email);
     return ResponseEntity.ok(response);
   }
