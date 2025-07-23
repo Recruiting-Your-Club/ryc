eacc6b63a3123aa6100ea54da9867af6a05af647
huro0906 <huro0906@gmail.com>
2025-07-23 11:35:06 +0900

    Feat: Applicant request, response 정의

---
+++ server/src/main/java/com/ryc/api/v2/applicant/presentation/dto/request/ApplicantPersonalInfoCreateRequest.java
@@ -0,0 +1,14 @@
+package com.ryc.api.v2.applicant.presentation.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import com.ryc.api.v2.applicationForm.domain.enums.PersonalInfoQuestionType;
+
+import lombok.Builder;
+
+@Builder
+public record ApplicantPersonalInfoCreateRequest(
+    @NotNull(message = "questionId shouldn't be null")
+        PersonalInfoQuestionType personalInfoQuestionType,
+    @NotBlank(message = "value shouldn't be blank") String value) {}
