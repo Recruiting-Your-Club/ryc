acc29368d3c496462d1eb3d833105d988096123c
Dochanii <dmlwhd010@naver.com>
2025-07-29 16:38:43 +0900

    feat: RecruitDialog에 공고 상세 정보 데이터 props 추가

--- client/src/components/RecruitDialog/types.ts
+++ client/src/components/RecruitDialog/types.ts
@@ -1,5 +1,8 @@
+import type { Announcement } from '@api/domain/announcement/types';
+
 export interface RecruitmentDialogProps {
     open?: boolean;
     handleClose?: () => void;
     link?: string;
+    announcementDetail: Announcement;
 }
