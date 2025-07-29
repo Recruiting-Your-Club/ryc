fdbe02ae9ce44032153ecdcd8db1f6f58f1d300d
Dochanii <dmlwhd010@naver.com>
2025-07-29 16:37:26 +0900

    feat: 공고 전체 리스트, 공고 상세 정보에 대한 쿼리 생성

--- client/src/api/queryFactory/announcementQueries.ts
+++ client/src/api/queryFactory/announcementQueries.ts
@@ -1,12 +1,21 @@
 import { queryOptions } from '@tanstack/react-query';
 import { announcementKeys } from '../querykeyFactory';
-import { getApplicationForm, getAnnouncementList } from '@api/domain/announcement/announcement';
+import {
+    getApplicationForm,
+    getAnnouncementList,
+    getAnnouncementDetail,
+} from '@api/domain/announcement/announcement';
 
 const announcementQueries = {
     getAnnouncementList: (clubId: string) =>
         queryOptions({
             queryKey: announcementKeys.lists(clubId),
             queryFn: () => getAnnouncementList(clubId),
+        }),
+    getAnnouncementDetail: (announcementId: string) =>
+        queryOptions({
+            queryKey: announcementKeys.detail(announcementId),
+            queryFn: () => getAnnouncementDetail(announcementId),
         }),
     getApplicationForm: () =>
         queryOptions({
