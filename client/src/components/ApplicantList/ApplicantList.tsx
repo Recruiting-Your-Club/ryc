1f149f5bbafc8cb851586aa5c4d21bbc8fcb1e83
littleemm <lilysolar@naver.com>
2025-08-02 01:24:06 +0900

    feat: ApplicantList Container에 커스텀 스타일 위한 sx prop 추가

--- client/src/components/ApplicantList/ApplicantList.tsx
+++ client/src/components/ApplicantList/ApplicantList.tsx
@@ -21,6 +21,7 @@ function ApplicantList({
     onSelectApplicantId,
     titleMode = 'titleString',
     children,
+    sx,
 }: ApplicationListProps) {
     // prop destruction
     // lib hooks
@@ -44,7 +45,7 @@ function ApplicantList({
     // handlers
     // effects
     return (
-        <div css={s_listContainer(height)}>
+        <div css={[s_listContainer(height), sx]}>
             <div css={s_titleContainer}>
                 <Text as="span" type="captionSemibold" textAlign="start">
                     {titleMode === 'titleString' ? title : children}
--- client/src/components/ApplicantList/types.ts
+++ client/src/components/ApplicantList/types.ts
@@ -1,4 +1,5 @@
 import type { Applicant } from '@api/domain/applicant/types';
+import type { CSSObject } from '@emotion/react';
 import type { ReactNode } from 'react';
 
 export interface ApplicationListProps {
@@ -9,6 +10,7 @@ export interface ApplicationListProps {
     onSelectApplicantId: (id: number) => void;
     titleMode?: TitleMode;
     children?: ReactNode;
+    sx?: CSSObject;
 }
 
 export type TitleMode = 'titleString' | 'titleNode';
