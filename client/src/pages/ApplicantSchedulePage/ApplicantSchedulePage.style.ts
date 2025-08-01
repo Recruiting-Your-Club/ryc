9d1c094db3156effadf212377f61a6bfc765ea4e
littleemm <lilysolar@naver.com>
2025-08-02 01:26:15 +0900

    feat: componentMover 적용, 지원자 목록 컴포넌트에 dropdown 적용, 기준 지원자 목록 컴포넌트 강조

--- client/src/pages/ApplicantSchedulePage/ApplicantSchedulePage.style.ts
+++ client/src/pages/ApplicantSchedulePage/ApplicantSchedulePage.style.ts
@@ -1,4 +1,6 @@
 import { css } from '@emotion/react';
+import theme from '@styles/theme';
+import { hexToRgb } from '@utils/hexToRgb';
 
 export const s_applicantSchedulePageContainer = css`
     width: 100%;
@@ -7,11 +9,17 @@ export const s_applicantSchedulePageContainer = css`
     overflow: visible;
     padding: 3rem 2rem;
     display: flex;
-    gap: 1rem;
+    /* gap: 0.5rem; */
+    justify-content: center;
+`;
+
+export const s_arrowContainer = css`
+    flex: 0.05;
+    /* background-color: olive; */
 `;
 
 export const s_contentComponentWrapper = css`
-    flex: 2.5;
+    flex: 3;
     height: 100%;
     /* background-color: gray; */
     padding: 2rem 1.8rem;
@@ -34,4 +42,17 @@ export const s_timeTable = css`
 
 export const s_buttonGroup = css`
     height: 22rem;
+`;
+
+export const s_selectionButton = css`
+    border-color: ${theme.colors.gray[300]};
+    border-radius: 20px;
+    padding: 0 1.5rem;
+    width: 11.5rem;
+    height: 2.8rem;
+    ${theme.typography.captionRegular}
+`;
+
+export const s_highlightedApplicantList = css`
+    box-shadow: 0px 0px 30px 0px rgba(${hexToRgb(theme.colors.default)}, 0.25);
 `;
--- client/src/pages/ApplicantSchedulePage/ApplicantSchedulePage.tsx
+++ client/src/pages/ApplicantSchedulePage/ApplicantSchedulePage.tsx
@@ -3,64 +3,144 @@ import {
     s_applicantSchedulePageContainer,
     s_contentComponentWrapper,
     s_buttonGroup,
-    s_timeTable,
+    s_arrowContainer,
+    s_selectionButton,
+    s_highlightedApplicantList,
 } from './ApplicantSchedulePage.style';
-import { ApplicantList, InterviewTimeTable } from '@components';
+import { ApplicantList, Button, ComponentMover, Dropdown, InterviewTimeTable } from '@components';
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { interviewQueries } from '@api/queryFactory';
 import { convertDate } from '@utils/convertDate';
 import { applicantQueries } from '@api/queryFactory/applicantQueries';
+import { useToast } from '@hooks/useToast';
 
 function ApplicantSchedulePage() {
     // prop destruction
     // lib hooks
+    const { toast } = useToast();
     // initial values
     const { data: interviewSchedulelist = [] } = useSuspenseQuery(
         interviewQueries.allInterviewSchedules(),
     );
     // state, ref, querystring hooks
+    const [open, setOpen] = useState<boolean>(false);
+    const [standardOpen, setStandardOpen] = useState<boolean>(false);
+
     const [selectedApplicantId, setSelectedApplicantId] = useState<number>(1);
+    const [selectedStandardApplicantId, setSelectedStandardApplicantId] = useState<number>(1);
     const [selectedInterviewLabel, setSelectedInterviewLabel] = useState<string>(() => {
         if (interviewSchedulelist[0]) {
             const date = convertDate(interviewSchedulelist[0].date);
-            const name = interviewSchedulelist[0].interviewSets[0].name;
+            const name = interviewSchedulelist[0].interviewSets[1].name;
             return `${date} ${name}`;
         }
         return '면접 일정 없음';
     });
+    const [selectedStandardInterviewLabel, setSelectedStandardInterviewLabel] = useState<string>(
+        () => {
+            if (interviewSchedulelist[0]) {
+                const date = convertDate(interviewSchedulelist[0].date);
+                const name = interviewSchedulelist[0].interviewSets[0].name;
+                return `${date} ${name}`;
+            }
+            return '면접 일정 없음';
+        },
+    );
+
     // form hooks
     // query hooks
     const { data: applicantList = [] } = useSuspenseQuery(applicantQueries.allApplicants());
+
     // calculated values
     // handlers
+    const handleSelectStandardLabel = (label: string) => {
+        const isSame = label === selectedInterviewLabel && label !== '면접 일정 없음';
+
+        if (isSame) {
+            toast('같은 면접 일정은 선택할 수 없어요!', { type: 'error', toastTheme: 'black' });
+            setStandardOpen(true);
+            return;
+        }
+
+        setSelectedStandardInterviewLabel(label);
+        setStandardOpen(false);
+    };
+
+    const handleSelectLabel = (label: string) => {
+        const isSame = label === selectedStandardInterviewLabel && label !== '면접 일정 없음';
+
+        if (isSame) {
+            toast('같은 면접 일정은 선택할 수 없어요!', { type: 'error', toastTheme: 'black' });
+            setOpen(true);
+            return;
+        }
+
+        setSelectedInterviewLabel(label);
+        setOpen(false);
+    };
     // effects
     return (
         <div css={s_applicantSchedulePageContainer}>
             <div css={s_contentComponentWrapper}>
-                <InterviewTimeTable
-                    interviewSchedules={interviewSchedulelist}
-                    selectedInterviewLabel={selectedInterviewLabel}
-                    onSelect={(label) => setSelectedInterviewLabel(label)}
-                    sx={s_timeTable}
-                    listSx={s_buttonGroup}
-                />
-            </div>
-            <div css={s_contentComponentWrapper}>
                 <ApplicantList
                     applicantList={applicantList}
                     selectedApplicantId={selectedApplicantId}
                     onSelectApplicantId={setSelectedApplicantId}
-                />
+                    titleMode="titleNode"
+                >
+                    <Dropdown key="wannaChange" open={open} onOpenChange={setOpen}>
+                        <Dropdown.Trigger asChild>
+                            <Button variant="outlined" sx={s_selectionButton}>
+                                {selectedInterviewLabel}
+                            </Button>
+                        </Dropdown.Trigger>
+                        <Dropdown.Content offsetX={11.7} offsetY={42}>
+                            <InterviewTimeTable
+                                interviewSchedules={interviewSchedulelist}
+                                selectedInterviewLabel={selectedInterviewLabel}
+                                onSelect={handleSelectLabel}
+                                onOpenChange={setOpen}
+                                listSx={s_buttonGroup}
+                            />
+                        </Dropdown.Content>
+                    </Dropdown>
+                </ApplicantList>
+            </div>
+            <div css={s_arrowContainer}>
+                <ComponentMover></ComponentMover>
             </div>
             <div css={s_contentComponentWrapper}>
                 <ApplicantList
                     applicantList={applicantList}
-                    selectedApplicantId={selectedApplicantId}
-                    onSelectApplicantId={setSelectedApplicantId}
-                />
+                    selectedApplicantId={selectedStandardApplicantId}
+                    onSelectApplicantId={setSelectedStandardApplicantId}
+                    titleMode="titleNode"
+                    sx={s_highlightedApplicantList}
+                >
+                    <Dropdown key="standard" open={standardOpen} onOpenChange={setStandardOpen}>
+                        <Dropdown.Trigger asChild>
+                            <Button variant="outlined" sx={s_selectionButton}>
+                                {selectedStandardInterviewLabel}
+                            </Button>
+                        </Dropdown.Trigger>
+                        <Dropdown.Content offsetX={11.7} offsetY={42}>
+                            <InterviewTimeTable
+                                interviewSchedules={interviewSchedulelist}
+                                selectedInterviewLabel={selectedStandardInterviewLabel}
+                                onSelect={handleSelectStandardLabel}
+                                onOpenChange={setStandardOpen}
+                                listSx={s_buttonGroup}
+                            />
+                        </Dropdown.Content>
+                    </Dropdown>
+                </ApplicantList>
+            </div>
+            <div css={s_arrowContainer}>
+                <ComponentMover />
             </div>
             <div css={s_contentComponentWrapper}>
                 <ApplicantList
+                    title="면접 일정 미지정자"
                     applicantList={applicantList}
                     selectedApplicantId={selectedApplicantId}
                     onSelectApplicantId={setSelectedApplicantId}
