cd225bbbd3a97fc55a022a4aeae754afa4eadc44
huro0906 <huro0906@gmail.com>
2025-08-03 22:13:34 +0900

    Feat: s3 의존성 추가 및 s3 Config파일 추가

---
+++ server/src/main/java/com/ryc/api/config/S3Config.java
@@ -0,0 +1,48 @@
+package com.ryc.api.config;
+
+import org.apache.catalina.authenticator.BasicAuthenticator;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+
+@Configuration
+public class S3Config {
+
+  @Value("${CLOUD_AWS_ACCESS_KEY}")
+  private String accessKey;
+
+  @Value("${CLOUD_AWS_SECRET_KEY}")
+  private String secretKey;
+
+  @Value("${CLOUD_AWS_REGION}")
+  private String region;
+
+  @Bean
+  public S3AsyncClient s3AsyncClient() {
+    AwsBasicCredentials awsBasicCredentials = AwsBasicCredentials.create(accessKey, secretKey);
+    Region region = Region.of(this.region);
+
+    return S3AsyncClient.builder()
+            .credentialsProvider(() -> awsBasicCredentials)
+            .region(region)
+        .build();
+  }
+
+  @Bean
+  public S3Presigner s3Presigner() {
+    AwsBasicCredentials awsBasicCredentials = AwsBasicCredentials.create(accessKey, secretKey);
+    Region region = Region.of(this.region);
+
+    return S3Presigner.builder()
+            .credentialsProvider(() -> awsBasicCredentials)
+            .region(region)
+        .build();
+  }
+
+}
