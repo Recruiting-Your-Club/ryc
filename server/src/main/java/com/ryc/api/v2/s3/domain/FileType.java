b2426e2f9c385b317d5d8ca7eeeac3258e8c8df6
huro0906 <huro0906@gmail.com>
2025-08-04 11:53:06 +0900

    Feat: FileType에 따른 contentType 제한 로직 추가

--- server/src/main/java/com/ryc/api/v2/s3/domain/FileType.java
+++ server/src/main/java/com/ryc/api/v2/s3/domain/FileType.java
@@ -1,7 +1,26 @@
 package com.ryc.api.v2.s3.domain;
 
+import java.util.Set;
+
 public enum FileType {
-  IMAGE,
-  DOCUMENT,
-  VIDEO
+  CLUB_PROFILE(Set.of("image/jpeg", "image/png", "image/webp")),
+  ANNOUNCEMENT_IMAGE(Set.of("image/jpeg", "image/png", "image/webp")),
+  APPLICATION_ATTACHMENT(Set.of("image/jpeg", "image/png", "image/webp",
+          // PDF
+          "application/pdf"));
+
+  private final Set<String> allowedContentTypes;
+
+    FileType(Set<String> allowedContentTypes) {
+        this.allowedContentTypes = allowedContentTypes;
+    }
+
+    public boolean isAllowedContentType(String contentType) {
+        if(contentType == null) {
+            return false;
+        }
+        String lowCaseContentType = contentType.toLowerCase();
+
+        return allowedContentTypes.contains(lowCaseContentType);
+    }
 }
