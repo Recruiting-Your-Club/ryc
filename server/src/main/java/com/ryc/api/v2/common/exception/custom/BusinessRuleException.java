b6a497453832db969c07742af19cfc5be86c4324
huro0906 <huro0906@gmail.com>
2025-08-04 13:59:59 +0900

    Refactor: BusinessRuleException message 커스터마이징 추가
    Params를 통해 message에 동적할당 가능

--- server/src/main/java/com/ryc/api/v2/common/exception/custom/BusinessRuleException.java
+++ server/src/main/java/com/ryc/api/v2/common/exception/custom/BusinessRuleException.java
@@ -6,7 +6,21 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public class BusinessRuleException extends RuntimeException {
   private final ErrorCode errorCode;
+  private final Object[] params;
+
+  public BusinessRuleException(ErrorCode errorCode, Object... params) {
+    super(errorCode.getMessage());
+    this.errorCode = errorCode;
+    this.params = params;
+  }
+
+  public String getFormattedMessage() {
+    if(params == null) {
+      return errorCode.getMessage();
+    }
+
+    return String.format(errorCode.getMessage(), params);
+  }
 }
