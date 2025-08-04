329160f705671ba558ddaaf88a06c76075ea4ec3
SangHyeok0908 <tkdgur9799@gmail.com>
2025-08-04 20:03:37 +0900

    Feat: Announcement 도메인 응답 값 설정

--- server/src/main/java/com/ryc/api/v2/announcement/presentation/AnnouncementHttpApi.java
+++ server/src/main/java/com/ryc/api/v2/announcement/presentation/AnnouncementHttpApi.java
@@ -8,21 +8,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import com.ryc.api.v2.announcement.common.exception.code.AnnouncementErrorCode;
 import com.ryc.api.v2.announcement.presentation.dto.request.AnnouncementCreateRequest;
 import com.ryc.api.v2.announcement.presentation.dto.request.AnnouncementUpdateRequest;
 import com.ryc.api.v2.announcement.presentation.dto.response.*;
 import com.ryc.api.v2.applicationForm.presentation.response.ApplicationFormResponse;
 import com.ryc.api.v2.common.aop.annotation.HasRole;
-import com.ryc.api.v2.common.exception.response.ErrorResponse;
+import com.ryc.api.v2.common.exception.annotation.ApiErrorCodeExample;
+import com.ryc.api.v2.common.exception.code.ClubErrorCode;
+import com.ryc.api.v2.common.exception.code.CommonErrorCode;
+import com.ryc.api.v2.common.exception.code.PermissionErrorCode;
 import com.ryc.api.v2.role.domain.enums.Role;
 import com.ryc.api.v2.security.dto.CustomUserDetail;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.headers.Header;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RequestMapping("/api/v2")
@@ -32,24 +31,25 @@ public interface AnnouncementHttpApi {
   @PostMapping("/clubs/{club-id}/announcements")
   @HasRole(Role.MEMBER)
   @Operation(summary = "클럽 공고 생성", operationId = "createAnnouncement")
-  @ApiResponses(
-      value = {
-        @ApiResponse(
-            responseCode = "201",
-            description = "Created",
-            content =
-                @Content(
-                    mediaType = "application/json",
-                    schema = @Schema(implementation = AnnouncementCreateResponse.class)),
-            headers = {@Header(name = "Location", description = "생성된 리소스의 상세정보 조회 URI")}),
-        @ApiResponse(
-            responseCode = "400",
-            description = "Bad Request",
-            content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
-        @ApiResponse(
-            responseCode = "404",
-            description = "존재하지 않는 클럽입니다.",
-            content = @Content(schema = @Schema(hidden = true)))
+  // TODO: EntityNotFoundException 예외 응답 코드 정의 필요
+  @ApiErrorCodeExample(
+      value = {AnnouncementErrorCode.class, PermissionErrorCode.class, CommonErrorCode.class},
+      include = {
+        "INVALID_ANNOUNCEMENT_STATUS",
+        "INTERVIEW_PERIOD_REQUIRED",
+        "DOCUMENT_RESULT_PERIOD_REQUIRED",
+        "INTERVIEW_PERIOD_NOT_ALLOWED",
+        "DOCUMENT_RESULT_PERIOD_NOT_ALLOWED",
+        "DOCUMENT_PERIOD_MUST_BE_AFTER_APPLICATION",
+        "INTERVIEW_PERIOD_MUST_BE_AFTER_DOCUMENT",
+        "FINAL_RESULT_PERIOD_MUST_BE_AFTER_INTERVIEW",
+        "FINAL_RESULT_PERIOD_MUST_BE_AFTER_APPLICATION",
+        "MISSING_REQUIRED_PERSONAL_INFO",
+        "INVALID_QUESTION_TYPE",
+        "QUESTION_OPTION_REQUIRED",
+        "QUESTION_OPTION_NOT_ALLOWED",
+        "FORBIDDEN_NOT_CLUB_MEMBER",
+        "INVALID_PARAMETER"
       })
   ResponseEntity<AnnouncementCreateResponse> create(
       @AuthenticationPrincipal CustomUserDetail userDetail,
@@ -58,44 +58,42 @@ public interface AnnouncementHttpApi {
 
   @GetMapping("/clubs/{club-id}/announcements")
   @Operation(summary = "클럽 공고 목록 조회")
-  @ApiResponses(
-      value = {
-        @ApiResponse(responseCode = "200", description = "OK"),
-        @ApiResponse(
-            responseCode = "404",
-            description = "존재하지 않는 클럽입니다.",
-            content = @Content(schema = @Schema(hidden = true)))
-      })
   ResponseEntity<List<AnnouncementGetAllResponse>> getAnnouncementsByClubId(
       @PathVariable("club-id") String clubId);
 
   @GetMapping("/announcements/{announcement-id}")
   @Operation(summary = "공고 상세 조회")
-  @ApiResponses(
-      value = {
-        @ApiResponse(responseCode = "200", description = "OK"),
-        @ApiResponse(
-            responseCode = "404",
-            description = "존재하지 않는 공고입니다.",
-            content = @Content(schema = @Schema(hidden = true)))
-      })
+  // TODO: EntityNotFoundException 예외 응답 코드 정의 필요
   ResponseEntity<AnnouncementGetDetailResponse> getAnnouncementDetail(
       @PathVariable("announcement-id") String announcementId);
 
   @PutMapping("/clubs/{club-id}/announcements/{announcement-id}")
   @HasRole(Role.MEMBER)
   @Operation(summary = "공고 수정", operationId = "updateAnnouncement")
-  @ApiResponses(
+  @ApiErrorCodeExample(
       value = {
-        @ApiResponse(responseCode = "200", description = "OK"),
-        @ApiResponse(
-            responseCode = "400",
-            description = "Bad Request",
-            content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
-        @ApiResponse(
-            responseCode = "404",
-            description = "존재하지 않는 클럽 또는 공고입니다.",
-            content = @Content(schema = @Schema(hidden = true)))
+        AnnouncementErrorCode.class,
+        ClubErrorCode.class,
+        PermissionErrorCode.class,
+        CommonErrorCode.class
+      },
+      include = {
+        "INVALID_ANNOUNCEMENT_STATUS",
+        "INTERVIEW_PERIOD_REQUIRED",
+        "DOCUMENT_RESULT_PERIOD_REQUIRED",
+        "INTERVIEW_PERIOD_NOT_ALLOWED",
+        "DOCUMENT_RESULT_PERIOD_NOT_ALLOWED",
+        "DOCUMENT_PERIOD_MUST_BE_AFTER_APPLICATION",
+        "INTERVIEW_PERIOD_MUST_BE_AFTER_DOCUMENT",
+        "FINAL_RESULT_PERIOD_MUST_BE_AFTER_INTERVIEW",
+        "FINAL_RESULT_PERIOD_MUST_BE_AFTER_APPLICATION",
+        "MISSING_REQUIRED_PERSONAL_INFO",
+        "INVALID_QUESTION_TYPE",
+        "QUESTION_OPTION_REQUIRED",
+        "QUESTION_OPTION_NOT_ALLOWED",
+        "CLUB_NOT_FOUND",
+        "FORBIDDEN_NOT_CLUB_MEMBER",
+        "INVALID_PARAMETER"
       })
   ResponseEntity<AnnouncementUpdateResponse> updateAnnouncementDetail(
       @PathVariable("club-id") String clubId,
@@ -104,14 +102,7 @@ public interface AnnouncementHttpApi {
 
   @Operation(summary = "지원폼 조회")
   @GetMapping("/announcements/{announcement-id}/application-form")
-  @ApiResponses(
-      value = {
-        @ApiResponse(responseCode = "200", description = "OK"),
-        @ApiResponse(
-            responseCode = "404",
-            description = "announcement Not found",
-            content = @Content(schema = @Schema(hidden = true)))
-      })
+  // TODO: EntityNotFoundException 예외 응답 코드 정의 필요
   ResponseEntity<ApplicationFormResponse> getApplicationForm(
       @PathVariable("announcement-id") String announcementId);
 }
