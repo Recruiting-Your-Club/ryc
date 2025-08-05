b74c13e370d5f55953c5687f9f9488e2f1eb6b8d
Dochanii <dmlwhd010@naver.com>
2025-08-05 18:01:15 +0900

    refactor: getPlaceholder 함수 유틸로 이동

--- client/src/pages/ClubApplyPage/utils.ts
+++ client/src/pages/ClubApplyPage/utils.ts
@@ -3,3 +3,22 @@ import type { Answer } from './types';
 export const getAnswer = (answers: Answer[], questionTitle: string): string => {
     return answers.find((answer) => answer.questionTitle === questionTitle)?.value || '';
 };
+
+export const getPlaceholder = (label: string) => {
+    switch (label) {
+        case '학번':
+            return 'ex) 19011069';
+        case '이름':
+            return 'ex) 홍길동, John Smith';
+        case '이메일':
+            return 'ex) example@gmail.com';
+        case '생년월일':
+            return 'ex) 990101';
+        case '전화번호':
+            return 'ex) 01012345678';
+        case '전공':
+            return 'ex) 소프트웨어학과';
+        default:
+            return '';
+    }
+};
