92881ae7c38841f010d19cacc7c9311b7ad1519e
littleemm <lilysolar@naver.com>
2025-08-05 23:16:26 +0900

    fix: ui 상 업데이트 안되는 현상 발생 -> 지원자 상태 동적 업데이트 가능한 핸들러로 수정

--- client/src/mocks/handlers/stepHandler.ts
+++ client/src/mocks/handlers/stepHandler.ts
@@ -4,12 +4,23 @@ import stepApplicantList from '../data/step/stepApplicantList.json';
 import totalSteps from '../data/step/totalSteps.json';
 import type { Step, StepApplicant } from '@api/domain/step/types';
 
+let stepApplicants = [...stepApplicantList];
+
 const stepHandler = [
     http.get(`${BASE_URL}step/all`, () => {
         return HttpResponse.json(totalSteps as Step, { status: 200 });
     }),
     http.get(`${BASE_URL}step-applicants/all`, () => {
-        return HttpResponse.json(stepApplicantList as StepApplicant[], { status: 200 });
+        return HttpResponse.json(stepApplicants as StepApplicant[], { status: 200 });
+    }),
+    http.patch(`${BASE_URL}step-applicants/:applicantId/status`, async ({ request, params }) => {
+        const { applicantId } = params;
+        const { status } = (await request.json()) as { status: string };
+
+        stepApplicants = stepApplicants.map((applicant) =>
+            applicant.applicantId === applicantId ? { ...applicant, status } : applicant,
+        );
+        return HttpResponse.json({ status: 200 });
     }),
 ];
 
