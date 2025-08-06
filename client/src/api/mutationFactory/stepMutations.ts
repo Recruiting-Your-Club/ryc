c357977ae3934404d986f1154d396815be84b4f1
littleemm <lilysolar@naver.com>
2025-08-06 13:30:50 +0900

    chore: 코드 정리

--- client/src/api/mutationFactory/stepMutations.ts
+++ client/src/api/mutationFactory/stepMutations.ts
@@ -1,11 +1,8 @@
 import { updateStepApplicantsStatus } from '@api/domain';
-import { evaluationKeys, stepKeys } from '@api/querykeyFactory';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useMutation } from '@tanstack/react-query';
 
 export const stepMutations = {
     useUpdateStepApplicantStatus: () => {
-        const queryClient = useQueryClient();
-
         return useMutation({
             mutationFn: (params: { applicantId: string; status: string }) =>
                 updateStepApplicantsStatus(params),
