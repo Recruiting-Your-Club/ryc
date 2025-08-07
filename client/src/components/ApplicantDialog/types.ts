ca9cc48fac28284a5acb766308f9f6e55442ba0d
littleemm <lilysolar@naver.com>
2025-08-07 12:32:16 +0900

    refactor: 필요없는 코드 정리

--- client/src/components/ApplicantDialog/types.ts
+++ client/src/components/ApplicantDialog/types.ts
@@ -1,24 +1,9 @@
 import type { EvaluationDataWithSummary } from '@api/domain/evaluation/types';
 import type { StepApplicant } from '@api/domain/step/types';
-import type { evaluation } from '@constants/applicantDialog';
-
-export type EvaluationType = (typeof evaluation)[number];
-
-export interface Evaluator {
-    id: number;
-    name: string;
-    score: number;
-    comment: string;
-}
 
 export interface Document {
     question: string;
     answer: string;
-}
-export interface Evaluation {
-    type: EvaluationType;
-    averageScore: number;
-    evaluators: Evaluator[];
 }
 
 export interface ApplicantDialogProps {
