f5c658c2384726e894c832e2f17fc2ac25ae8b2c
littleemm <lilysolar@naver.com>
2025-08-10 03:01:32 +0900

    refactor: 평가 post api 함수 변경에 따라 컴포넌트 prop 내 타입 추가

--- client/src/components/EvaluationBox/types.ts
+++ client/src/components/EvaluationBox/types.ts
@@ -2,7 +2,13 @@ import type { EvaluationDataWithSummary } from '@api/domain/evaluation/types';
 export interface EvaluationBoxProps {
     selectedApplicantId: string | null;
     evaluation: EvaluationDataWithSummary;
-    onPostComment: (applicantId: string, score: number, comment: string, clubId: string) => void;
+    onPostComment: (
+        applicantId: string,
+        score: number,
+        comment: string,
+        clubId: string,
+        type: 'application' | 'interview',
+    ) => void;
     onDeleteComment: (evaluationId: string, clubId: string) => void;
     onUpdateComment: (evaluationId: string, score: number, comment: string, clubId: string) => void;
     height?: string;
