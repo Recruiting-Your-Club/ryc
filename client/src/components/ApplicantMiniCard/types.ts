697f576be1024e544ec30d14124bd651f4636734
littleemm <lilysolar@naver.com>
2025-08-10 00:11:55 +0900

    refactor: 지원자 미니 카드의 지원자 타입 변경

--- client/src/components/ApplicantMiniCard/types.ts
+++ client/src/components/ApplicantMiniCard/types.ts
@@ -1,14 +1,8 @@
 import type { CSSObject } from '@emotion/react';
 import type { HTMLAttributes } from 'react';
 
-export interface Applicant {
-    id: number;
-    name: string;
-    email: string;
-}
-
 export interface ApplicationMiniCardProps extends HTMLAttributes<HTMLButtonElement> {
-    applicant: Applicant;
+    applicant: { applicantId: string; name: string; email: string };
     isCompleted?: boolean;
     isActivated?: boolean;
     sx?: CSSObject;
