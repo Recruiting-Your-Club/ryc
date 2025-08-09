727f6ba8eca9e9ed7e1219381f4cdd1832ff7fc5
ohsung0722 <99doldol@naver.com>
2025-08-09 22:24:29 +0900

    feat: 공고 생성 및 조회 api type 설정

--- monorepo/apps/admin/src/api/domain/announcement/type.ts
+++ monorepo/apps/admin/src/api/domain/announcement/type.ts
@@ -1,18 +1,61 @@
-export interface announcements {
+export interface Announcements {
     announcementId: string;
     title: string;
 }
 
-export interface detailAnnouncements {
+export interface DetailAnnouncements {
     id: string;
-    title: string;
-    summaryDescription: string;
-    detailDescription: string;
-    target: string;
-    field: string;
-    announcementsStatus: string;
-    announcementType: string;
-    hasInterview: boolean; //안필요할거같은데?
+    title: string; //공고 제목
+    summaryDescription: string; //공고 요약 설명 (공고 카드)
+    detailDescription: string; //공고 상세
+    target: string; //모집 대상
+    field: string; //모집 분야
+    announcementsStatus: 'UPCOMING' | 'RECRUITING' | 'CLOSED' | 'EMPTY';
+    announcementType: 'ALWAYS_OPEN' | 'LIMITED_TIME';
     activityPeriod: string;
     numberOfPeople: string;
+    applicationPeriod: Period; //서류 접수 기간
+    interviewPeriod: Period; //면접 날짜
+    documentResultPeriod: Period; // 서류평가 결과
+    finalResultPeriod: Period; //최종 결과
+    applicationForm: ApplicationForm;
+    tags: string[]; //공고 카드 태그 정보
+    images: Image[];
+}
+
+interface Period {
+    startDate: string;
+    endDate: string;
+}
+
+interface ApplicationForm {
+    applicationQuestions: ApplicationQuestion[];
+    preQuestions: PreQuestion[];
+    personalInfoQuestions: PersonalInfoQuestion[];
+}
+
+type PersonalInfoQuestion = 'STUDENT_ID' | 'PHONE_NUMBER' | 'PROFILE_IMAGE' | 'NAME' | 'EMAIL';
+
+interface ApplicationQuestion {
+    id: string;
+    type: string;
+    label: string;
+    isRequired: boolean;
+    subLabel: string;
+}
+
+interface PreQuestion {
+    id: string;
+    type: string;
+    label: string;
+    isRequired: boolean;
+    options: Option[];
+}
+
+interface Option {
+    option: string;
+}
+
+interface Image {
+    fileMetaData: string;
 }
