f7e5d8415a03ecdd8211c9ac3caf58774f42565b
Dochanii <dmlwhd010@naver.com>
2025-08-10 17:41:28 +0900

    feat: 제출 성공 페이지 모바일 반응형 css 추가

--- client/src/pages/ClubApplyPage/ClubApplySuccessPage/ClubApplySuccessPage.style.ts
+++ client/src/pages/ClubApplyPage/ClubApplySuccessPage/ClubApplySuccessPage.style.ts
@@ -1,5 +1,6 @@
 import { css } from '@emotion/react';
 import theme from '@styles/theme';
+
 export const s_applicationSuccessPageContainer = css`
     width: 100%;
     height: 100%;
@@ -7,15 +8,38 @@ export const s_applicationSuccessPageContainer = css`
     flex-direction: column;
     align-items: center;
     justify-content: center;
+    padding: 2rem 1rem;
+
+    @media (max-width: ${theme.breakpoint.mobile}) {
+        padding: 1rem 0.5rem;
+        justify-content: center;
+    }
 `;
 
-export const s_sucecessDescriptionContainer = css`
+export const s_successDescriptionContainer = css`
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
     gap: 2rem;
     margin-top: 3rem;
+    text-align: center;
+
+    @media (max-width: ${theme.breakpoint.mobile}) {
+        gap: 3rem;
+        padding: 0 1rem;
+    }
+`;
+
+export const s_successDescriptionTextSx = css`
+    @media (max-width: ${theme.breakpoint.mobile}) {
+        ${theme.typography.h3Bold};
+    }
+`;
+export const s_successSubDescriptionTextSx = css`
+    @media (max-width: ${theme.breakpoint.mobile}) {
+        ${theme.typography.bodyRegular};
+    }
 `;
 
 export const s_applicationUserInfoBox = css`
@@ -28,6 +52,13 @@ export const s_applicationUserInfoBox = css`
     padding: 1rem 2rem;
     width: 33rem;
     margin-top: 7rem;
+
+    @media (max-width: ${theme.breakpoint.mobile}) {
+        max-width: 33rem;
+        padding: 1rem 0;
+        margin-top: 4rem;
+        gap: 2.5rem;
+    }
 `;
 
 export const s_applicationUserInfoLabel = css`
@@ -47,4 +78,9 @@ export const s_goToHomeButtonWrapper = css`
     display: flex;
     justify-content: center;
     margin-top: 8rem;
+
+    @media (max-width: ${theme.breakpoint.mobile}) {
+        max-width: 33rem;
+        margin-top: 5rem;
+    }
 `;
