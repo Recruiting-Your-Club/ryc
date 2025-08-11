602aa08584774c017de444b571bc65dad74dfda7
ohsung0722 <99doldol@naver.com>
2025-08-11 16:57:43 +0900

    feat: prefetch 용 query 함수 작성 및 staletime 추가

--- monorepo/apps/admin/src/api/queryFactory/announcementQueries.ts
+++ monorepo/apps/admin/src/api/queryFactory/announcementQueries.ts
@@ -1,11 +1,21 @@
 import { getAllAnnouncements, getDetailAnnouncement } from '@api/domain';
 import { announcementKeys } from '@api/querykeyFactory';
 import { DEFAULT_GCTIME } from '@constants/gcTime';
-import { DEFAULT_STALETIME } from '@constants/staleTime';
+import { DEFAULT_STALETIME, PREFETCH_STALETIME } from '@constants/staleTime';
 import { queryOptions } from '@tanstack/react-query';
 
 const announcementQueries = {
-    list: (clubId: string) =>
+    //prefetch/loader에서 쓸 list 기본 옵션
+    listOpts: (clubId: string) =>
+        queryOptions({
+            queryKey: announcementKeys.list(clubId),
+            queryFn: () => getAllAnnouncements(clubId),
+            staleTime: PREFETCH_STALETIME,
+            gcTime: DEFAULT_GCTIME,
+        }),
+
+    //컴포넌트에서 사용할 list (useSuspenseQuery)
+    listUse: (clubId: string) =>
         queryOptions({
             queryKey: announcementKeys.list(clubId),
             queryFn: () => getAllAnnouncements(clubId),
--- monorepo/apps/admin/src/constants/staleTime.ts
+++ monorepo/apps/admin/src/constants/staleTime.ts
@@ -1,1 +1,2 @@
 export const DEFAULT_STALETIME = 0; /*1000 * 20*/
+export const PREFETCH_STALETIME = 1000 * 20;
