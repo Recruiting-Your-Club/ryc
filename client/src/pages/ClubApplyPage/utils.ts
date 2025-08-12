a538e6827bba6482293ca1a8c499c76bc00b75a6
Dochanii <dmlwhd010@naver.com>
2025-08-12 17:40:50 +0900

    feat: post 유틸함수에서 데이터에 파일 관련 로직 추가

--- client/src/pages/ClubApplyPage/utils.ts
+++ client/src/pages/ClubApplyPage/utils.ts
@@ -120,7 +120,10 @@ export const makeAnsewerDataForSubmit = (answers: Answer[]): ApplicationSubmissi
         answers: applicationAnswers.map((answer) => ({
             questionId: answer.id === null ? '' : answer.id,
             textAnswer: answer.optionIds ? null : answer.value,
-            fileMetadataId: null,
+            fileMetadataId:
+                answer.questionType === 'FILE' || answer.questionType === 'PROFILE_IMAGE'
+                    ? answer.value
+                    : null,
             answerChoices:
                 answer.optionIds?.map((optionId) => ({
                     optionId: optionId,
