4502d74ec1958a7c68388008eb1dbe9b7bd71b91
Dochanii <dmlwhd010@naver.com>
2025-08-12 22:51:06 +0900

    feat: LoadingPage 모노레포 최신화

---
+++ monorepo/apps/user/src/pages/LoadingPage/RecruitmentLoadingPage/RecruitmentLoadingPage.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+
+import {
+    skeletonApplyButton,
+    skeletonApplyButtonMobile,
+    skeletonClubBoxContainer,
+    skeletonClubBoxItem,
+    skeletonClubBoxLabel,
+    skeletonClubBoxValue,
+    skeletonClubName,
+    skeletonClubNameContainer,
+    skeletonContentBody,
+    skeletonContentContainer,
+    skeletonContentHeader,
+    skeletonDescriptionText,
+    skeletonHeaderSubContainer,
+    skeletonImageItem,
+    skeletonImageListContainer,
+    skeletonRecruitmentContainer,
+    skeletonStatusTag,
+    skeletonTextContainer,
+    skeletonTitle,
+} from './RecruitmentLoadingPage.style';
+
+function RecruitmentLoadingPage() {
+    return (
+        <div css={skeletonRecruitmentContainer}>
+            <div css={skeletonContentContainer}>
+                <div css={skeletonContentHeader}>
+                    <div css={skeletonTitle} />
+                    <div css={skeletonHeaderSubContainer}>
+                        <div css={skeletonClubNameContainer}>
+                            <div css={skeletonClubName} />
+                            <div css={skeletonStatusTag} />
+                        </div>
+                        <div css={skeletonApplyButton} />
+                    </div>
+                </div>
+
+                <div css={skeletonContentBody}>
+                    <div css={skeletonClubBoxContainer}>
+                        {Array.from({ length: 6 }).map((_, i) => (
+                            <div key={i} css={skeletonClubBoxItem}>
+                                <div css={skeletonClubBoxLabel} />
+                                <div css={skeletonClubBoxValue} />
+                            </div>
+                        ))}
+                    </div>
+
+                    <div css={skeletonTextContainer}>
+                        <div css={skeletonDescriptionText} />
+                        <div css={skeletonDescriptionText} />
+                        <div css={skeletonDescriptionText} />
+                    </div>
+
+                    <div css={skeletonImageListContainer}>
+                        {Array.from({ length: 9 }).map((_, i) => (
+                            <div key={i} css={skeletonImageItem} />
+                        ))}
+                    </div>
+                </div>
+            </div>
+            <div css={skeletonApplyButtonMobile} />
+        </div>
+    );
+}
+
+export { RecruitmentLoadingPage };
