75df01d8ddf30db7da275ed3934812e7854aa414
littleemm <lilysolar@naver.com>
2025-08-13 13:33:27 +0900

    fix: handler 코드 추가하여 데이터 안불러와지는 현상 해결

--- client/src/mocks/handlers/interviewHandler.ts
+++ client/src/mocks/handlers/interviewHandler.ts
@@ -1,58 +1,10 @@
-import type {
-    ApplicantReservedInterview,
-    Evaluation,
-    Interviewee,
-    IntervieweeDetail,
-    InterviewSchedule,
-} from '@api/domain/interview/types';
 import { BASE_URL } from '@constants/api';
 import { http, HttpResponse } from 'msw';
-import documentList from '../data/interview/documentList.json';
-import evaluationList from '../data/interview/evaluationList.json';
-import intervieweeDetailList from '../data/interview/intervieweeDetailList.json';
-import intervieweeList from '../data/interview/intervieweeList.json';
-import interviewScheduleList from '../data/interview/interviewScheduleList.json';
-import type { Document } from '@api/domain/applicant/types';
 import interviewSlots from '../data/interview/interviewSlots.json';
 import unreservedApplicants from '../data/interview/unreservedApplicants.json';
 import interviewApplicants from '../data/interview/interviewApplicants.json';
 
 const interviewHandler = [
-    http.get(`${BASE_URL}interviewschedules/all`, () => {
-        return HttpResponse.json(interviewScheduleList as InterviewSchedule[], { status: 200 });
-    }),
-    http.get(`${BASE_URL}interviewees/all`, () => {
-        return HttpResponse.json(intervieweeList as Interviewee[], { status: 200 });
-    }),
-    http.get(`${BASE_URL}interviewees/:id`, ({ params }) => {
-        const detail = intervieweeDetailList.find(
-            (interviewee) => interviewee.id === Number(params.id),
-        );
-        return HttpResponse.json(detail as IntervieweeDetail, { status: 200 });
-    }),
-    http.get(`${BASE_URL}documents/:id`, ({ params }) => {
-        const detail = documentList.find((document) => document.applicantId === Number(params.id));
-        return HttpResponse.json(detail as Document, { status: 200 });
-    }),
-    http.get(`${BASE_URL}interviewer/:id`, ({ params }) => {
-        const detail = evaluationList.find(
-            (evaluation) => evaluation.applicantId === Number(params.id),
-        );
-        return HttpResponse.json(detail as Evaluation, { status: 200 });
-    }),
-    http.put(`${BASE_URL}interviewees/:intervieweeId`, async ({ request, params }) => {
-        const interviewee = intervieweeList.find(
-            (interviewee) => interviewee.id === Number(params.intervieweeId),
-        );
-        if (!interviewee) return HttpResponse.json({ status: 404 });
-
-        const body = (await request.json()) as { interviewSetId: number };
-
-        interviewee.interviewSetId = body.interviewSetId;
-
-        return HttpResponse.json(interviewee, { status: 200 });
-    }),
-
     http.get(`${BASE_URL}announcements/:announcementId/interview-slots`, () => {
         return HttpResponse.json(interviewSlots, { status: 200 });
     }),
@@ -69,6 +21,8 @@ const interviewHandler = [
             );
 
             if (!data) return HttpResponse.json({ status: 404 });
+
+            return HttpResponse.json(data, { status: 200 });
         },
     ),
 
