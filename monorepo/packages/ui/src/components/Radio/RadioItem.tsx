e6f54fbd2fd4068ccf0486387a2503950d1039cd
Dochanii <dmlwhd010@naver.com>
2025-08-15 17:23:48 +0900

    chore: husky 무시 제거

--- monorepo/packages/ui/src/components/Radio/RadioItem.tsx
+++ monorepo/packages/ui/src/components/Radio/RadioItem.tsx
@@ -14,10 +14,7 @@ function RadioItem({
     return (
         <label css={s_label(checked, disabled)}>
             <input type="radio" checked={checked} onChange={onChange} css={s_input} {...props} />
-            {/* eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex */}
-            <div css={s_radio(size)} tabIndex={0}>
-                {checked && <div css={s_radioInner} />}
-            </div>
+            <div css={s_radio(size)}>{checked && <div css={s_radioInner} />}</div>
             {option}
         </label>
     );
