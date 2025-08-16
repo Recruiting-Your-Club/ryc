797786dfa31e54169c47e37cf490c968a3e6a89b
SangHyeok0908 <tkdgur9799@gmail.com>
2025-08-16 18:43:18 +0900

    Fix: CORS Origin 파싱

--- server/src/main/java/com/ryc/api/config/WebConfig.java
+++ server/src/main/java/com/ryc/api/config/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-  @Value("${CLIENT_URL}/")
+  @Value("${CLIENT_URL}")
   private String CLIENT_URL;
 
   @Value("${LOCAL_CLIENT_URL}")
