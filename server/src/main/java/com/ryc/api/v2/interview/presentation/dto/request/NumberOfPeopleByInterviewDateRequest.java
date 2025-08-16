6127c129a512c19fcf72b74270f02a19576326f0
SangHyeok0908 <tkdgur9799@gmail.com>
2025-08-16 18:52:49 +0900

    Fix: 면접 시간 검증

--- server/src/main/java/com/ryc/api/v2/interview/presentation/dto/request/NumberOfPeopleByInterviewDateRequest.java
+++ server/src/main/java/com/ryc/api/v2/interview/presentation/dto/request/NumberOfPeopleByInterviewDateRequest.java
@@ -2,13 +2,16 @@ package com.ryc.api.v2.interview.presentation.dto.request;
 
 import java.time.LocalDateTime;
 
+import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record NumberOfPeopleByInterviewDateRequest(
-    @NotNull(message = "면접 시작 날짜와 시간은 null일 수 없습니다.") @Schema(description = "면접 시작 날짜와 시간")
+    @NotNull(message = "면접 시작 날짜와 시간은 null일 수 없습니다.")
+        @Future(message = "면접 시작 시간은 미래의 시간이여야 합니다.")
+        @Schema(description = "면접 시작 날짜와 시간")
         LocalDateTime start,
     @NotNull(message = "면접 당 진행 시간은 null일 수 없습니다.")
         @Min(value = 1, message = "면접 당 진행 시간은 0 이하일 수 없습니다.")
@@ -17,11 +20,4 @@ public record NumberOfPeopleByInterviewDateRequest(
     @NotNull(message = "면접 날짜별 인원 수는 null일 수 없습니다.")
         @Min(value = 1, message = "인원 수는 1명 이상이어야 합니다.")
         @Schema(description = "인원 수")
-        Integer numberOfPeople) {
-
-  public NumberOfPeopleByInterviewDateRequest {
-    if (start.isAfter(LocalDateTime.now())) {
-      throw new IllegalArgumentException("면접 시작 날짜와 시간은 현재 시간 이후여야 합니다.");
-    }
-  }
-}
+        Integer numberOfPeople) {}
