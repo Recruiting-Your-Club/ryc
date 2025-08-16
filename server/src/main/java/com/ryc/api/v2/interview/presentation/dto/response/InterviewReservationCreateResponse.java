b230d292f60078d5f61ed471823a6d575d564d26
SangHyeok0908 <tkdgur9799@gmail.com>
2025-08-16 14:25:22 +0900

    Docs: reservation html

--- server/src/main/java/com/ryc/api/v2/interview/presentation/dto/response/InterviewReservationCreateResponse.java
+++ server/src/main/java/com/ryc/api/v2/interview/presentation/dto/response/InterviewReservationCreateResponse.java
@@ -2,4 +2,5 @@ package com.ryc.api.v2.interview.presentation.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-public record InterviewReservationCreateResponse(@Schema(description = "면접 예약 ID") String interviewReservationId) {}
+public record InterviewReservationCreateResponse(
+    @Schema(description = "면접 예약 ID") String interviewReservationId) {}
