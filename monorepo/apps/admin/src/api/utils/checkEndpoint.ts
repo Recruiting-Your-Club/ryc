d012133a60c53847cf37585ec17a6636f0fb04b1
ohsung0722 <99doldol@naver.com>
2025-08-18 03:56:29 +0900

    fix: 로그인 시 refreshToken 검증하던 기존 오류 해결

--- monorepo/apps/admin/src/api/utils/checkEndpoint.ts
+++ monorepo/apps/admin/src/api/utils/checkEndpoint.ts
@@ -1,11 +1,5 @@
 import { BASE_URL } from '@constants/api';
 
 export const isAuthEndpoint = (rawUrl: string) => {
-    const url = new URL(rawUrl, BASE_URL);
-    const pathname = url.pathname;
-    return (
-        pathname.startsWith('/auth/login') ||
-        pathname.startsWith('/auth/logout') ||
-        pathname.startsWith('/auth/refresh-token')
-    );
+    return rawUrl === 'auth/login' || rawUrl === 'auth/logout' || rawUrl === 'auth/refresh-token';
 };
