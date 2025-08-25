910693f62ed301a345a59fc58b1c6ef6c20d5da0
littleemm <lilysolar@naver.com>
2025-08-25 13:28:06 +0900

    feat: 공고 편집 페이지 내 기본 정보 업데이트

--- monorepo/apps/admin/src/pages/RecruitEditPage/RecruitEditPage.tsx
+++ monorepo/apps/admin/src/pages/RecruitEditPage/RecruitEditPage.tsx
@@ -1,12 +1,38 @@
+import { useUpdateAnnouncement } from '@api/hooks/useUpdateAnnouncement';
+import { announcementQueries } from '@api/queryFactory';
+import { announcementKeys } from '@api/querykeyFactory';
+import { BASE_URL } from '@constants/api';
 import { INITIALRECRUITSTEP, TOTALRECRUITSTEPS } from '@constants/step';
 import { useQuestion } from '@hooks/useQuestion';
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import {
+    BasicInfoStep,
+    DescriptionStepPage,
+    PersonalStatementStep,
+} from '@pages/RecruitCreatePage';
+import { buildAnnouncementSubmitRequest } from '@pages/RecruitCreatePage/buildAnnouncementSubmitRequest';
+import { PreivewStep } from '@pages/RecruitCreatePage/PreviewStep';
+import type { BasicInfoFields, RecruitDetailInfo } from '@pages/RecruitCreatePage/types';
+import { useQuery } from '@tanstack/react-query';
+import dayjs from 'dayjs';
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { generatePath, useParams } from 'react-router-dom';
 
-import { useRouter } from '@ssoc/hooks';
-import { useStepper, useToast } from '@ssoc/ui';
+import { useFileUpload, useRouter } from '@ssoc/hooks';
+import { Button, Dialog, Stepper, useStepper, useToast } from '@ssoc/ui';
+
+import {
+    s_dialogContent,
+    s_dialogHeader,
+    s_prohibitDragArea,
+    s_recruitCreatePageContainer,
+    s_stepButtonContainer,
+    s_stepComponent,
+    s_stepWrapper,
+} from './RecruitEditPage.style';
 
 function RecruitEditPage() {
+    // prop destruction
+    // lib hooks
     const { activeStep, next, prev, isFirst, isLast } = useStepper(
         TOTALRECRUITSTEPS,
         INITIALRECRUITSTEP,
@@ -27,6 +53,326 @@ function RecruitEditPage() {
         updateApplicationQuestion,
         hydrateAll,
     } = useQuestion();
+
+    const { uploadFiles, isLoading: isFileUploading } = useFileUpload(BASE_URL);
+
+    // initial values
+
+    // state, ref, querystring hooks
+    const containerRef = useRef<HTMLDivElement>(null);
+    const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+    //공고 정보 상태 관리
+    const [recruitDetailInfo, setRecruitDetailInfo] = useState<RecruitDetailInfo>({
+        recruitmentSubject: '',
+        recruitmentSummaryDescription: '',
+        recruitmentNumber: '',
+        activityPeriod: '',
+        recruitmentField: '',
+        recruitmentTarget: '',
+        documentPeriod: { startDate: '', endDate: '' },
+        documentResult: { startDate: '', endDate: '' },
+        interviewSchedule: { startDate: '', endDate: '' },
+        finalResult: { startDate: '', endDate: '' },
+        tags: ['중앙동아리'],
+    });
+
+    //공고 상세 설명 글 상태 관리
+    const [detailDescription, setDetailDescription] = useState<string>('');
+
+    //공고 모집 관련 이미지 상태 관리
+    const [recruitFiles, setRecuritFiles] = useState<File[]>([]);
+    const [imageFileIds, setImageFileIds] = useState<string[]>([]);
+
+    //체크박스를 통한 신원 정보 상태관리
+    const [basicInfoFields, setBasicInfoFields] = useState<BasicInfoFields>({
+        studentId: false,
+        phone: false,
+        photo: false,
+    });
+
+    //json 파싱 데이터
+    const submitJson = useMemo(
+        () =>
+            buildAnnouncementSubmitRequest({
+                recruitDetailInfo,
+                basicInfoFields,
+                preQuestions: questions,
+                applicationQuestions,
+                detailDescription,
+                imageFileIds,
+            }),
+        [
+            recruitDetailInfo,
+            basicInfoFields,
+            questions,
+            applicationQuestions,
+            detailDescription,
+            imageFileIds,
+        ],
+    );
+
+    // form hooks
+    // query hooks
+    const { data: detailAnnouncement } = useQuery(announcementQueries.detail(announcementId!));
+    const { mutate: putAnnouncement } = useUpdateAnnouncement({
+        clubId: clubId!,
+        announcementId: announcementId!,
+        onSuccess: () => {
+            setIsDialogOpen(false);
+            const afterPath = generatePath('announcements/:clubId/:announcementId', {
+                clubId: clubId ?? '',
+                announcementId: announcementId ?? '',
+            });
+            removeHistoryAndGo(afterPath);
+        },
+        onError: () => {
+            setIsDialogOpen(false);
+            toast.error('공고 등록에 실패했습니다.');
+        },
+    });
+
+    // calculated values
+    const hasPeriod = (p: { startDate: string; endDate: string }) => !!p?.startDate && !!p?.endDate;
+
+    //--------Step별 유효성 검사--------//
+    //step1 검사
+    const isDescriptionStepValid = useMemo(() => {
+        return (
+            recruitDetailInfo.recruitmentSubject.trim() !== '' &&
+            recruitDetailInfo.recruitmentSummaryDescription.trim() !== '' &&
+            hasPeriod(recruitDetailInfo.documentPeriod)
+        );
+    }, [
+        recruitDetailInfo.recruitmentSubject,
+        recruitDetailInfo.documentPeriod,
+        recruitDetailInfo.recruitmentSummaryDescription,
+    ]);
+
+    //step2 감시
+    const isBasicInfoStepValid = useMemo(() => {
+        return questions.every((question) => {
+            if (question.title.trim() === '') return false;
+
+            if (question.type === 'multiple' || question.type === 'single') {
+                if (!question.options || question.options.length === 0) return false;
+                return question.options.every((option) => option.text.trim() !== '');
+            }
+
+            return true;
+        });
+    }, [questions]);
+
+    //step3 검사
+    const isPersonalStatementStepValid = useMemo(() => {
+        return applicationQuestions.every((question) => {
+            return question.title.trim() !== '';
+        });
+    }, [applicationQuestions]);
+
+    //종합
+    const isCurrentStepValid = () => {
+        switch (activeStep) {
+            case 0:
+                return isDescriptionStepValid;
+            case 1:
+                return isBasicInfoStepValid;
+            case 2:
+                return isPersonalStatementStepValid;
+            default:
+                return true;
+        }
+    };
+    //-----------------------------//
+    // handlers
+    const handleInputChange = (updateFields: Partial<RecruitDetailInfo>) => {
+        setRecruitDetailInfo((prev) => ({
+            ...prev,
+            ...updateFields,
+        }));
+    };
+
+    const handleDetailDescriptionChange = useCallback((html: string) => {
+        setDetailDescription(html);
+    }, []);
+
+    const handleFilesChage = useCallback(
+        async (files: File[]) => {
+            setRecuritFiles(files);
+
+            try {
+                if (!files.length) {
+                    setImageFileIds([]);
+                    return;
+                }
+
+                const ids = await uploadFiles(files, 'ANNOUNCEMENT_CREATE_IMAGE');
+                setImageFileIds(ids);
+                toast.success(`${files.length}개의 파일이 성공적으로 업로드되었습니다.`);
+            } catch (error) {
+                setImageFileIds([]);
+                console.error(error);
+                toast.error('파일 업로드에 실패했습니다. 잠시 후 다시 시도해주세요.');
+            }
+        },
+        [uploadFiles],
+    );
+
+    const handleNextClick = () => {
+        if (isLast) {
+            setIsDialogOpen(true);
+        } else {
+            next();
+        }
+    };
+
+    const handleConfirmSubmit = () => {
+        putAnnouncement(submitJson);
+    };
+
+    // effects
+    useEffect(() => {
+        containerRef.current?.scrollTo({ top: 0, behavior: 'smooth' });
+    }, [activeStep]);
+
+    useEffect(() => {
+        if (detailAnnouncement) {
+            setRecruitDetailInfo({
+                recruitmentSubject: detailAnnouncement.title,
+                recruitmentSummaryDescription: detailAnnouncement.summaryDescription,
+                recruitmentNumber: detailAnnouncement.numberOfPeople,
+                activityPeriod: detailAnnouncement.activityPeriod,
+                recruitmentField: detailAnnouncement.field,
+                recruitmentTarget: detailAnnouncement.target,
+                documentPeriod: {
+                    startDate: dayjs(detailAnnouncement.applicationPeriod.startDate).format(
+                        'YYYY-MM-DD',
+                    ),
+                    endDate: dayjs(detailAnnouncement.applicationPeriod.endDate).format(
+                        'YYYY-MM-DD',
+                    ),
+                },
+                documentResult: {
+                    startDate: dayjs(detailAnnouncement.documentResultPeriod.startDate).format(
+                        'YYYY-MM-DD',
+                    ),
+                    endDate: dayjs(detailAnnouncement.documentResultPeriod.endDate).format(
+                        'YYYY-MM-DD',
+                    ),
+                },
+                interviewSchedule: {
+                    startDate: dayjs(detailAnnouncement.interviewPeriod.startDate).format(
+                        'YYYY-MM-DD',
+                    ),
+                    endDate: dayjs(detailAnnouncement.interviewPeriod.endDate).format('YYYY-MM-DD'),
+                },
+                finalResult: {
+                    startDate: dayjs(detailAnnouncement.finalResultPeriod.startDate).format(
+                        'YYYY-MM-DD',
+                    ),
+                    endDate: dayjs(detailAnnouncement.finalResultPeriod.endDate).format(
+                        'YYYY-MM-DD',
+                    ),
+                },
+                tags: detailAnnouncement.tags,
+            });
+        }
+    }, [detailAnnouncement]);
+
+    const stepComponent = (step: number) => {
+        switch (step) {
+            case 0:
+                return (
+                    <DescriptionStepPage
+                        recruitDetailInfo={recruitDetailInfo}
+                        recruitFiles={recruitFiles}
+                        onChange={handleInputChange}
+                        onFilesChange={handleFilesChage}
+                        isFileUploading={isFileUploading}
+                        detailDescription={detailDescription}
+                        onDetailDescriptionChange={handleDetailDescriptionChange}
+                    />
+                );
+            case 1:
+                return (
+                    <BasicInfoStep
+                        infoFields={basicInfoFields}
+                        setInfoFields={setBasicInfoFields}
+                        questions={questions}
+                        addQuestion={addQuestion}
+                        removeQuestion={removeQuestion}
+                        updateQuestion={updateQuestion}
+                        handleQuestionTypeChange={handleQuestionTypeChange}
+                    />
+                );
+            case 2:
+                return (
+                    <PersonalStatementStep
+                        applicationQuestions={applicationQuestions}
+                        addApplicationQuestion={addApplicationQuestion}
+                        removeApplicationQuestion={removeApplicationQuestion}
+                        updateApplicationQuestion={updateApplicationQuestion}
+                    />
+                );
+            case 3:
+                return <PreivewStep preivewData={submitJson} recruitFiles={recruitFiles} />;
+            default:
+                <div>error</div>;
+        }
+    };
+
+    return (
+        <div css={s_recruitCreatePageContainer} ref={containerRef}>
+            <div css={s_stepWrapper}>
+                <Stepper activeStep={activeStep} sx={s_prohibitDragArea}>
+                    <Stepper.Step>
+                        <Stepper.Label>공고 상세 정보 작성</Stepper.Label>
+                    </Stepper.Step>
+                    <Stepper.Step>
+                        <Stepper.Label>사전 질문 설정</Stepper.Label>
+                    </Stepper.Step>
+                    <Stepper.Step>
+                        <Stepper.Label>자기소개서 설정</Stepper.Label>
+                    </Stepper.Step>
+                    <Stepper.Step>
+                        <Stepper.Label>미리보기</Stepper.Label>
+                    </Stepper.Step>
+                </Stepper>
+
+                <div css={s_stepComponent}>{stepComponent(activeStep)}</div>
+                <div css={s_stepButtonContainer}>
+                    <Button onClick={prev} disabled={isFirst}>
+                        이전
+                    </Button>
+                    <Button
+                        onClick={handleNextClick}
+                        disabled={!isCurrentStepValid()}
+                        loading={isFileUploading}
+                    >
+                        {isLast ? '완료' : '다음'}
+                    </Button>
+                    <Dialog
+                        open={isDialogOpen}
+                        handleClose={() => {
+                            setIsDialogOpen(false);
+                        }}
+                    >
+                        <Dialog.Header sx={s_dialogHeader}>제출 확인</Dialog.Header>
+                        <Dialog.Content sx={s_dialogContent}>
+                            <p>정말 모집 공고를 수정하시겠습니까?</p>
+                            <p>모집이 시작된 후 수정이 불가능합니다.</p>
+                        </Dialog.Content>
+                        <Dialog.Action position="end">
+                            <Button onClick={handleConfirmSubmit}>확인</Button>
+                            <Button onClick={() => setIsDialogOpen(false)} variant="outlined">
+                                취소
+                            </Button>
+                        </Dialog.Action>
+                    </Dialog>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export { RecruitEditPage };
