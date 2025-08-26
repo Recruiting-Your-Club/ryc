2cea104369089f5190859c63b0eb05a782731a23
littleemm <lilysolar@naver.com>
2025-08-27 01:35:35 +0900

    refactor: NotFoundPage 문구 constant로 처리

--- monorepo/apps/user/src/pages/NotFoundPage/NotFoundPage.tsx
+++ monorepo/apps/user/src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,4 +1,5 @@
 import AttentionTriangle from '@assets/images/attention-triangle.svg';
+import { DEFAULT_DESCRIPTION, ERROR_CODE_404_PAGE_NONCODE } from '@constants/errorText';
 
 import { useRouter } from '@ssoc/hooks';
 import { Button, Text } from '@ssoc/ui';
@@ -38,13 +39,9 @@ function NotFoundPage() {
                     404 ERROR
                 </Text>
                 <Text type="h4Semibold" sx={s_captionText}>
-                    페이지를 찾을 수 없습니다.
+                    {ERROR_CODE_404_PAGE_NONCODE}
                 </Text>
-                <Text type="captionRegular">
-                    {
-                        '불편을 드려 죄송합니다.\n궁금한 점이 있으시면 언제든지 채널톡을 통해 문의해주세요!'
-                    }
-                </Text>
+                <Text type="captionRegular">{DEFAULT_DESCRIPTION}</Text>
             </div>
             <div css={s_buttonContainer}>
                 <Button onClick={() => goTo('/')}>처음으로</Button>
