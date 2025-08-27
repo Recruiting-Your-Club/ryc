f36b19dcd544268042f8babcd5f9c26d19b1d5e1
조상준 <juncho12011201@gmail.com>
2025-08-27 17:17:16 +0200

    feat: 지원자 데이터 누락 유효성 검증 추가

--- server/src/main/java/com/ryc/api/v2/applicant/presentation/dto/request/ApplicantCreateRequest.java
+++ server/src/main/java/com/ryc/api/v2/applicant/presentation/dto/request/ApplicantCreateRequest.java
@@ -6,15 +6,16 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+import com.ryc.api.v2.common.validator.request.annotation.Email;
+import com.ryc.api.v2.common.validator.request.annotation.UserName;
+
 import lombok.Builder;
 
 @Builder
 public record ApplicantCreateRequest(
-    @NotBlank(message = "email shouldn't be blank") String email,
-    @NotBlank(message = "name shouldn't be blank") String name,
-    List<
-            @NotNull(message = "personalInfo shouldn't be null") @Valid
-            ApplicantPersonalInfoCreateRequest>
+    @NotBlank(message = "지원자 이메일은 빈 값일 수 없습니다.") @Email String email,
+    @NotBlank(message = "지원자 이름은 빈값일 수 없습니다.") @UserName String name,
+    List<@NotNull(message = "지원자 개인정보는 빈값일 수 없습니다.") @Valid ApplicantPersonalInfoCreateRequest>
         personalInfos) {
   public ApplicantCreateRequest {
     personalInfos = personalInfos == null ? List.of() : personalInfos;
