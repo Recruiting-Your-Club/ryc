fbf4f2bc7d68cbb463e02a009a20d6893e9301cd
조상준 <juncho12011201@gmail.com>
2025-08-27 18:49:59 +0200

    면접 시작일 유효성 검증

--- server/src/main/java/com/ryc/api/v2/interview/presentation/dto/request/NumberOfPeopleByInterviewDateRequest.java
+++ server/src/main/java/com/ryc/api/v2/interview/presentation/dto/request/NumberOfPeopleByInterviewDateRequest.java
@@ -6,18 +6,21 @@ import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record NumberOfPeopleByInterviewDateRequest(
-    @NotNull(message = "면접 시작 날짜와 시간은 null일 수 없습니다.")
+    @Schema(description = "면접 시작 날짜와 시간")
+        @NotNull(message = "면접 시작 날짜와 시간은 null일 수 없습니다.")
+        @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
         @Future(message = "면접 시작 시간은 미래의 시간이여야 합니다.")
-        @Schema(description = "면접 시작 날짜와 시간")
         LocalDateTime start,
-    @NotNull(message = "면접 당 진행 시간은 null일 수 없습니다.")
+    @Schema(description = "면접 당 진행 시간(분)")
+        @NotNull(message = "면접 당 진행 시간은 null일 수 없습니다.")
         @Min(value = 1, message = "면접 당 진행 시간은 0 이하일 수 없습니다.")
-        @Schema(description = "면접 당 진행 시간(분)")
         Integer interviewDuration,
-    @NotNull(message = "면접 날짜별 인원 수는 null일 수 없습니다.")
+    @Schema(description = "인원 수")
+        @NotNull(message = "면접 날짜별 인원 수는 null일 수 없습니다.")
         @Min(value = 1, message = "인원 수는 1명 이상이어야 합니다.")
-        @Schema(description = "인원 수")
         Integer numberOfPeople) {}
