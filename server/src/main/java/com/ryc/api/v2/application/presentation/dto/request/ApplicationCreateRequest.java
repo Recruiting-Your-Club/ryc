4c3a01187bc1c084f258e1169c191ae3e421b4d7
조상준 <juncho12011201@gmail.com>
2025-08-27 17:27:51 +0200

    feat: 지원서 유효성 검증 수정

--- server/src/main/java/com/ryc/api/v2/application/presentation/dto/request/ApplicationCreateRequest.java
+++ server/src/main/java/com/ryc/api/v2/application/presentation/dto/request/ApplicationCreateRequest.java
@@ -3,11 +3,13 @@ package com.ryc.api.v2.application.presentation.dto.request;
 import java.util.List;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 
 import lombok.Builder;
 
 @Builder
-public record ApplicationCreateRequest(List<@Valid AnswerCreateRequest> answers) {
+public record ApplicationCreateRequest(
+    List<@NotNull(message = "각 응답 원소는 null일 수 없습니다.") @Valid AnswerCreateRequest> answers) {
   public ApplicationCreateRequest {
     answers = answers == null ? List.of() : answers;
   }
