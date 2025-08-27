f89807f2e5142db7b7058a328c1782cd2085446e
조상준 <juncho12011201@gmail.com>
2025-08-28 01:43:47 +0200

    fix: 기한 값 null 허용

--- server/src/main/java/com/ryc/api/v2/common/dto/request/PeriodRequest.java
+++ server/src/main/java/com/ryc/api/v2/common/dto/request/PeriodRequest.java
@@ -2,8 +2,6 @@ package com.ryc.api.v2.common.dto.request;
 
 import java.time.LocalDateTime;
 
-import jakarta.validation.constraints.NotNull;
-
 import org.springframework.format.annotation.DateTimeFormat;
 
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -22,7 +20,6 @@ public record PeriodRequest(
             pattern = "yyyy-MM-dd'T'HH:mm",
             description = "시작 날짜",
             example = "2025-06-29T00:00")
-        @NotNull(message = "시작일은 null일 수 없습니다.")
         @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
         LocalDateTime startDate,
     @Schema(
@@ -30,6 +27,5 @@ public record PeriodRequest(
             pattern = "yyyy-MM-dd'T'HH:mm",
             description = "끝 날짜",
             example = "2025-07-20T00:00")
-        @NotNull(message = "종료일은 null일 수 없습니다.")
         @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
         LocalDateTime endDate) {}
