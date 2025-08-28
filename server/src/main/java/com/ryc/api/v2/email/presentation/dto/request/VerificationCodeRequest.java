9f492aa5d18f1ec010cb8213602e66e05fc7d744
SangHyeok0908 <tkdgur9799@gmail.com>
2025-08-28 14:09:30 +0900

    Refactor: MAX Validation value 변경

--- server/src/main/java/com/ryc/api/v2/email/presentation/dto/request/VerificationCodeRequest.java
+++ server/src/main/java/com/ryc/api/v2/email/presentation/dto/request/VerificationCodeRequest.java
@@ -9,6 +9,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 public record VerificationCodeRequest(
     @Schema(description = "인증 받을 이메일") @Email(message = "인증 받는 이메일 형식이 올바르지 않습니다.") String email,
     @Schema(description = "인증 코드")
-        @Max(value = 1000000, message = "인증 코드는 6자리입니다.")
+        @Max(value = 999999, message = "인증 코드는 6자리입니다.")
         @Min(value = 99999, message = "인증 코드는 6자리입니다.")
         Integer code) {}
