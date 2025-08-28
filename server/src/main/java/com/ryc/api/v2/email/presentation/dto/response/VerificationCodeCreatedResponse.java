8c876f242b4226b8e41f65dd85a2deaadae5b18e
SangHyeok0908 <tkdgur9799@gmail.com>
2025-08-28 13:41:18 +0900

    Rename: 만료 시각 중 "초" 부분에 대한 나노 세컨트 단위 제거

--- server/src/main/java/com/ryc/api/v2/email/presentation/dto/response/VerificationCodeCreatedResponse.java
+++ server/src/main/java/com/ryc/api/v2/email/presentation/dto/response/VerificationCodeCreatedResponse.java
@@ -1,8 +1,15 @@
 package com.ryc.api.v2.email.presentation.dto.response;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record VerificationCodeCreatedResponse(
-    @Schema(description = "만료 시각") LocalDateTime expiresAt) {}
+    @Schema(description = "만료 시각") LocalDateTime expiresAt) {
+
+  public VerificationCodeCreatedResponse {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+    expiresAt = LocalDateTime.parse(expiresAt.format(formatter));
+  }
+}
