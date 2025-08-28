46b602c2cace6fffc3def016eeac762f4ba8f592
SangHyeok0908 <tkdgur9799@gmail.com>
2025-08-28 17:54:31 +0900

    Refactor: 이메일 요청 필드에 not blank validation 추가

--- server/src/main/java/com/ryc/api/v2/email/presentation/dto/request/VerificationCodeRequest.java
+++ server/src/main/java/com/ryc/api/v2/email/presentation/dto/request/VerificationCodeRequest.java
@@ -3,11 +3,15 @@ package com.ryc.api.v2.email.presentation.dto.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record VerificationCodeRequest(
-    @Schema(description = "인증 받을 이메일") @Email(message = "인증 받는 이메일 형식이 올바르지 않습니다.") String email,
+    @Schema(description = "인증 받을 이메일")
+        @NotBlank(message = "이메일은 공백일 수 없습니다.")
+        @Email(message = "인증 받는 이메일 형식이 올바르지 않습니다.")
+        String email,
     @Schema(description = "인증 코드")
         @Max(value = 999999, message = "인증 코드는 6자리입니다.")
         @Min(value = 99999, message = "인증 코드는 6자리입니다.")
