7b7a8db7ea9cf4d7c80cc3f84f0d8fd87d62ffee
aiminghee3 <zzuni3423@naver.com>
2025-08-31 18:32:43 +0900

    feat: sentry 설정

--- monorepo/apps/admin/webpack.prod.js
+++ monorepo/apps/admin/webpack.prod.js
@@ -1,7 +1,17 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const { SentryCliPlugin } = require('@sentry/webpack-plugin');
+const packageJson = require('./package.json');
 
 module.exports = merge(common, {
     mode: 'production',
-    devtool: 'source-map',
+    devtool: 'hidden-source-map',
+    plugins: [
+        new SentryCliPlugin({
+            include: './dist', // 소스맵이 있는 디렉토리
+            ignore: ['node_modules', 'webpack.*.js'],
+            configFile: '.sentryclirc',
+            release: `user@${packageJson.version}`, // package.json 버전과 연동
+        }),
+    ],
 });
