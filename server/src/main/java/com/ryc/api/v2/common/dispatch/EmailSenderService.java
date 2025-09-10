41f8c0f046a36b7ed7aa4446f788b1d1c1359955
SangHyeok0908 <tkdgur9799@gmail.com>
2025-09-10 17:12:58 +0900

    Refactor: email 전송 로직 메서드화

--- server/src/main/java/com/ryc/api/v2/common/dispatch/EmailSenderService.java
+++ server/src/main/java/com/ryc/api/v2/common/dispatch/EmailSenderService.java
@@ -27,38 +27,48 @@ public class EmailSenderService {
   private final EmailService emailService;
 
   @Scheduled(fixedDelay = 10000)
-  public void sendPendingEmails() {
+  protected void sendPendingEmails() {
     List<Email> pendingEmails = emailService.findPendingEmails();
 
-    for (Email mail : pendingEmails) {
-      MimeMessage msg = mailSender.createMimeMessage();
+    pendingEmails.forEach(
+        email -> {
+          try {
+            sendEmail(email);
+          } catch (MailException e) {
+            if (email.getRetryCount() >= 3) {
+              log.warn(
+                  "이메일 재시도 초과로 실패: {}, 재시도 횟수: {}", email.getRecipient(), email.getRetryCount());
+              emailService.updateStatus(email, EmailSentStatus.FAILURE);
 
-      try {
-        MimeMessageHelper helper = new MimeMessageHelper(msg, true, "UTF-8");
-        helper.setTo(mail.getRecipient());
-        helper.setSubject(mail.getSubject());
-        helper.setText(mail.getContent(), true);
+            } else {
+              log.error(
+                  "이메일 전송 실패. 10초 후 재시도 예정: {}, 재시도 횟수: {}",
+                  email.getRecipient(),
+                  email.getRetryCount());
+              emailService.incrementRetryCount(email);
+            }
 
-        mailSender.send(msg);
+          } catch (MessagingException e) {
+            log.error("이메일 메시지 생성 실패: {}, 오류: {}", email.getRecipient(), e.getMessage());
+            emailService.updateStatus(email, EmailSentStatus.FAILURE);
+          }
 
-        log.info("이메일 전송 완료: {}", mail.getRecipient());
-        emailService.updateStatus(mail, EmailSentStatus.SENT);
+          log.info("이메일 전송 완료: {}", email.getRecipient());
+          emailService.updateStatus(email, EmailSentStatus.SENT);
+        });
+  }
 
-      } catch (MailException e) {
-        if (mail.getRetryCount() >= 3) {
-          log.warn("이메일 재시도 초과로 실패: {}, 재시도 횟수: {}", mail.getRecipient(), mail.getRetryCount());
-          emailService.updateStatus(mail, EmailSentStatus.FAILURE);
+  @Scheduled(cron = "0 * * * * *")
+  protected void sendInterviewNotifications() {}
 
-        } else {
-          log.error(
-              "이메일 전송 실패. 10초 후 재시도 예정: {}, 재시도 횟수: {}", mail.getRecipient(), mail.getRetryCount());
-          emailService.incrementRetryCount(mail);
-        }
+  private void sendEmail(Email mail) throws MessagingException {
+    MimeMessage msg = mailSender.createMimeMessage();
+    MimeMessageHelper helper = new MimeMessageHelper(msg, true, "UTF-8");
 
-      } catch (MessagingException e) {
-        log.error("이메일 메시지 생성 실패: {}, 오류: {}", mail.getRecipient(), e.getMessage());
-        emailService.updateStatus(mail, EmailSentStatus.FAILURE);
-      }
-    }
+    helper.setTo(mail.getRecipient());
+    helper.setSubject(mail.getSubject());
+    helper.setText(mail.getContent(), true);
+
+    mailSender.send(msg);
   }
 }
