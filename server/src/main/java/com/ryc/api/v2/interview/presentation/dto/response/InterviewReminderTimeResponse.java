33f04b2e1315ca81577bcc3e3d55bc847c160863
SangHyeok0908 <tkdgur9799@gmail.com>
2025-09-20 13:30:19 +0900

    Fix: NPE 해결

--- server/src/main/java/com/ryc/api/v2/interview/presentation/dto/response/InterviewReminderTimeResponse.java
+++ server/src/main/java/com/ryc/api/v2/interview/presentation/dto/response/InterviewReminderTimeResponse.java
@@ -1,3 +1,3 @@
 package com.ryc.api.v2.interview.presentation.dto.response;
 
-public record InterviewReminderTimeResponse(String announcementId, int reminderTime) {}
+public record InterviewReminderTimeResponse(String announcementId, Integer reminderTime) {}
