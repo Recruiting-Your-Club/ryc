667f2eef9e2c8007781c9188c3146fb22397f5a5
littleemm <lilysolar@naver.com>
2025-10-01 21:51:05 +0900

    refactor: 면접 일정 컴포넌트 header text margin 수정

--- monorepo/apps/admin/src/pages/InterviewSchedulePage/InterviewSchedulePage.style.ts
+++ monorepo/apps/admin/src/pages/InterviewSchedulePage/InterviewSchedulePage.style.ts
@@ -147,7 +147,7 @@ export const s_scheduleContentContainer = css`
 `;
 
 export const s_titleText = css`
-    margin: 1.5rem 0 2rem 0;
+    margin: 1rem 0 1.5rem 0;
 `;
 
 export const s_titleAdditionText = css`
